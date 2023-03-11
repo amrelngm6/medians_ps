d6f048b4f945d95d0442cd54097b41bb2f8f490f
Amr <amr@cg.sa>
2023-03-11 07:19:44 +0200

    ...

--- app/config/APP.php
+++ app/config/APP.php
@@ -104,8 +104,8 @@ class APP
 	                array('title'=>__('All bookings'),  'icon'=>'fa-dashboard', 'link'=>'devices/orders'),
 	                array('title'=>__('Active bookings'),  'icon'=>'fa-dashboard', 'link'=>'devices/orders?status=active'),
 	                array('title'=>__('Completed bookings'), 'icon'=>'fa-dashboard','link'=>'devices/orders?status=completed'),
-	                array('title'=>__('Canceled bookings'), 'icon'=>'fa-dashboard', 'link'=>'devices/orders?status=completed'),
 	                array('title'=>__('Paid bookings'),  'icon'=>'fa-dashboard', 'link'=>'devices/orders?status=paid'),
+	                array('title'=>__('Canceled bookings'), 'icon'=>'fa-dashboard', 'link'=>'devices/orders?status=canceled'),
 				]
 			),
 			array('title'=>__('Devices'),  'icon'=>'fa-desktop', 'link'=>'', 'sub'=>
