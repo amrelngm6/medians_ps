1746cd3667b490fc487047deee0f78de4f49730c
amrelngm6 <amr_elngm6@yahoo.com>
2023-05-04 15:08:48 +0200

    ...

--- src/components/calendar-status-list.vue
+++ src/components/calendar-status-list.vue
@@ -4,23 +4,23 @@
             <span class="m-2 py-2 px-4 text-center text-purple font-semibold hover:text-red-600" v-text="__('status_options')" @click="showStatusList = !showStatusList"></span>
             
             <div class="flex flex-wrap" v-if="showStatusList">
-                <div class="flex items-center mx-4 mb-2">
+                <div class="flex items-center mx-4 mb-2" @click="switchClass('active')">
                     <div class="w-3 h-3 rounded-full bg-purple-600 mx-2"></div>
                     <div v-text="__('active')"></div>
                 </div>
-                <div class="flex items-center mx-4 mb-2">
+                <div class="flex items-center mx-4 mb-2" @click="switchClass('new')">
                     <div class="w-3 h-3 rounded-full new bg-gradient-purple mx-2"></div>
                     <div v-text="__('new')"></div>
                 </div>
-                <div class="flex items-center mx-4 mb-2">
+                <div class="flex items-center mx-4 mb-2" @click="switchClass('completed')">
                     <div class="w-3 h-3 rounded-full bg-red-500 mx-2"></div>
                     <div v-text="__('completed')"></div>
                 </div>
-                <div class="flex items-center mx-4 mb-2">
+                <div class="flex items-center mx-4 mb-2"  @click="switchClass('paid')">
                     <div class="w-3 h-3 rounded-full bg-orange-500 mx-2"></div>
                     <div v-text="__('paid')"></div>
                 </div>
-                <div class="flex items-center mx-4 mb-2">
+                <div class="flex items-center mx-4 mb-2"  @click="switchClass('canceled')">
                     <div class="w-3 h-3 rounded-full bg-gray-300 mx-2"></div>
                     <div v-text="__('canceled')"></div>
                 </div>
@@ -41,6 +41,10 @@ export default {
         }
     }, 
     methods: {
+        switchClass(status)
+        {
+            jQuery('.animated.'+status).toggleClass('hidden')
+        },
         __(i)
         {
             return this.$parent.__(i);
