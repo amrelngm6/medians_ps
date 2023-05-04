fba0d0d1749b23b460e192e45e5f1607ec413cb8
amrelngm6 <amr_elngm6@yahoo.com>
2023-05-04 16:15:45 +0200

    ...

--- src/components/calendar-status-list.vue
+++ src/components/calendar-status-list.vue
@@ -3,7 +3,7 @@
         <div >
             <span class="cursor-pointer m-2 py-2 px-4 text-center text-purple font-semibold hover:text-red-600" v-text="__('status_options')" @click="showStatusList = !showStatusList"></span>
             
-            <div class="flex flex-wrap" v-if="showStatusList">
+            <div class="flex flex-wrap py-4" v-if="showStatusList">
                 <div class="flex items-center mx-4 mb-2 cursor-pointer " id="active-status" @click="switchClass('active', 'active-status')">
                     <div class="w-3 h-3 rounded-full bg-purple-600 mx-2"></div>
                     <div v-text="__('active')"></div>
