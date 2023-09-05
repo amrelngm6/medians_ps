b10bdb5ba2831625b1e1106b57bf17d9a08ae9e4
unknown <amr_elngm6@yahoo.com>
2023-09-05 10:16:59 +0300

    .

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -93,7 +93,10 @@
                                         <img :src="message.image_path" class="tyn-image" alt="">
                                     </a>
                                 </div>
-                                <div v-if="!message.message_type || message.message_type === 'text'" class="tyn-reply-text"> {{message.message_text}} </div>
+                                <div v-if="!message.message_type || message.message_type === 'text'" class="tyn-reply-text"> 
+                                    <a v-if="checkHasLink(message.message_text)"  v-text="message.message_text"></a>
+                                    <span v-else >{{message.message_text}}</span>
+                                </div>
                             </div><!-- .tyn-reply-bubble -->
                         </div><!-- .tyn-reply-group -->
                     </div><!-- .tyn-reply-item -->
@@ -250,31 +253,7 @@
                             <div class="tab-content">
                                 <div class="tab-pane show active" id="chat-media-images" tabindex="0">
                                     <div class="row g-3">
-                                        <div class="col-4">
-                                            <a href="/uploads/images/1.jpg" class="glightbox tyn-thumb" data-gallery="media-photo">
-                                                <img src="/uploads/images/thumb-1.jpg" class="tyn-image" alt="">
-                                            </a>
-                                        </div>
-                                        <div class="col-4">
-                                            <a href="/uploads/images/2.jpg" class="glightbox tyn-thumb" data-gallery="media-photo">
-                                                <img src="/uploads/images/thumb-2.jpg" class="tyn-image" alt="">
-                                            </a>
-                                        </div>
-                                        <div class="col-4">
-                                            <a href="/uploads/images/3.jpg" class="glightbox tyn-thumb" data-gallery="media-photo">
-                                                <img src="/uploads/images/thumb-3.jpg" class="tyn-image" alt="">
-                                            </a>
-                                        </div>
-                                        <div class="col-4">
-                                            <a href="/uploads/images/4.jpg" class="glightbox tyn-thumb" data-gallery="media-photo">
-                                                <img src="/uploads/images/thumb-4.jpg" class="tyn-image" alt="">
-                                            </a>
-                                        </div>
-                                        <div class="col-4">
-                                            <a href="/uploads/images/5.jpg" class="glightbox tyn-thumb" data-gallery="media-photo">
-                                                <img src="/uploads/images/thumb-5.jpg" class="tyn-image" alt="">
-                                            </a>
-                                        </div>
+                                        
                                         <div class="col-4">
                                             <a href="/uploads/images/6.jpg" class="glightbox tyn-thumb" data-gallery="media-photo">
                                                 <img src="/uploads/images/thumb-6.jpg" class="tyn-image" alt="">
@@ -284,61 +263,7 @@
                                 </div><!-- .tab-pane -->
                                 <div class="tab-pane" id="chat-media-videos" tabindex="0">
                                     <div class="row g-3">
-                                        <div class="col-6">
-                                            <a href="https://www.youtube.com/watch?v=ag0qTaAKqT8" class="glightbox tyn-video" data-gallery="media-video">
-                                                <img src="/uploads/images/hr-1.jpg" class="tyn-image" alt="">
-                                                <div class="tyn-video-icon">
-                                                    <!-- play-fill -->
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
-                                                        <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
-                                                    </svg>
-                                                </div>
-                                            </a>
-                                        </div>
-                                        <div class="col-6">
-                                            <a href="https://www.youtube.com/watch?v=ag0qTaAKqT8" class="glightbox tyn-video" data-gallery="media-video">
-                                                <img src="/uploads/images/hr-2.jpg" class="tyn-image" alt="">
-                                                <div class="tyn-video-icon">
-                                                    <!-- play-fill -->
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
-                                                        <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
-                                                    </svg>
-                                                </div>
-                                            </a>
-                                        </div>
-                                        <div class="col-6">
-                                            <a href="https://www.youtube.com/watch?v=ag0qTaAKqT8" class="glightbox tyn-video" data-gallery="media-video">
-                                                <img src="/uploads/images/hr-3.jpg" class="tyn-image" alt="">
-                                                <div class="tyn-video-icon">
-                                                    <!-- play-fill -->
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
-                                                        <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
-                                                    </svg>
-                                                </div>
-                                            </a>
-                                        </div>
-                                        <div class="col-6">
-                                            <a href="https://www.youtube.com/watch?v=ag0qTaAKqT8" class="glightbox tyn-video" data-gallery="media-video">
-                                                <img src="/uploads/images/hr-4.jpg" class="tyn-image" alt="">
-                                                <div class="tyn-video-icon">
-                                                    <!-- play-fill -->
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
-                                                        <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
-                                                    </svg>
-                                                </div>
-                                            </a>
-                                        </div>
-                                        <div class="col-6">
-                                            <a href="https://www.youtube.com/watch?v=ag0qTaAKqT8" class="glightbox tyn-video" data-gallery="media-video">
-                                                <img src="/uploads/images/vr-5.jpg" class="tyn-image" alt="">
-                                                <div class="tyn-video-icon">
-                                                    <!-- play-fill -->
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
-                                                        <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
-                                                    </svg>
-                                                </div>
-                                            </a>
-                                        </div>
+                                        
                                         <div class="col-6">
                                             <a href="https://www.youtube.com/watch?v=ag0qTaAKqT8" class="glightbox tyn-video" data-gallery="media-video">
                                                 <img src="/uploads/images/hr-6.jpg" class="tyn-image" alt="">
@@ -354,70 +279,7 @@
                                 </div><!-- .tab-pane -->
                                 <div class="tab-pane" id="chat-media-files" tabindex="0">
                                     <ul class="tyn-media-list gap gap-3">
-                                        <li>
-                                            <a href="#" class="tyn-file">
-                                                <div class="tyn-media-group">
-                                                    <div class="tyn-media tyn-size-lg text-bg-light">
-                                                        <!-- filetype-docx -->
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filetype-docx" viewBox="0 0 16 16">
-                                                            <path fill-rule="evenodd" d="M14 4.5V11h-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5Zm-6.839 9.688v-.522a1.54 1.54 0 0 0-.117-.641.861.861 0 0 0-.322-.387.862.862 0 0 0-.469-.129.868.868 0 0 0-.471.13.868.868 0 0 0-.32.386 1.54 1.54 0 0 0-.117.641v.522c0 .256.04.47.117.641a.868.868 0 0 0 .32.387.883.883 0 0 0 .471.126.877.877 0 0 0 .469-.126.861.861 0 0 0 .322-.386 1.55 1.55 0 0 0 .117-.642Zm.803-.516v.513c0 .375-.068.7-.205.973a1.47 1.47 0 0 1-.589.627c-.254.144-.56.216-.917.216a1.86 1.86 0 0 1-.92-.216 1.463 1.463 0 0 1-.589-.627 2.151 2.151 0 0 1-.205-.973v-.513c0-.379.069-.704.205-.975.137-.274.333-.483.59-.627.257-.147.564-.22.92-.22.357 0 .662.073.916.22.256.146.452.356.59.63.136.271.204.595.204.972ZM1 15.925v-3.999h1.459c.406 0 .741.078 1.005.235.264.156.46.382.589.68.13.296.196.655.196 1.074 0 .422-.065.784-.196 1.084-.131.301-.33.53-.595.689-.264.158-.597.237-.999.237H1Zm1.354-3.354H1.79v2.707h.563c.185 0 .346-.028.483-.082a.8.8 0 0 0 .334-.252c.088-.114.153-.254.196-.422a2.3 2.3 0 0 0 .068-.592c0-.3-.04-.552-.118-.753a.89.89 0 0 0-.354-.454c-.158-.102-.361-.152-.61-.152Zm6.756 1.116c0-.248.034-.46.103-.633a.868.868 0 0 1 .301-.398.814.814 0 0 1 .475-.138c.15 0 .283.032.398.097a.7.7 0 0 1 .273.26.85.85 0 0 1 .12.381h.765v-.073a1.33 1.33 0 0 0-.466-.964 1.44 1.44 0 0 0-.49-.272 1.836 1.836 0 0 0-.606-.097c-.355 0-.66.074-.911.223-.25.148-.44.359-.571.633-.131.273-.197.6-.197.978v.498c0 .379.065.704.194.976.13.271.321.48.571.627.25.144.555.216.914.216.293 0 .555-.054.785-.164.23-.11.414-.26.551-.454a1.27 1.27 0 0 0 .226-.674v-.076h-.765a.8.8 0 0 1-.117.364.699.699 0 0 1-.273.248.874.874 0 0 1-.401.088.845.845 0 0 1-.478-.131.834.834 0 0 1-.298-.393 1.7 1.7 0 0 1-.103-.627v-.495Zm5.092-1.76h.894l-1.275 2.006 1.254 1.992h-.908l-.85-1.415h-.035l-.852 1.415h-.862l1.24-2.015-1.228-1.984h.932l.832 1.439h.035l.823-1.439Z" />
-                                                        </svg>
-                                                    </div>
-                                                    <div class="tyn-media-col">
-                                                        <h6 class="name">Konstantin_cv.docx</h6>
-                                                        <div class="meta">24.65 MB</div>
-                                                    </div>
-                                                </div>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="#" class="tyn-file">
-                                                <div class="tyn-media-group">
-                                                    <div class="tyn-media tyn-size-lg text-bg-light">
-                                                        <!-- filetype-ai -->
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filetype-ai" viewBox="0 0 16 16">
-                                                            <path fill-rule="evenodd" d="M14 4.5V14a2 2 0 0 1-2 2H6v-1h6a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM1.113 14.82.8 15.85H0l1.342-3.999h.926l1.336 3.999h-.841l-.314-1.028H1.113Zm1.178-.588-.49-1.617h-.034l-.49 1.617h1.014Zm2.425-2.382v3.999h-.791V11.85h.79Z" />
-                                                        </svg>
-                                                    </div>
-                                                    <div class="tyn-media-col">
-                                                        <h6 class="name">themeyn_logo.ai</h6>
-                                                        <div class="meta">2.08 MB</div>
-                                                    </div>
-                                                </div>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="#" class="tyn-file">
-                                                <div class="tyn-media-group">
-                                                    <div class="tyn-media tyn-size-lg text-bg-light">
-                                                        <!-- filetype-pdf -->
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filetype-pdf" viewBox="0 0 16 16">
-                                                            <path fill-rule="evenodd" d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM1.6 11.85H0v3.999h.791v-1.342h.803c.287 0 .531-.057.732-.173.203-.117.358-.275.463-.474a1.42 1.42 0 0 0 .161-.677c0-.25-.053-.476-.158-.677a1.176 1.176 0 0 0-.46-.477c-.2-.12-.443-.179-.732-.179Zm.545 1.333a.795.795 0 0 1-.085.38.574.574 0 0 1-.238.241.794.794 0 0 1-.375.082H.788V12.48h.66c.218 0 .389.06.512.181.123.122.185.296.185.522Zm1.217-1.333v3.999h1.46c.401 0 .734-.08.998-.237a1.45 1.45 0 0 0 .595-.689c.13-.3.196-.662.196-1.084 0-.42-.065-.778-.196-1.075a1.426 1.426 0 0 0-.589-.68c-.264-.156-.599-.234-1.005-.234H3.362Zm.791.645h.563c.248 0 .45.05.609.152a.89.89 0 0 1 .354.454c.079.201.118.452.118.753a2.3 2.3 0 0 1-.068.592 1.14 1.14 0 0 1-.196.422.8.8 0 0 1-.334.252 1.298 1.298 0 0 1-.483.082h-.563v-2.707Zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638H7.896Z" />
-                                                        </svg>
-                                                    </div>
-                                                    <div class="tyn-media-col">
-                                                        <h6 class="name">realReact.pdf</h6>
-                                                        <div class="meta">19.54 MB</div>
-                                                    </div>
-                                                </div>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="#" class="tyn-file">
-                                                <div class="tyn-media-group">
-                                                    <div class="tyn-media tyn-size-lg text-bg-light">
-                                                        <!-- filetype-csv -->
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filetype-csv" viewBox="0 0 16 16">
-                                                            <path fill-rule="evenodd" d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM3.517 14.841a1.13 1.13 0 0 0 .401.823c.13.108.289.192.478.252.19.061.411.091.665.091.338 0 .624-.053.859-.158.236-.105.416-.252.539-.44.125-.189.187-.408.187-.656 0-.224-.045-.41-.134-.56a1.001 1.001 0 0 0-.375-.357 2.027 2.027 0 0 0-.566-.21l-.621-.144a.97.97 0 0 1-.404-.176.37.37 0 0 1-.144-.299c0-.156.062-.284.185-.384.125-.101.296-.152.512-.152.143 0 .266.023.37.068a.624.624 0 0 1 .246.181.56.56 0 0 1 .12.258h.75a1.092 1.092 0 0 0-.2-.566 1.21 1.21 0 0 0-.5-.41 1.813 1.813 0 0 0-.78-.152c-.293 0-.551.05-.776.15-.225.099-.4.24-.527.421-.127.182-.19.395-.19.639 0 .201.04.376.122.524.082.149.2.27.352.367.152.095.332.167.539.213l.618.144c.207.049.361.113.463.193a.387.387 0 0 1 .152.326.505.505 0 0 1-.085.29.559.559 0 0 1-.255.193c-.111.047-.249.07-.413.07-.117 0-.223-.013-.32-.04a.838.838 0 0 1-.248-.115.578.578 0 0 1-.255-.384h-.765ZM.806 13.693c0-.248.034-.46.102-.633a.868.868 0 0 1 .302-.399.814.814 0 0 1 .475-.137c.15 0 .283.032.398.097a.7.7 0 0 1 .272.26.85.85 0 0 1 .12.381h.765v-.072a1.33 1.33 0 0 0-.466-.964 1.441 1.441 0 0 0-.489-.272 1.838 1.838 0 0 0-.606-.097c-.356 0-.66.074-.911.223-.25.148-.44.359-.572.632-.13.274-.196.6-.196.979v.498c0 .379.064.704.193.976.131.271.322.48.572.626.25.145.554.217.914.217.293 0 .554-.055.785-.164.23-.11.414-.26.55-.454a1.27 1.27 0 0 0 .226-.674v-.076h-.764a.799.799 0 0 1-.118.363.7.7 0 0 1-.272.25.874.874 0 0 1-.401.087.845.845 0 0 1-.478-.132.833.833 0 0 1-.299-.392 1.699 1.699 0 0 1-.102-.627v-.495Zm8.239 2.238h-.953l-1.338-3.999h.917l.896 3.138h.038l.888-3.138h.879l-1.327 4Z" />
-                                                        </svg>
-                                                    </div>
-                                                    <div class="tyn-media-col">
-                                                        <h6 class="name">my_contacts.csv</h6>
-                                                        <div class="meta">0.97 MB</div>
-                                                    </div>
-                                                </div>
-                                            </a>
-                                        </li>
+                                        
                                         <li>
                                             <a href="#" class="tyn-file">
                                                 <div class="tyn-media-group">
@@ -545,6 +407,14 @@ export default {
     },
 
     methods: {
+
+        /**
+         * 
+         */
+        checkHasLink(message)
+        {
+            return message.indexOf('http') ? true : false;
+        },
 
         /**
          * Upload image to WP
