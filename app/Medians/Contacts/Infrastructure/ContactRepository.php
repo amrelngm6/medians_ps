229f9ad8ead120753456a5e9eeaaf3fdcaad5467
unknown <amr_elngm6@yahoo.com>
2023-09-12 09:25:55 +0300

    .

--- app/Medians/Contacts/Infrastructure/ContactRepository.php
+++ app/Medians/Contacts/Infrastructure/ContactRepository.php
@@ -10,7 +10,20 @@ class ContactRepository
 
     public function loadContacts()
     {
-        return Contact::with('last_message')->with('last_sent_message')->groupBy('wa_id')->where('id', '>', '1')->get();
+        $return = Contact::with('last_message')
+        ->with('last_sent_message')
+        ->groupBy('wa_id')->where('id', '>', '1')->get();
+
+
+        foreach($return as $key => $item)
+        {
+            if (isset($item->last_sent_message->id) && isset($item->last_message->id))
+                $item->last_message = $item->last_sent_message->id > $item->last_message->id  ? $item->last_sent_message : $item->last_message;
+
+            $return[$key] = $item ;
+        }
+        
+        return $return;
     }
 
 
