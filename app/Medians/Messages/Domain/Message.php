36a9a1d69ff798d7dcb1fc773f6aa256e9f1e586
unknown <amr_elngm6@yahoo.com>
2023-09-12 18:04:30 +0300

    .

--- app/Medians/Messages/Domain/Message.php
+++ app/Medians/Messages/Domain/Message.php
@@ -39,7 +39,7 @@ class Message extends CustomModel
 
 	public function reply_message()
 	{
-		return $this->hasOne(Message::class, 'message_id', 'reply_message_id');
+		return $this->hasOne(Message::class, 'reply_message_id', 'message_id');
 	}
 
 	public function reaction()
