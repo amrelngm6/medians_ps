619c37307365e8e06b4b518e111445bcc3d8984a
unknown <amr_elngm6@yahoo.com>
2023-09-12 07:47:13 +0300

    .

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -26,9 +26,9 @@
                         </div>
                     </div>
                 </div>
-                <ul class="tyn-list-inline gap gap-3 ms-auto">
+                <ul class="tyn-list-inline gap gap-3 ms-auto hidden">
                     
-                    <li class="d-none d-sm-block"><button class="btn btn-icon btn-light js-toggle-chat-search">
+                    <li class="d-none d-sm-block"><button class="btn btn-icon btn-light js-toggle-chat-search ">
                             <!-- search -->
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
