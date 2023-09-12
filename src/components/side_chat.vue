2f5fc0b99fd558cfe90b57f0a428bdbd4a6877ed
unknown <amr_elngm6@yahoo.com>
2023-09-12 22:19:16 +0300

    .

--- src/components/side_chat.vue
+++ src/components/side_chat.vue
@@ -269,7 +269,6 @@ export default {
         
         setValues(data) {
             console.log(data)
-            jQuery('#new_chats_count').html(data && data.contacts ? data.contacts.length : null)
             this.content = JSON.parse(JSON.stringify(data)); return this
              
         },
