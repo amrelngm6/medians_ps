c824316e1e6cf2b9b409000e685045244a6e6b20
unknown <amr_elngm6@yahoo.com>
2023-09-12 17:48:54 +0300

    .

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -90,7 +90,7 @@
                             <div class="tyn-reply-bubble" @click="setReadMsg(message.id)">
                                 <span v-if="message.reaction && message.reaction.id" class="p-1 bg-white absolute left-0 bottom-0 w-8 h-8 rounded-full" v-html="message.reaction.message_emojis" style="max-width: none;left: -20px;"></span>
                                 
-                                <div v-if="message.reply_message && message.reply_message.message_type != 'reaction' && message.reply_message.id" class="p-1 bg-gray-100 relative left-0 bottom-0 w-auto rounded-full"  style="max-width: none;">
+                                <div v-if="message.reply_message && message.message_type != 'reaction' && message.reply_message.id" class="p-1 bg-gray-100 relative left-0 bottom-0 w-auto rounded-full"  style="max-width: none;">
                                     <div v-if="message.reply_message.message_type === 'document' && !message.reply_message.is_video" class="tyn-reply-file">
                                         <a :href="message.reply_message.media_path" target="_blank" class="tyn-file">
                                             <div class="tyn-media-group">
