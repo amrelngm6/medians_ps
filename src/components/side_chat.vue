5ab63b8160fc6f7519f75b39f5782f3fe152dbe1
unknown <amr_elngm6@yahoo.com>
2023-09-12 11:16:12 +0300

    .

--- src/components/side_chat.vue
+++ src/components/side_chat.vue
@@ -126,29 +126,22 @@
                                                     </button>
                                                     <div class="dropdown-menu dropdown-menu-end">
                                                         <ul class="tyn-list-links">
-                                                            <li><p class="cursor-pointer" @click="$parent.$refs ? $parent.$refs.activeItem.setReadMsg(contact.last_message.id) : null">
+                                                            <li><p class="cursor-pointer" @click="markRead(contact)">
                                                                     <!-- check -->
                                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
                                                                         <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
                                                                     </svg>
                                                                     <span>Mark as Read</span>
                                                                 </p></li>
-                                                            <li><a href="#">
-                                                                    <!-- person -->
-                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-                                                                        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
-                                                                    </svg>
-                                                                    <span>View Profile</span>
-                                                                </a></li>
                                                                 
-                                                            <li><a href="#">
+                                                            <li><p class="cursor-pointer" @click="joinConversation(contact)">
                                                                     <!-- exclamation-triangle -->
                                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle" viewBox="0 0 16 16">
                                                                         <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z" />
                                                                         <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995z" />
                                                                     </svg>
                                                                     <span>Join</span>
-                                                                </a></li>
+                                                                </p></li>
                                                         </ul>
                                                     </div>
                                                 </li>
@@ -214,6 +207,18 @@ export default {
 
     methods: 
     {
+        
+        markRead(contact)
+        {
+            if (contact && contact.last_message)
+                this.$parent.$refs ? this.$parent.$refs.activeItem.setReadMsg(contact.last_message.id) : null;
+        },
+
+        joinConversation(contact)
+        {
+            if (contact && contact.last_message)
+                this.$parent.$refs ? this.$parent.$refs.activeItem.setReadMsg(contact.last_message.id) : null;
+        },
         selectContact(contact)
         {
 
