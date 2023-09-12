1a25815d779d17db876ae43f0b633134d313d846
unknown <amr_elngm6@yahoo.com>
2023-09-12 21:10:02 +0300

    .

--- src/components/new_chats.vue
+++ src/components/new_chats.vue
@@ -1,38 +1,39 @@
 <template>
     <div class=" " v-if="content.contacts && content.contacts.length">
-        
-        <section class="text-gray-600 body-font mt-12" v-for="conversation in content.contacts">
-            <div class="container px-5 py-24 mx-auto " v-if="conversation">
-                <div class="p-5 bg-white flex items-center mx-auto border-b  mb-10 border-gray-200 rounded-lg sm:flex-row flex-col">
-                    <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-                        <p class="leading-relaxed text-base" >Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
-                        <div class="py-4 "></div>
-                        <div class="md:flex font-bold text-gray-800">
-                            <div class="w-full md:w-1/2 flex space-x-3">
-                                <div class="w-1/2">
-                                    <h2 class="text-gray-500">Name</h2>
-                                    <p v-if="conversation.contact" v-html="conversation.contact.name"></p>
-                                </div>
-                                <div class="w-1/2">
-                                    <h2 class="text-gray-500">Number</h2>
-                                    <p v-if="conversation.contact" v-html="conversation.wa_id"></p>
-                                </div>
-                                <div class="w-1/2">
-                                    <h2 class="text-gray-500">Waiting time</h2>
-                                    <p v-if="conversation.last_message" v-html="conversation.last_message.time_ago"></p>
+        <div class="w-full"  v-for="conversation in content.contacts">
+            <section class="text-gray-600 body-font mt-12">
+                <div class="container px-5 py-24 mx-auto " v-if="conversation">
+                    <div class="p-5 bg-white flex items-center mx-auto border-b  mb-10 border-gray-200 rounded-lg sm:flex-row flex-col">
+                        <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
+                            <p class="leading-relaxed text-base" >Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
+                            <div class="py-4 "></div>
+                            <div class="md:flex font-bold text-gray-800">
+                                <div class="w-full md:w-1/2 flex space-x-3">
+                                    <div class="w-1/2">
+                                        <h2 class="text-gray-500">Name</h2>
+                                        <p v-if="conversation.contact" v-html="conversation.contact.name"></p>
+                                    </div>
+                                    <div class="w-1/2">
+                                        <h2 class="text-gray-500">Number</h2>
+                                        <p v-if="conversation.contact" v-html="conversation.wa_id"></p>
+                                    </div>
+                                    <div class="w-1/2">
+                                        <h2 class="text-gray-500">Waiting time</h2>
+                                        <p v-if="conversation.last_message" v-html="conversation.last_message.time_ago"></p>
+                                    </div>
                                 </div>
                             </div>
+                            <button class="w-1/2 block mx-auto rounded-full bg-gray-900 hover:shadow-lg font-semibold text-white px-6 py-2">Join Chat</button>
+                            <a class="mt-3 text-indigo-500 inline-flex items-center">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                                <path d="M5 12h14M12 5l7 7-7 7"></path>
+                            </svg>
+                            </a>
                         </div>
-                        <button class="w-1/2 block mx-auto rounded-full bg-gray-900 hover:shadow-lg font-semibold text-white px-6 py-2">Join Chat</button>
-                        <a class="mt-3 text-indigo-500 inline-flex items-center">
-                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                            <path d="M5 12h14M12 5l7 7-7 7"></path>
-                        </svg>
-                        </a>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </div>
     </div>
 </template>
 
