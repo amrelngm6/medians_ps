914c9da951c511c8440f3ed6bd9bd2e6e720bf1a
unknown <amr_elngm6@yahoo.com>
2023-09-12 10:02:59 +0300

    .

--- app/Medians/Contacts/Infrastructure/ContactRepository.php
+++ app/Medians/Contacts/Infrastructure/ContactRepository.php
@@ -17,9 +17,10 @@ class ContactRepository
 
         foreach($return as $key => $item)
         {
-            if (isset($item->last_sent_message->id) && isset($item->last_message->id))
+            if (isset($item->last_sent_message->id) && isset($item->last_message->id)) {
                 $item->last_message = $item->last_sent_message->id > $item->last_message->id  ? $item->last_sent_message : $item->last_message;
-
+            }
+            print_r($item);
             $return[$key] = $item ;
         }
         
