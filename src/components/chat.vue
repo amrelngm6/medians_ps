36d4a150f5b0d1933f99efd13e5a66d185d726b3
unknown <amr_elngm6@yahoo.com>
2023-09-13 23:01:04 +0300

    .

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -7,7 +7,6 @@
                 <div class="w-full  flex justify-center">
                     <div class="w-full cursor-pointer" @click="selectContact(contact)">
                         <div class="m-4 block rounded-lg bg-white p-6 shadow-lg dark:bg-neutral-800 dark:shadow-black/20">
-                            <!--Testimonial-->
                             <div class="md:flex md:flex-row gap-6">
                                 <div class="mx-auto mb-6 flex w-36 items-center justify-center md:mx-0 md:w-96 lg:mb-0">
                                     <img src="/uploads/user.svg"
@@ -21,7 +20,7 @@
                                 </div>
                                 <div :class="checkUnread(contact) ? 'text-green-400' : ''" class="md:ml-6 w-full pt-4 text-default" v-if="contact.last_message">
                                     <span v-if="contact.unread" class="inline-block whitespace-nowrap rounded-[0.27rem] bg-danger-100 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-danger-700">New</span>
-                                    <p v-html="contact.last_message.message_text" class="mb-6 text-xl font-light text-neutral-500 dark:text-neutral-300"></p>
+                                    <p v-html="contact.last_message.message_emojis ? contact.last_message.message_emojis : contact.last_message.message_text" class="mb-6 text-xl font-light text-neutral-500 dark:text-neutral-300"></p>
                                     <p class="text-sm font-semibold" v-html="contact.last_message.time_ago"></p>
                                 </div>
                             </div>
