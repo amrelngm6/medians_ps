5a2c6c1991bade21176faf254b4a15f8925d200a
unknown <amr_elngm6@yahoo.com>
2023-09-13 22:49:53 +0300

    .

--- src/components/new_chats.vue
+++ src/components/new_chats.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class=" " v-if="$parent.contacts && $parent.contacts.length">
+    <div class=" " v-if="$parent.new_contacts && $parent.new_contacts.length">
         <h1 class="text-xl text-center mx-auto pt-6 ">Pick your customer</h1>
         <p class=" text-center mx-auto pb-6 pt-2 ">You should pick any customer ASAP to help him </p>
         <div class="container mx-auto w-full row grid overflow-auto" style="max-height: calc(100vh - 90px); padding-bottom:100px" >
-            <section class="text-gray-600 body-font mt-12 col-6"  v-for="conversation in $parent.contacts">
+            <section class="text-gray-600 body-font mt-12 col-6"  v-for="conversation in $parent.new_contacts">
                 <div class="container px-2 py-24 mx-auto " v-if="conversation">
                     <div class="p-5 bg-white flex items-center mx-auto border-b  mb-10 border-gray-200 rounded-lg sm:flex-row flex-col">
                         <div class="w-full flex-grow sm:text-left text-center mt-6 sm:mt-0" v-if="conversation.contact">
