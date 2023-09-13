39a3b55c7a27f65c070ff5da02ab788d069a3f64
unknown <amr_elngm6@yahoo.com>
2023-09-13 22:12:44 +0300

    .

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -19,7 +19,7 @@
                                     <p class="mb-0 font-semibold text-neutral-500 dark:text-neutral-400" v-html="contact.wa_id">
                                     </p>
                                 </div>
-                                <div class="md:ml-6 w-full pt-4 text-default" v-if="contact.last_message">
+                                <div :class="checkUnread(contact) ? 'text-green-400' : ''" class="md:ml-6 w-full pt-4 text-default" v-if="contact.last_message">
                                     <p v-html="contact.last_message.message_text" class="mb-6 text-xl font-light text-neutral-500 dark:text-neutral-300"></p>
                                     <p class="text-sm font-semibold" v-html="contact.last_message.time_ago"></p>
                                 </div>
@@ -512,6 +512,18 @@ export default {
     },
     methods: {
 
+        /**
+         * Check if its unread 
+         */
+        checkUnread(contact)
+        {
+            if (contact && contact.last_message && !contact.last_message.read && contact.last_message.income)
+            {
+                return true;
+            }
+            return false
+        },
+        
         /**
          * Select conversation 
          * 
