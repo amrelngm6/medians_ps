93bef3601caa8ad09ec9ea008f66846ec72dd7ac
unknown <amr_elngm6@yahoo.com>
2023-09-16 23:28:14 +0300

    .

---
+++ src/components/includes/emptydata.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="w-full text-center py-4">
+        <img src="/uploads/img/empty-data.png"  />
+    </div>
+</template>
+<script>
+
+export default 
+{
+};
+</script>
--- src/components/new_chats.vue
+++ src/components/new_chats.vue
@@ -1,76 +1,81 @@
 <template>
-    <div class="container mx-auto mt-4 " v-if="$parent.new_contacts && $parent.new_contacts.length">
-        <h1 class="text-xl  mx-auto pt-6 " v-text="__('new chats')"></h1>
-        <p class="mx-auto pb-6 pt-2 " v-text="__('new chats note')"> </p>
-        <div class="container mx-auto w-full overflow-auto" style="max-height: calc(100vh - 90px); padding-bottom:100px">
-            <div v-for="conversation in $parent.new_contacts" class="w-full my-4 ">
-                <div id="tynChatHead" class="tyn-chat-head bg-white p-2">
+    <div class="w-full" >
+        <emptydata v-if="!$parent.new_contacts.length"></emptydata>
+        <div class="container mx-auto mt-4 " v-if="$parent.new_contacts && $parent.new_contacts.length">
+            <h1 class="text-xl  mx-auto pt-6 " v-text="__('new chats')"></h1>
+            <p class="mx-auto pb-6 pt-2 " v-text="__('new chats note')"> </p>
+            <div class="container mx-auto w-full overflow-auto" style="max-height: calc(100vh - 90px); padding-bottom:100px">
+                <div v-for="conversation in $parent.new_contacts" class="w-full my-4 ">
+                    <div id="tynChatHead" class="tyn-chat-head bg-white p-2">
 
-                    <div class="tyn-media-group" v-if="conversation.contact">
-                        <div class="tyn-media tyn-size-lg d-none d-sm-inline-flex"><img src="/uploads/user.svg" alt=""></div>
-                        <div class="tyn-media tyn-size-rg d-sm-none"><img src="/uploads/user.svg" alt=""></div>
-                        <div class="tyn-media-col">
-                            <div class="tyn-media-row">
-                                <h6 class="name" v-text="conversation.contact.name"></h6>
+                        <div class="tyn-media-group" v-if="conversation.contact">
+                            <div class="tyn-media tyn-size-lg d-none d-sm-inline-flex"><img src="/uploads/user.svg" alt=""></div>
+                            <div class="tyn-media tyn-size-rg d-sm-none"><img src="/uploads/user.svg" alt=""></div>
+                            <div class="tyn-media-col">
+                                <div class="tyn-media-row">
+                                    <h6 class="name" v-text="conversation.contact.name"></h6>
+                                </div>
+                                <div class="tyn-media-row has-dot-sap"><span class="meta" v-text="conversation.wa_id" ></span></div>
                             </div>
-                            <div class="tyn-media-row has-dot-sap"><span class="meta" v-text="conversation.wa_id" ></span></div>
                         </div>
+                        <p class=" px-4 chat-list-msg-web font-bold text-base" v-html="conversation.contact.last_message.message_emojis ? conversation.contact.last_message.message_emojis : conversation.contact.last_message.message_text"></p>
+                        <ul class="tyn-list-inline gap gap-3 ms-auto" v-if="conversation.contact.last_message">
+                            <li><button @click="joinChat(conversation.wa_id)" class="hover:bg-gray-600  text-sm block mx-auto rounded-full bg-gray-900 hover:shadow-lg font-semibold text-white px-6 py-2">Join Chat</button></li>
+                        </ul>
                     </div>
-                    <p class=" px-4 chat-list-msg-web font-bold text-base" v-html="conversation.contact.last_message.message_emojis ? conversation.contact.last_message.message_emojis : conversation.contact.last_message.message_text"></p>
-                    <ul class="tyn-list-inline gap gap-3 ms-auto" v-if="conversation.contact.last_message">
-                        <li><button @click="joinChat(conversation.wa_id)" class="hover:bg-gray-600  text-sm block mx-auto rounded-full bg-gray-900 hover:shadow-lg font-semibold text-white px-6 py-2">Join Chat</button></li>
-                    </ul>
+                    <p class=" bg-white px-4 rounded-full py-2 mt-3 mb-4 chat-list-msg-mobile font-bold text-base " v-html="conversation.contact.last_message.message_emojis ? conversation.contact.last_message.message_emojis : conversation.contact.last_message.message_text"></p>
                 </div>
-                <p class=" bg-white px-4 rounded-full py-2 mt-3 mb-4 chat-list-msg-mobile font-bold text-base " v-html="conversation.contact.last_message.message_emojis ? conversation.contact.last_message.message_emojis : conversation.contact.last_message.message_text"></p>
             </div>
-        </div>
-        <!-- <div class="container mx-auto w-full row grid overflow-auto"
-            style="max-height: calc(100vh - 90px); padding-bottom:100px">
-            <section class="text-gray-600 body-font mt-12 col-6" v-for="conversation in $parent.new_contacts">
-                <div class="container px-2 py-24 mx-auto " v-if="conversation">
-                    <div
-                        class="p-5 bg-white flex items-center mx-auto border-b  mb-10 border-gray-200 rounded-lg sm:flex-row flex-col">
-                        <div class="w-full flex-grow sm:text-left text-center mt-6 sm:mt-0" v-if="conversation.contact">
-                            <p class="leading-relaxed text-lg text-gray-600"
-                                v-html="conversation.contact.message_emojis && conversation.contact.last_message.message_emojis ? conversation.contact.last_message.message_emojis : conversation.contact.last_message.message_text">
-                            </p>
-                            <div class="py-4 "></div>
-                            <div class="md:flex font-bold text-gray-600">
-                                <div class="w-full md:w-1/2 flex space-x-3">
-                                    <div class="w-1/2">
-                                        <h2 class="text-gray-500">Name</h2>
-                                        <p v-if="conversation.contact" v-html="conversation.contact.name"></p>
-                                    </div>
-                                    <div class="w-1/2">
-                                        <h2 class="text-gray-500">Number</h2>
-                                        <p v-if="conversation.contact" v-html="conversation.wa_id"></p>
-                                    </div>
-                                    <div class="w-1/2">
-                                        <h2 class="text-gray-500">Waiting time</h2>
-                                        <p v-if="conversation.contact && conversation.contact.last_message"
-                                            v-html="conversation.contact.last_message.time_ago"></p>
+            <!-- <div class="container mx-auto w-full row grid overflow-auto"
+                style="max-height: calc(100vh - 90px); padding-bottom:100px">
+                <section class="text-gray-600 body-font mt-12 col-6" v-for="conversation in $parent.new_contacts">
+                    <div class="container px-2 py-24 mx-auto " v-if="conversation">
+                        <div
+                            class="p-5 bg-white flex items-center mx-auto border-b  mb-10 border-gray-200 rounded-lg sm:flex-row flex-col">
+                            <div class="w-full flex-grow sm:text-left text-center mt-6 sm:mt-0" v-if="conversation.contact">
+                                <p class="leading-relaxed text-lg text-gray-600"
+                                    v-html="conversation.contact.message_emojis && conversation.contact.last_message.message_emojis ? conversation.contact.last_message.message_emojis : conversation.contact.last_message.message_text">
+                                </p>
+                                <div class="py-4 "></div>
+                                <div class="md:flex font-bold text-gray-600">
+                                    <div class="w-full md:w-1/2 flex space-x-3">
+                                        <div class="w-1/2">
+                                            <h2 class="text-gray-500">Name</h2>
+                                            <p v-if="conversation.contact" v-html="conversation.contact.name"></p>
+                                        </div>
+                                        <div class="w-1/2">
+                                            <h2 class="text-gray-500">Number</h2>
+                                            <p v-if="conversation.contact" v-html="conversation.wa_id"></p>
+                                        </div>
+                                        <div class="w-1/2">
+                                            <h2 class="text-gray-500">Waiting time</h2>
+                                            <p v-if="conversation.contact && conversation.contact.last_message"
+                                                v-html="conversation.contact.last_message.time_ago"></p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <hr class="my-4" />
-                            <button @click="joinChat(conversation.wa_id)"
-                                class="w-1/2 block mx-auto rounded-full bg-gray-900 hover:shadow-lg font-semibold text-white px-6 py-2">Join
-                                Chat</button>
+                                <hr class="my-4" />
+                                <button @click="joinChat(conversation.wa_id)"
+                                    class="w-1/2 block mx-auto rounded-full bg-gray-900 hover:shadow-lg font-semibold text-white px-6 py-2">Join
+                                    Chat</button>
 
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
-        </div> -->
+                </section>
+            </div> -->
+        </div>
     </div>
 </template>
 
 <script>
 const axios = require('axios').default;
+import emptydata from './components/includes/emptydata.vue'
 
 export default {
     name: 'app',
     components: {
+        emptydata
     },
     data() {
         return {
