14214c52b5d3be414dd73db87226b47f7bb55735
unknown <amr_elngm6@yahoo.com>
2023-10-09 06:22:11 +0300

    .

--- index.php
+++ index.php
@@ -13,39 +13,6 @@ file_exists(__DIR__.'/app/config/database.php')
 
 require_once __DIR__.'/vendor/autoload.php';
 
-function calculateDistance($lat1, $lon1, $lat2, $lon2) {
-    // Radius of the Earth in kilometers
-    $radius = 6371;
-
-    // Convert latitude and longitude from degrees to radians
-    $lat1 = deg2rad($lat1);
-    $lon1 = deg2rad($lon1);
-    $lat2 = deg2rad($lat2);
-    $lon2 = deg2rad($lon2);
-
-    // Haversine formula
-    $dlat = $lat2 - $lat1;
-    $dlon = $lon2 - $lon1;
-
-    $a = sin($dlat / 2) * sin($dlat / 2) + cos($lat1) * cos($lat2) * sin($dlon / 2) * sin($dlon / 2);
-    $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
-
-    // Calculate the distance
-    $distance = $radius * $c;
-
-    return $distance;
-}
-
-// Example usage
-$lat1 = 30.0211563; // Latitude of location 1
-$lon1 = 31.1854662; // Longitude of location 1
-
-$lat2 = 30.012948; // Latitude of location 2
-$lon2 = 31.123689;  // Longitude of location 2
-
-$distance = calculateDistance($lat1, $lon1, $lat2, $lon2);
-echo "Distance between the two locations: " . number_format($distance, 2) . " km";
-
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 $capsule = new Capsule;
