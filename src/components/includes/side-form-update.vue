87e83c1e52b72bc904713842499010157673a647
unknown <amr_elngm6@yahoo.com>
2023-10-10 01:53:59 +0300

    .

--- src/components/includes/side-form-update.vue
+++ src/components/includes/side-form-update.vue
@@ -12,7 +12,7 @@
                 <div class="py-1 w-full" v-for="column in columns" v-if="columns ">
                     <input v-if="model_id && column && column.column_type == 'hidden'" :name="'params['+column.key+']'" type="hidden" v-model="item[column.key]">
                     <div class="w-full" v-if="column && column.fillable" >
-                        <span class="block mb-2" v-text="column.title"></span>
+                        <span class="block mb-2" v-text="column.title" v-if="column.column_type != 'hidden'"></span>
 
                         <input v-if="isInput(column.column_type)" :name="'params['+column.key+']'" :type="column.column_type" class="h-12 mb-3 rounded w-full border px-3 text-gray-700  focus:border-blue-100 dark:bg-gray-800  dark:border-gray-600" :placeholder="column.title" v-model="item[column.key]">
                     
