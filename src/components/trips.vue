a0096ce86394b4ea7534fd343610f343446380a5
unknown <amr_elngm6@yahoo.com>
2023-10-14 11:26:15 +0300

    .

--- src/components/trips.vue
+++ src/components/trips.vue
@@ -160,7 +160,7 @@ export default
             let locations = [];
             // = parseFloat(location.latitude);
             for (let i = 0; i < route.pickup_locations.length; i++) {
-                a = route.pickup_locations[i];
+                a = route.pickup_locations[i].location;
                 locations[i] = {icon: this.conf.url+'uploads/images/blue_pin.gif', origin: { lat: parseFloat(a.latitude), lng: parseFloat(a.longitude) }, destination: { lat: parseFloat(a.latitude), lng: parseFloat(a.longitude) } }
             }
             locations[locations.length] = {icon: this.conf.url+'uploads/images/car.svg', origin: { lat: parseFloat(route.vehicle.last_latitude), lng: parseFloat(route.vehicle.last_longitude) }, destination: { lat: parseFloat(route.vehicle.last_latitude), lng: parseFloat(route.vehicle.last_longitude) } }
