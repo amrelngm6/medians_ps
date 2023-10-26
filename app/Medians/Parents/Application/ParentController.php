51644ab2b51a6d1dc0e22206dcdadba6ff5e2c9c
unknown <amr_elngm6@yahoo.com>
2023-10-26 22:13:40 +0300

    .

--- app/Medians/Parents/Application/ParentController.php
+++ app/Medians/Parents/Application/ParentController.php
@@ -61,9 +61,9 @@ class ParentController extends CustomController
             [ 'key'=> "first_name", 'title'=> __('first_name'), 'sortable'=> true, 'fillable'=> true, 'column_type'=>'text' ],
             [ 'key'=> "last_name", 'title'=> __('last_name'), 'sortable'=> true, 'fillable'=>true, 'column_type'=>'text' ],
             [ 'key'=> "picture", 'title'=> __('picture'), 'sortable'=> true, 'fillable'=>true, 'column_type'=>'file' ],
-            [ 'key'=> "parent_guardian_name", 'title'=> __('parent_guardian_name'), 'sortable'=> true, 'fillable'=> true, 'column_type'=>'text' ],
-            [ 'key'=> "contact_number", 'title'=> __('contact_number'), 'sortable'=> true, 'fillable'=> true, 'column_type'=>'phone' ],
-            [ 'key'=> "date_of_birth", 'title'=> __('date_of_birth'), 'sortable'=> true, 'fillable'=> true, 'column_type'=>'date' ],
+            [ 'key'=> "email", 'title'=> __('Email'), 'sortable'=> true, 'fillable'=> true, 'column_type'=>'email' ],
+            [ 'key'=> "mobile", 'title'=> __('mobilecontact_number'), 'sortable'=> true, 'fillable'=> true, 'column_type'=>'phone' ],
+            [ 'key'=> "password", 'title'=> __('Password'), 'sortable'=> true, 'fillable'=> true, 'column_type'=>'password' ],
         ];
 	}
 
