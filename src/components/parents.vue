f778c3cebbd03b938f89b002aec747bb381de23e
unknown <amr_elngm6@yahoo.com>
2023-10-27 00:25:01 +0200

    .

--- src/components/parents.vue
+++ src/components/parents.vue
@@ -1,6 +1,6 @@
 <template>
     <div class=" w-full">
-        <data_table_page :conf="conf" :setting="setting" :auth="auth" object_name="Parents" object_key="parent_id" ></data_table_page>
+        <data_table_page :conf="conf" :path="path" :setting="setting" :auth="auth" object_name="Parents" object_key="parent_id" ></data_table_page>
     </div>
 </template>
 <script>
