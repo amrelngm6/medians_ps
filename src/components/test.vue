c833b764852c1edad6c034e96fe69b32bb9177e8
unknown <amr_elngm6@yahoo.com>
2023-11-01 03:58:20 +0200

    .

--- src/components/test.vue
+++ src/components/test.vue
@@ -94,7 +94,7 @@ userIdx: {}
             <div class="card-body p-4">
                 <h5 class="card-title mb-4">Comments</h5>
 
-                <div data-simplebar="init" style="height: 300px;" class="px-3 mx-n3 simplebar-scrollable-y"><div class="simplebar-wrapper" style="margin: 0px -16px;"><div class="simplebar-height-auto-observer-wrapper"><div class="simplebar-height-auto-observer"></div></div><div class="simplebar-mask"><div class="simplebar-offset" style="right: 0px; bottom: 0px;"><div class="simplebar-content-wrapper" tabindex="0" role="region" aria-label="scrollable content" style="height: 100%; overflow: hidden scroll;"><div class="simplebar-content" style="padding: 0px 16px;">
+                <div data-simplebar="init" style="height: 300px;" class="px-3 mx-n3 simplebar-scrollable-y"><div class="simplebar-wrapper" style="margin: 0px -16px;"><div class="simplebar-height-auto-observer-wrapper"><div class="simplebar-height-auto-observer"></div></div><div class="simplebar-mask"><div class="simplebar-offset" style="right: 0px; bottom: 0px;"><div class="simplebar-content-wrapper" tabindex="0" role="region" aria-label="scrollable content" ><div class="simplebar-content" style="padding: 0px 16px;">
                     <div class="d-flex mb-4">
                         <div class="flex-shrink-0">
                             <img src="assets/images/users/avatar-8.jpg" alt="" class="avatar-xs rounded-circle">
@@ -153,7 +153,8 @@ userIdx: {}
                             </div>
                         </div>
                     </div>
-                </div></div></div></div><div class="simplebar-placeholder" style="width: 1033px; height: 583px;"></div></div><div class="simplebar-track simplebar-horizontal" style="visibility: hidden;"><div class="simplebar-scrollbar" style="width: 0px; display: none;"></div></div><div class="simplebar-track simplebar-vertical" style="visibility: visible;"><div class="simplebar-scrollbar" style="height: 154px; transform: translate3d(0px, 0px, 0px); display: block;"></div></div></div>
+                </div></div></div></div>
+            </div><div class="simplebar-track simplebar-horizontal" style="visibility: hidden;"><div class="simplebar-scrollbar" style="width: 0px; display: none;"></div></div><div class="simplebar-track simplebar-vertical" style="visibility: visible;"><div class="simplebar-scrollbar" style="height: 154px; transform: translate3d(0px, 0px, 0px); display: block;"></div></div></div>
                 <form action="javascript:void(0);" class="mt-3">
                     <div class="row g-3">
                         <div class="col-lg-12">
