44f37220c15337f5284fe15ce1787b3e5f08fbfa
unknown <amr_elngm6@yahoo.com>
2023-11-01 12:03:14 +0200

    .

--- src/components/drivers.vue
+++ src/components/drivers.vue
@@ -3,11 +3,10 @@
         <!-- Rounded Ribbon -->
         <div class="card ribbon-box border shadow-none mb-lg-0">
             <div class="card-body">
-                <div class="ribbon ribbon-primary round-shape">Primary</div>
-                <h5 class="fs-14 text-end">Rounded Ribbon</h5>
+                <div class="ribbon ribbon-primary round-shape">Important</div>
+                <h5 class="fs-14 text-end">Add new driver</h5>
                 <div class="ribbon-content mt-4 text-muted">
-                    <p class="mb-0">Quisque nec turpis at urna dictum luctus. Suspendisse convallis dignissim eros at volutpat. In egestas
-                        mattis dui. Aliquam mattis dictum aliquet. Nulla sapien mauris, eleifend et sem ac, commodo dapibus odio.</p>
+                    <p class="mb-0">When you create new driver you should make sure that you have the right <b>Email</b>. Because the generated password will be sent to him</p>
                 </div>
             </div>
         </div>
