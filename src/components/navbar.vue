e2022db23a782557506926f586adce2c9f404f9d
unknown <amr_elngm6@yahoo.com>
2023-11-01 03:36:16 +0200

    .

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -10,7 +10,7 @@
                     </div>
                 </div>
                 <div class="lg:inline-flex no-mobile">
-                    <ul class="md:flex items-center justify-center text-base text-gray-400  pt-2  ">
+                    <ul class="md:flex items-center justify-center text-base text-gray-400  py-4  ">
                         <li>
                             <a :href="conf.url"  v-text="setting.sitename"></a>
                         </li>
