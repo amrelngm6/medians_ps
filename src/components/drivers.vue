18d1a26cd06d1eee53e43b88c503e548bf244432
unknown <amr_elngm6@yahoo.com>
2023-11-02 00:54:22 +0200

    .

--- src/components/drivers.vue
+++ src/components/drivers.vue
@@ -1,18 +1,20 @@
 <template>
     <div class=" w-full">
+
         <!-- Primary Alert -->
         <div class="alert alert-primary alert-dismissible bg-primary text-white alert-label-icon fade show" role="alert">
             <i class="ri-user-smile-line label-icon"></i><strong>Primary</strong> - Label icon alert
             <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
         <div class="alert alert-primary alert-top-border alert-dismissible fade show" role="alert">
-    <i class="ri-user-smile-line me-3 align-middle fs-16 text-primary"></i><strong>Primary</strong> - Top border alert
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
+            <i class="ri-user-smile-line me-3 align-middle fs-16 text-primary"></i><strong>Primary</strong> - Top border
+            alert
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
         <div class="relative card ribbon-box border shadow-none mb-lg-0">
             <div class="card-body">
                 <div class="ribbon ribbon-primary round-shape" v-text="__('Important')"></div>
-                <h5 class="fs-14 text-end" v-text="__('Before Create Account')"></h5>
+                <h5 class="fs-14 text-end ml-20" v-text="__('Before Create Account')"></h5>
                 <div class="ribbon-content mt-4 text-muted">
                     <div class="mb-0" v-html="__('Before Create driver note')"></div>
                 </div>
@@ -20,18 +22,18 @@
         </div>
 
         <div class="sm:grid sm:space-y-0 space-y-6 xl:!grid-cols-3 md:grid-cols-2 gap-6">
-           
+
             <div class="box mb-0 overflow-hidden p-4 bg-white rounded-xl" v-for="driver in content.items">
                 <div class="box-body space-y-5">
                     <div class="flex">
                         <div class="sm:flex sm:space-x-3 sm:space-y-0 space-y-4 rtl:space-x-reverse"><img
-                                class="avatar avatar-lg rounded-sm"
-                                :src="driver.picture"
-                                alt="Image Description">
+                                class="avatar avatar-lg rounded-sm" :src="driver.picture" alt="Image Description">
                             <div class="space-y-1 my-auto">
                                 <h5 class="font-semibold text-base leading-none" v-text="driver.name"></h5>
-                                <p class="text-gray-500 dark:text-white/70 font-semibold text-xs truncate max-w-[9rem]" v-text="driver.email"></p>
-                                <p class="text-primary dark:text-primary text-xs font-semibold" v-text="driver.contact_number"></p>
+                                <p class="text-gray-500 dark:text-white/70 font-semibold text-xs truncate max-w-[9rem]"
+                                    v-text="driver.email"></p>
+                                <p class="text-primary dark:text-primary text-xs font-semibold"
+                                    v-text="driver.contact_number"></p>
                             </div>
                         </div>
 
@@ -39,110 +41,111 @@
                 </div>
                 <div class="box-footer mt-2">
                     <div class="grid grid-cols-12 gap-x-3">
-                        <div class="sm:col-span-2 col-span-4 "><span
-                                class="cursor-pointer inline-flex !p-1 flex-shrink-0 justify-center items-center gap-2 rounded-sm border font-medium bg-white text-gray-500 shadow-sm align-middle focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-offset-white focus:ring-primary transition-all text-xs dark:bg-bgdark dark:border-white/10 dark:text-white/70 dark:focus:ring-offset-white/10"
-                                ><i
-                                    class="fa fa-phone  py-1  px-2"></i></span></div>
+                        <div class="sm:col-span-2 col-span-4 "><span @click="handleAction('view', driver)"
+                                class="cursor-pointer inline-flex !p-1 flex-shrink-0 justify-center items-center gap-2 rounded-sm border font-medium bg-white text-gray-500 shadow-sm align-middle focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-offset-white focus:ring-primary transition-all text-xs dark:bg-bgdark dark:border-white/10 dark:text-white/70 dark:focus:ring-offset-white/10"><i
+                                    class="fa fa-eye  py-1  px-2"></i></span></div>
                         <div @click="handleAction('edit', driver)" class="sm:col-span-8 col-span-4"><span
-                                class="cursor-pointer inline-flex !p-1 flex-shrink-0 justify-center items-center gap-2 w-full rounded-sm border font-medium bg-white text-gray-500 shadow-sm align-middle focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-offset-white focus:ring-primary transition-all text-xs dark:bg-bgdark dark:border-white/10 dark:text-white/70 dark:focus:ring-offset-white/10"
-                                ><i
-                                    class="fa fa-edit py-1"></i> <span
-                                    class="text-base  " v-text="__('Edit')"></span></span></div>
+                                class="cursor-pointer inline-flex !p-1 flex-shrink-0 justify-center items-center gap-2 w-full rounded-sm border font-medium bg-white text-gray-500 shadow-sm align-middle focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-offset-white focus:ring-primary transition-all text-xs dark:bg-bgdark dark:border-white/10 dark:text-white/70 dark:focus:ring-offset-white/10"><i
+                                    class="fa fa-edit py-1"></i> <span class="text-base  "
+                                    v-text="__('Edit')"></span></span></div>
                         <div class="sm:col-span-2 col-span-4">
-                            <div class="hs-dropdown ti-dropdown flex justify-end"><span @click="handleAction('delete', driver)"
+                            <div class="hs-dropdown ti-dropdown flex justify-end"><span
+                                    @click="handleAction('delete', driver)"
                                     class="cursor-pointer hs-dropdown-toggle ti-dropdown-toggle inline-flex !p-1 flex-shrink-0 justify-center items-center gap-2 rounded-sm border font-medium bg-white text-gray-500 shadow-sm align-middle focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-offset-white focus:ring-primary transition-all text-xs dark:bg-bgdark dark:border-white/10 dark:text-white/70 dark:focus:ring-offset-white/10"><i
                                         class="fa fa-trash text-danger py-1 px-2"></i></span>
-                                        </div></div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
 
-        <side-form-create :conf="conf" model="Driver.create" v-if="showAddSide && content && content.fillable" :columns="content.fillable"  class="col-md-3" />
+        <side-form-create :conf="conf" model="Driver.create" v-if="showAddSide && content && content.fillable"
+            :columns="content.fillable" class="col-md-3" />
 
-        <side-form-update :conf="conf" model="Driver.update" :item="activeItem" :model_id="activeItem.driver_id" :index="activeItem.driver_id" v-if="showEditSide && !showAddSide " :columns="content.fillable"  class="col-md-3" />
+        <side-form-update :conf="conf" model="Driver.update" :item="activeItem" :model_id="activeItem.driver_id"
+            :index="activeItem.driver_id" v-if="showEditSide && !showAddSide" :columns="content.fillable"
+            class="col-md-3" />
 
 
         <!-- <data_table_page ref="data_table" :conf="conf" :path="path" :setting="setting" :auth="auth" object_name="Driver" -->
-            <!-- object_key="driver_id"></data_table_page> -->
+        <!-- object_key="driver_id"></data_table_page> -->
     </div>
 </template>
 <script>
 
 export default
-{   
-
-    data()
-    {
-        return {
-            url: this.conf.url+this.path+'?load=json',
-            content: {
-                items:[]
-            },
-            activeItem: {},
-            showAddSide:false,
-            showEditSide:false,
-            showLoader: true,
-        }
-    },
-
-    props: [
-        'path',
-        'lang',
-        'setting',
-        'conf',
-        'auth',
-    ],
-    mounted() 
-    {
-        this.load()
-    },
-
-    methods: 
     {
 
-        /**
-         * Handle actions from datatable buttons
-         * Called From 'dataTableActions' component
-         * 
-         * @param String actionName 
-         * @param Object data
-         */  
-        handleAction(actionName, data) {
-            switch(actionName) 
-            {
-                case 'view':
-                    // window.open(this.conf.url+data.content.prefix)
-                    break;  
-
-                case 'edit':
-                    this.showEditSide = true; 
-                    this.showAddSide = false; 
-                    this.activeItem = data
-                    break;  
-
-                case 'delete':
-                    this.$root.$children[0].deleteByKey('driver_id', data, 'Driver.delete');
-                    break;  
+        data() {
+            return {
+                url: this.conf.url + this.path + '?load=json',
+                content: {
+                    items: []
+                },
+                activeItem: {},
+                showAddSide: false,
+                showEditSide: false,
+                showLoader: true,
             }
         },
 
-        load()
-        {
-            this.showLoader = true;
-            this.$root.$children[0].handleGetRequest( this.url ).then(response=> {
-                this.setValues(response)
-                this.showLoader = false;
-            });
+        props: [
+            'path',
+            'lang',
+            'setting',
+            'conf',
+            'auth',
+        ],
+        mounted() {
+            this.load()
         },
-        
-        setValues(data) {
-            this.content = JSON.parse(JSON.stringify(data)); return this
-        },
-        __(i)
+
+        methods:
         {
-            return this.$root.$children[0].__(i);
+
+            /**
+             * Handle actions from datatable buttons
+             * Called From 'dataTableActions' component
+             * 
+             * @param String actionName 
+             * @param Object data
+             */
+            handleAction(actionName, data) {
+                switch (actionName) {
+                    case 'view':
+                        this.showEditSide = false;
+                        this.showAddSide = true;
+                        this.activeItem = data
+                        // window.open(this.conf.url+data.content.prefix)
+                        break;
+
+                    case 'edit':
+                        this.showEditSide = true;
+                        this.showAddSide = false;
+                        this.activeItem = data
+                        break;
+
+                    case 'delete':
+                        this.$root.$children[0].deleteByKey('driver_id', data, 'Driver.delete');
+                        break;
+                }
+            },
+
+            load() {
+                this.showLoader = true;
+                this.$root.$children[0].handleGetRequest(this.url).then(response => {
+                    this.setValues(response)
+                    this.showLoader = false;
+                });
+            },
+
+            setValues(data) {
+                this.content = JSON.parse(JSON.stringify(data)); return this
+            },
+            __(i) {
+                return this.$root.$children[0].__(i);
+            }
         }
-    }
-};
+    };
 </script>
