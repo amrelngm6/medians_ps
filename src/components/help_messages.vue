371260650acc301f4d0a88a7050b4a6e08924c95
unknown <amr_elngm6@yahoo.com>
2023-11-01 06:28:50 +0200

    .

--- src/components/help_messages.vue
+++ src/components/help_messages.vue
@@ -38,6 +38,11 @@
                 </div>
                 <!-- end row -->
 
+                <ul class="gap-6 flex nav nav-tabs-custom border-bottom-0" role="tablist">
+                    <li v-for="option in statusList"  class="nav-item" role="presentation">
+                        <a @click="switchStatus(option)" :class="option.status == activeStatus ? 'font-bold' : ''" v-text="option.text" class="nav-link fw-semibold" data-bs-toggle="tab" href="#project-overview" role="tab" aria-selected="false" tabindex="-1"></a>
+                    </li>
+                </ul>
                 <div class="team-list list-view-filter" v-for="item in content.items" >
                     <div class="card team-box" v-if="!showLoader">
                         <div class="card-body px-4">
