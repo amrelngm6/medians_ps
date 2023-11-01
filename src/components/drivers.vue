c4da274277118912ff407f4ac81fc10bdf43745c
unknown <amr_elngm6@yahoo.com>
2023-11-01 12:02:09 +0200

    .

--- src/components/drivers.vue
+++ src/components/drivers.vue
@@ -1,5 +1,16 @@
 <template>
     <div class=" w-full">
+        <!-- Rounded Ribbon -->
+        <div class="card ribbon-box border shadow-none mb-lg-0">
+            <div class="card-body">
+                <div class="ribbon ribbon-primary round-shape">Primary</div>
+                <h5 class="fs-14 text-end">Rounded Ribbon</h5>
+                <div class="ribbon-content mt-4 text-muted">
+                    <p class="mb-0">Quisque nec turpis at urna dictum luctus. Suspendisse convallis dignissim eros at volutpat. In egestas
+                        mattis dui. Aliquam mattis dictum aliquet. Nulla sapien mauris, eleifend et sem ac, commodo dapibus odio.</p>
+                </div>
+            </div>
+        </div>
         <data_table_page ref="data_table" :conf="conf" :path="path" :setting="setting" :auth="auth" object_name="Driver" object_key="driver_id" ></data_table_page>
     </div>
 </template>
