084a78469b372c20375cc089af481aca1d700967
unknown <amr_elngm6@yahoo.com>
2023-11-02 05:01:25 +0200

    .

--- src/components/driver_profile.vue
+++ src/components/driver_profile.vue
@@ -55,17 +55,17 @@
                                 <button @click="setActiveStatus('trips')" type="button"
                                     v-text="__('Trips')"
                                     :class="activeStatus == 'trips' ? 'menu-dark text-whit font-semibolde' : 'text-gray-50'"
-                                    class="hover:bg-white hs-tab-active:font-semibold hs-tab-active:bg-white dark:hs-tab-active:bg-gray-700 w-full flex justify-center py-2 rounded items-center gap-2 border-b-2 border-transparent -mb-px transition-all text-sm whitespace-nowrap text-gray-50 dark:text-white">
+                                    class="hover:bg-white hs-tab-active:font-semibold hs-tab-active:bg-white dark:hs-tab-active:bg-gray-700 w-full flex justify-center py-2 rounded items-center gap-2 border-b-2 border-transparent -mb-px transition-all text-sm whitespace-nowrap dark:text-white">
                                 </button> <!-- button-end -->
                                 <button @click="setActiveStatus('reviews')"  type="button"
                                 v-text="__('Reviews')"
                                     :class="activeStatus == 'reviews' ? 'menu-dark text-whit font-semibolde' : 'text-gray-50'"
-                                    class="hover:bg-white hs-tab-active:font-semibold hs-tab-active:bg-white dark:hs-tab-active:bg-gray-700 w-full flex justify-center py-2 rounded items-center gap-2 border-b-2 border-transparent -mb-px transition-all text-sm whitespace-nowrap text-gray-500 dark:text-white">
+                                    class="hover:bg-white hs-tab-active:font-semibold hs-tab-active:bg-white dark:hs-tab-active:bg-gray-700 w-full flex justify-center py-2 rounded items-center gap-2 border-b-2 border-transparent -mb-px transition-all text-sm whitespace-nowrap dark:text-white">
                                 </button> <!-- button-end -->
                                 <button type="button"
                                     :class="activeStatus == 'help_messages' ? 'menu-dark text-whit font-semibolde' : 'text-gray-50'"
                                     v-text="__('Help messages')"
-                                    class="hover:bg-white hs-tab-active:font-semibold hs-tab-active:bg-white dark:hs-tab-active:bg-gray-700 w-full flex justify-center py-2 rounded items-center gap-2 border-b-2 border-transparent -mb-px transition-all text-sm whitespace-nowrap text-gray-500 dark:text-white">
+                                    class="hover:bg-white hs-tab-active:font-semibold hs-tab-active:bg-white dark:hs-tab-active:bg-gray-700 w-full flex justify-center py-2 rounded items-center gap-2 border-b-2 border-transparent -mb-px transition-all text-sm whitespace-nowrap dark:text-white">
                                 </button> <!-- button-end -->
                             </nav> <!-- nav-end -->
 
