9df6a0475e4b50d20f0c387525471e7a28a7357f
unknown <amr_elngm6@yahoo.com>
2023-11-02 05:40:59 +0200

    .

--- src/components/driver_profile.vue
+++ src/components/driver_profile.vue
@@ -62,7 +62,7 @@
                                     :class="activeStatus == 'reviews' ? 'menu-dark text-white font-semibold' : 'text-gray-500'"
                                     class="hover:bg-white hover:text-blue-800 hs-tab-active:font-semibold hs-tab-active:bg-white dark:hs-tab-active:bg-gray-700 w-full flex justify-center py-2 rounded items-center gap-2 border-b-2 border-transparent -mb-px transition-all text-sm whitespace-nowrap dark:text-white">
                                 </button> <!-- button-end -->
-                                <button type="button"
+                                <button type="button"  @click="setActiveStatus('help_messages')"
                                     :class="activeStatus == 'help_messages' ? 'menu-dark text-white font-semibold' : 'text-gray-500'"
                                     v-text="__('Help messages')"
                                     class="hover:bg-white hover:text-blue-800 hs-tab-active:font-semibold hs-tab-active:bg-white dark:hs-tab-active:bg-gray-700 w-full flex justify-center py-2 rounded items-center gap-2 border-b-2 border-transparent -mb-px transition-all text-sm whitespace-nowrap dark:text-white">
