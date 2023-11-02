88091d4578ad90e25fe774b58f4205bc500d08c3
unknown <amr_elngm6@yahoo.com>
2023-11-02 08:08:31 +0200

    .

--- app/Medians/Trips/Infrastructure/TripRepository.php
+++ app/Medians/Trips/Infrastructure/TripRepository.php
@@ -50,7 +50,7 @@ class TripRepository
 
 	public function get($limit = 100)
 	{
-		return Trip::withCount('moving_locations')->withCount('pickup_locations')->with('route', 'pickup_locations', 'waiting_locations', 'driver', 'vehicle')->limit($limit)->get();
+		return Trip::withCount('moving_locations')->withCount('pickup_locations')->with('route', 'pickup_locations', 'waiting_locations', 'driver', 'vehicle')->orderBy('trip_id', 'DESC')->limit($limit)->get();
 	}
 
 	public function eventsByDate($params)
