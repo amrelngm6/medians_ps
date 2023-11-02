0f564cf64e885328bf4f46822f329d1df1699853
unknown <amr_elngm6@yahoo.com>
2023-11-02 11:05:58 +0200

    .

--- src/components/includes/map.vue
+++ src/components/includes/map.vue
@@ -120,6 +120,7 @@ export default
             }
 
             setInterval(() => {
+                this.polylineCoordinates = []
                 t.calculateAndDisplayRoute();
             }, 1000);
             console.log(this.waypoints)
