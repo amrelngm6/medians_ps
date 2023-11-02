8b7e27c70fbdceb68e208014629adfadfb1a1a57
unknown <amr_elngm6@yahoo.com>
2023-11-02 06:18:23 +0200

    .

--- src/components/driver_profile.vue
+++ src/components/driver_profile.vue
@@ -83,7 +83,7 @@
                                                         <p class="text-title text-sm font-semibold mb-1 dark:text-white" v-text="help_message.subject"> </p>
                                                         <p class="text-content3 text-2xs truncate"  v-text="help_message.message"></p>
                                                     </div>
-                                                    <div class="flex items-center justify-between gap-1"> 
+                                                    <div class="absolute right-0  flex items-center justify-between gap-1"> 
                                                         <span class="bg-blue-50 px-4 py-1 text-secondary badge-sm rounded-5 " v-text="help_message.status"></span>
                                                     </div>
                                                 </div>
