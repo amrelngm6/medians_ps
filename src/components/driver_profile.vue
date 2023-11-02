32a3eb2bee630dd0f82dd99f288bd203f492b283
unknown <amr_elngm6@yahoo.com>
2023-11-02 05:10:04 +0200

    .

--- src/components/driver_profile.vue
+++ src/components/driver_profile.vue
@@ -49,21 +49,21 @@
                                 aria-label="Tabs" role="tablist">
                                 <button @click="setActiveStatus('info')" type="button"
                                     v-text="__('Info')"
-                                    :class="activeStatus == 'info' ? 'menu-dark text-white font-semibold' : 'text-gray-50'"
+                                    :class="activeStatus == 'info' ? 'menu-dark text-white font-semibold' : 'text-gray-500'"
                                     class="hover:bg-white hover:text-blue-800 hs-tab-active:font-semibold hs-tab-active:bg-white dark:hs-tab-active:bg-gray-700 w-full flex justify-center py-2 rounded items-center gap-2 border-b-2 border-transparent -mb-px transition-all text-sm whitespace-nowrap dark:text-white active">
                                 </button> <!-- button-end -->
                                 <button @click="setActiveStatus('trips')" type="button"
                                     v-text="__('Trips')"
-                                    :class="activeStatus == 'trips' ? 'menu-dark text-white font-semibold' : 'text-gray-50'"
+                                    :class="activeStatus == 'trips' ? 'menu-dark text-white font-semibold' : 'text-gray-500'"
                                     class="hover:bg-white hover:text-blue-800 hs-tab-active:font-semibold hs-tab-active:bg-white dark:hs-tab-active:bg-gray-700 w-full flex justify-center py-2 rounded items-center gap-2 border-b-2 border-transparent -mb-px transition-all text-sm whitespace-nowrap dark:text-white">
                                 </button> <!-- button-end -->
                                 <button @click="setActiveStatus('reviews')"  type="button"
                                 v-text="__('Reviews')"
-                                    :class="activeStatus == 'reviews' ? 'menu-dark text-white font-semibold' : 'text-gray-50'"
+                                    :class="activeStatus == 'reviews' ? 'menu-dark text-white font-semibold' : 'text-gray-500'"
                                     class="hover:bg-white hover:text-blue-800 hs-tab-active:font-semibold hs-tab-active:bg-white dark:hs-tab-active:bg-gray-700 w-full flex justify-center py-2 rounded items-center gap-2 border-b-2 border-transparent -mb-px transition-all text-sm whitespace-nowrap dark:text-white">
                                 </button> <!-- button-end -->
                                 <button type="button"
-                                    :class="activeStatus == 'help_messages' ? 'menu-dark text-white font-semibold' : 'text-gray-50'"
+                                    :class="activeStatus == 'help_messages' ? 'menu-dark text-white font-semibold' : 'text-gray-500'"
                                     v-text="__('Help messages')"
                                     class="hover:bg-white hover:text-blue-800 hs-tab-active:font-semibold hs-tab-active:bg-white dark:hs-tab-active:bg-gray-700 w-full flex justify-center py-2 rounded items-center gap-2 border-b-2 border-transparent -mb-px transition-all text-sm whitespace-nowrap dark:text-white">
                                 </button> <!-- button-end -->
