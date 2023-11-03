8f7fe0911b5d78d8b8ebbf910327f0b9c514f6b7
unknown <amr_elngm6@yahoo.com>
2023-11-03 06:22:01 +0200

    .

--- src/components/routes.vue
+++ src/components/routes.vue
@@ -160,9 +160,9 @@ export default
         
         updateMarker(item, event)
         {
-            console.log(this.activeItem)
-            console.log(item)
-            console.log(event)
+            this.activeItem.latitude = item.destination.lat;
+            this.activeItem.longitude = item.destination.lng;
+            this.handleAction('edit', this.activeItem);
         },
 
         setLocationsPickups(route)
