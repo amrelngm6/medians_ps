918816fa67214d0be0a6a37c409d96836cbde9c3
unknown <amr_elngm6@yahoo.com>
2023-11-05 11:00:27 +0200

    .

--- src/App.vue
+++ src/App.vue
@@ -305,7 +305,4 @@ export default {
 @import './assets/theme.css';
 @import './assets/plugins.css';
 
-import 'quill/dist/quill.core.css'
-import 'quill/dist/quill.snow.css'
-import 'quill/dist/quill.bubble.css'
 </style>
