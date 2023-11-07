b3b7a148fc256bdeb4d3ef8fccdb7adc61019b47
unknown <amr_elngm6@yahoo.com>
2023-11-07 02:45:46 +0200

    .

--- app/Shared/OneSignal/OneSignalService.php
+++ app/Shared/OneSignal/OneSignalService.php
@@ -66,7 +66,7 @@ class OneSignalService
             // 'include_external_user_ids' => [$this->user_onesignal_id],
             // 'data' => $receiver,
             'target_channel' => 'push',
-            'include_aliases' => ['external_id'=>['D-23', '23']]
+            'include_aliases' => ['external_id'=>['D-23']]
         );
         
 
