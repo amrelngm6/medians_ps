457dc12ad51cf6ca2a0c63cfba6ec65742ae0962
unknown <amr_elngm6@yahoo.com>
2023-11-07 03:47:50 +0200

    .

--- app/Medians/Help/Application/HelpMessageController.php
+++ app/Medians/Help/Application/HelpMessageController.php
@@ -180,8 +180,6 @@ class HelpMessageController extends CustomController
 
         try {	
 
-        	$params['created_by'] = $this->app->auth()->driver_id;
-
             $returnData = (!empty($this->repo->store($params))) 
             ? array('success'=>1, 'result'=>__('THNKS_MSG'), 'reload'=>1)
             : array('success'=>0, 'result'=>'Error', 'error'=>1);
