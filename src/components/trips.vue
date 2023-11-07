664940b9150ef566b2a0d11d868870f49a7e4e25
unknown <amr_elngm6@yahoo.com>
2023-11-07 10:35:47 +0200

    .

--- src/components/trips.vue
+++ src/components/trips.vue
@@ -7,7 +7,7 @@
             
         <div  v-if="content && !showTrip " class=" w-full relative">
 
-            <maps :conf="conf" :key="locations" :center="center" @click-marker="clickMarker" @update-marker="updateMarker" :showroute="false" :waypoints="locations" @interval-callback="callback"></maps>
+            <maps v-if="center" :conf="conf" :key="locations" :center="center" @click-marker="clickMarker" @update-marker="updateMarker" :showroute="false" :waypoints="locations" @interval-callback="callback"></maps>
 
             <div style="max-height:calc(100vh - 140px)" class="h-full absolute top-4 rounded-lg p-4   bg-white rounded-xl flex-col justify-start items-start inline-flex">
                 <div class="self-stretch py-4 flex-col justify-center items-start flex">
@@ -147,8 +147,6 @@ export default
         getUserLocation() 
         {
             this.center = this.activeTrip ? this.locations[0].destination : {}; 
-            console.log('aa')
-            console.log(this.center)
             if (navigator.geolocation) {
 
                 navigator.geolocation.getCurrentPosition(
