6b33267dfd82aff7f87d6e0ef77df970d0a16b6c
unknown <amr_elngm6@yahoo.com>
2023-11-08 06:11:44 +0200

    .

--- src/components/routes.vue
+++ src/components/routes.vue
@@ -43,8 +43,9 @@
                     
                 </div>
                 <div
-                    class="self-stretch grow shrink basis-0  justify-between items-center inline-flex">
+                    class="flex self-stretch grow shrink basis-0  justify-between items-center inline-flex">
                     <div class="menu-dark rounded-lg text-white text-xs font-medium px-4 py-3 uppercase cursor-pointer" @click="showLoader = true, showAddSide = true,activeItem = {}, showLoader = false; " v-text="__('add new')"></div>
+                    <div class="">1</div>
                 </div>
             </div>
 
