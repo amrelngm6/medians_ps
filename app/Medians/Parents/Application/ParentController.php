493ee96ad05259c5f8fda57cb3712cc680e01195
unknown <amr_elngm6@yahoo.com>
2023-11-08 14:04:53 +0200

    .

--- app/Medians/Parents/Application/ParentController.php
+++ app/Medians/Parents/Application/ParentController.php
@@ -115,12 +115,12 @@ class ParentController extends CustomController
 		$token = $Auth->encrypt(strtotime(date('YmdHis')).$checkLogin->parent_id);
 		$generateToken = $checkLogin->insertCustomField('API_token', $token);
 
-		echo json_encode(
+		return
 		[
 			'success'=>true, 
 			'parent_id'=> isset($checkLogin->parent_id) ? $checkLogin->parent_id : null, 
 			'token'=>$generateToken->value
-		]);
+		];
 	}  
 
 
