1c950e25aac79fb6af121b45ad734d83f4379e1d
unknown <amr_elngm6@yahoo.com>
2023-11-17 02:37:09 +0200

    .

---
+++ app/Medians/Locations/Infrastructure/DestinationRepository.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace Medians\Locations\Infrastructure;
+
+use Medians\Locations\Domain\Destination;
+use Medians\CustomFields\Domain\CustomField;
+
+
+class DestinationRepository 
+{
+
+
+	/**
+	 * Load app for Sessions and helpful
+	 * methods for authentication and
+	 * settings for branch
+	 */ 
+	protected $app ;
+
+
+	function __construct()
+	{
+	}
+
+
+	public static function getModel()
+	{
+		return new Destination();
+	}
+
+
+	public function find($id)
+	{
+		return Destination::find($id);
+	}
+
+	public function get($limit = 100)
+	{
+		return Destination::limit($limit)->get();
+	}
+
+	public function search($request, $limit = 20)
+	{
+		$title = $request->get('search');
+		$arr =  json_decode(json_encode(['Destination'=>0, 'content'=>['title'=>$title ? $title : '-']]));
+
+		return $this->similar( $arr, $limit);
+	}
+
+
+	public function similar($item, $limit = 3)
+	{
+		$title = str_replace([' ','-'], '%', $item->content->title);
+
+		return Destination::whereHas('content', function($q) use ($title){
+			foreach (explode('%', $title) as $i) {
+				$q->where('title', 'LIKE', '%'.$i.'%')->orWhere('content', 'LIKE', '%'.$i.'%');
+			}
+		})
+		->with('category', 'content','user')->limit($limit)->orderBy('updated_at', 'DESC')->get();
+	}
+
+
+
+	/**
+	* Save item to database
+	*/
+	public function store($data) 
+	{
+
+		$Model = new Destination();
+		
+		foreach ($data as $key => $value) 
+		{
+			if (in_array($key, $this->getModel()->getFields()))
+			{
+				$dataArray[$key] = $value;
+			}
+		}		
+
+		// Return the FBUserInfo object with the new data
+    	$Object = Destination::create($dataArray);
+
+    	// Store Custom fields
+		if (isset($data['field']))
+	    	$this->storeCustomFields($data['field'], $Object->id);
+
+    	return $Object;
+    }
+    	
+
+    	
+    /**
+     * Update Lead
+     */
+    public function update($data)
+    {
+
+		$Object = Destination::find($data['destination_id']);
+		
+		// Return the FBUserInfo object with the new data
+    	$Object->update( (array) $data);
+
+    	// Store Custom fields
+    	!empty($data['field']) ? $this->storeCustomFields($data['field'], $data['destination_id']) : '';
+
+    	return $Object;
+
+    }
+
+
+	/**
+	* Delete item to database
+	*
+	* @Returns Boolen
+	*/
+	public function delete($id) 
+	{
+		try {
+			
+			$delete = Destination::find($id)->delete();
+
+			if ($delete){
+				$this->storeCustomFields(null, $id);
+			}
+
+			return true;
+
+		} catch (\Exception $e) {
+
+			throw new \Exception("Error Processing Request " . $e->getMessage(), 1);
+			
+		}
+	}
+
+
+
+	/**
+	* Save related items to database
+	*/
+	public function storeCustomFields($data, $id) 
+	{
+		CustomField::where('model_type', Destination::class)->where('model_id', $id)->delete();
+		if ($data)
+		{
+			foreach ($data as $key => $value)
+			{
+				$fields = [];
+				$fields['model_type'] = Destination::class;	
+				$fields['model_id'] = $id;	
+				$fields['code'] = $key;	
+				$fields['value'] = $value;
+
+				$Model = CustomField::create($fields);
+				$Model->update($fields);
+			}
+	
+			return $Model;		
+		}
+	}
+
+
+ 
+}
