8f47b5f357882e6180627991b195b3d0d477f330
unknown <amr_elngm6@yahoo.com>
2023-11-17 03:12:44 +0200

    .

--- app/Medians/Roles/Infrastructure/RoleRepository.php
+++ app/Medians/Roles/Infrastructure/RoleRepository.php
@@ -20,5 +20,53 @@ class RoleRepository
 		return Role::limit($limit)->get();
 	}
 
+
+	/**
+	* Save item to database
+	*/
+	public function store($data) 
+	{
+		$Model = new Role();
+		
+		$dataArray = array('name'=>$data['name']);
+		
+		// Return the FBUserInfo object with the new data
+    	$Object = Role::create($dataArray);
+
+    	return $Object;
+    }
+    	
+    /**
+     * Update Lead
+     */
+    public function update($data)
+    {
+
+		$Object = Role::find($data['id']);
+		
+    	$Object->update( (array) $data);
+
+    	return $Object;
+    }
+
+
+	/**
+	* Delete item to database
+	*
+	* @Returns Boolen
+	*/
+	public function delete($id) 
+	{
+		try {
+			
+			$delete = Student::find($id)->delete();
+			return true;
+
+		} catch (\Exception $e) {
+			throw new \Exception("Error Processing Request " . $e->getMessage(), 1);
+		}
+	}
+
+
  
 }
