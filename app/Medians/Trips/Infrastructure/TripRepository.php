3fad2e57663cccfe1d6801bd02878fbf86edfe19
unknown <amr_elngm6@yahoo.com>
2023-11-20 00:48:46 +0300

    .

--- app/Medians/Trips/Infrastructure/TripRepository.php
+++ app/Medians/Trips/Infrastructure/TripRepository.php
@@ -53,7 +53,7 @@ class TripRepository
 			'pickup_locations', function($q) use ($id){
 				$q->where('model_id', $id);
 			}
-		)->orderBy('trip_id','DESC')->limit(10)->get();
+		)->orderBy('trip_id','DESC')->limit(10)->paginate()->items();
 	}
 
 	public function get($limit = 100)
