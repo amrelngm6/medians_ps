7a98749f37aebe4e27125cbecf4005eeb5c8e921
unknown <amr_elngm6@yahoo.com>
2023-11-23 23:58:25 +0200

    .

--- app/Medians/Trips/Domain/TripPickup.php
+++ app/Medians/Trips/Domain/TripPickup.php
@@ -67,7 +67,7 @@ class TripPickup extends CustomModel
 
 	public function parent() 
 	{
-		return $this->hasOneThrough(Parents::class, Student::class, 'parent_id', 'model_id', 'parent_id', 'student_id');	
+		return $this->hasOneThrough(Parents::class, Student::class, 'model_id', 'parent_id', 'student_id', 'parent_id');	
 	}
 
 
