bc7487d7e1be018e251b3fd9fb022414eff8457e
unknown <amr_elngm6@yahoo.com>
2024-01-01 15:36:41 +0200

    .

--- src/components/trip_page.vue
+++ src/components/trip_page.vue
@@ -242,9 +242,9 @@ export default
                 {
                     for (let i = 0; i < props.trip.pickup_locations.length; i++) {
                         icon1 = props.trip.pickup_locations[i].status != 'waiting' ? 'yellow_pin.gif' : 'blue_pin.gif';
-                        icon2 = props.trip.destinations[i].status != 'waiting'  ? 'yellow_pin.gif' : 'blue_pin.gif';
                         locationsList.push(handlePickup(props.trip.pickup_locations[i], props.trip.destinations[i], icon1));
-
+                        
+                        icon2 = (props.props.trip.destinations[i] && trip.destinations[i].status != 'waiting')  ? 'yellow_pin.gif' : 'blue_pin.gif';
                         props.trip.destinations[i] ?? locationsList.push(handlePickup(props.trip.destinations[i], props.trip.pickup_locations[i], icon2));
                     }
                 }
