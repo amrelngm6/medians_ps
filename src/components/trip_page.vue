cc6d8545fb44995cc2026c207c089b0a4ee51a2c
unknown <amr_elngm6@yahoo.com>
2024-01-01 15:33:40 +0200

    .

--- src/components/trip_page.vue
+++ src/components/trip_page.vue
@@ -238,11 +238,14 @@ export default
                 let icon1, icon2;
                 console.log(props.trip)
 
-                for (let i = 0; i < props.trip.pickup_locations.length; i++) {
-                    icon1 = props.trip.pickup_locations[i].status != 'waiting' ? 'yellow_pin.gif' : 'blue_pin.gif';
-                    icon2 = props.trip.destinations[i].status != 'waiting'  ? 'yellow_pin.gif' : 'blue_pin.gif';
-                    locationsList.push(handlePickup(props.trip.pickup_locations[i], props.trip.destinations[i], icon1));
-                    locationsList.push(handlePickup(props.trip.destinations[i], props.trip.pickup_locations[i], icon2));
+                if (props.trip.pickup_locations)
+                {
+                    for (let i = 0; i < props.trip.pickup_locations.length; i++) {
+                        icon1 = props.trip.pickup_locations[i].status != 'waiting' ? 'yellow_pin.gif' : 'blue_pin.gif';
+                        icon2 = props.trip.destinations[i].status != 'waiting'  ? 'yellow_pin.gif' : 'blue_pin.gif';
+                        locationsList.push(handlePickup(props.trip.pickup_locations[i], props.trip.destinations[i], icon1));
+                        locationsList.push(handlePickup(props.trip.destinations[i], props.trip.pickup_locations[i], icon2));
+                    }
                 }
 
                 locationsList.push(handlePickup(props.trip.route, props.trip.vehicle, 'destination.svg'));
