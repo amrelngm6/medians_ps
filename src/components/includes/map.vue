54371c54efb57cf5f3aaffb434faac1d70129dbd
unknown <amr_elngm6@yahoo.com>
2024-01-01 09:25:27 +0200

    .

--- src/components/includes/map.vue
+++ src/components/includes/map.vue
@@ -53,211 +53,124 @@ import { ref } from "vue";
 import { GoogleMap,  CustomMarker, InfoWindow , Marker,Polyline } from "vue3-google-map";
 
 export default
+{
+    components: {
+        GoogleMap, 
+        Marker,
+        Polyline,
+        InfoWindow,
+        CustomMarker 
+        // DirectionsRenderer,
+    },
+    name: 'Map',
+    emits:[
+        'click-marker',
+        'update-marker',
+    ],
+    setup(props, {emit}) 
     {
-        components: {
-            GoogleMap, 
-            Marker,
-            Polyline,
-            InfoWindow,
-            CustomMarker 
-            // DirectionsRenderer,
-        },
-        name: 'Map',
-        emits:[
-            'click-marker',
-            'update-marker',
-        ],
-        setup(props, {emit}) 
-        {
-            console.log(props.waypoints);
+        console.log(props.waypoints);
 
 
-            const reload = ref(null);
-            const render = ref(null);
-            const showDrag = ref(null);
-            const travelMode = ref('Driving');
-            const zoom = ref(14);
-            const markers = ref([]);
-            const polylineCoordinates = ref([]);
-            const directionPoints = ref({});
-            const activeDestination = ref({});
-            const activeMarkerIndex = ref({});
-            
-            const  enableDrag = (marker, i) =>  {
-                showDrag.value = true
-                checkMarker(marker, i)
-            }
-            
-            
-            const  checkMarker = async (marker, i ) =>  {
-                activeMarkerIndex.value = i;
-                emit('click-marker', props.waypoints[i], i, JSON.parse(JSON.stringify(marker)));
-            }
-            
-            
-            const  updateMarker = async (event) =>  {
-                let newObject = props.waypoints[activeMarkerIndex.value]
-                newObject.latitude = event.latLng.lat()
-                newObject.longitude = event.latLng.lng()
-                newObject.address = await handlePositionToPlaceId(newObject.latitude, newObject.longitude);
+        const reload = ref(null);
+        const render = ref(null);
+        const showDrag = ref(null);
+        const travelMode = ref('Driving');
+        const zoom = ref(14);
+        const markers = ref([]);
+        const polylineCoordinates = ref([]);
+        const directionPoints = ref({});
+        const activeDestination = ref({});
+        const activeMarkerIndex = ref({});
+        
+        const  enableDrag = (marker, i) =>  {
+            showDrag.value = true
+            checkMarker(marker, i)
+        }
+        
+        
+        const  checkMarker = async (marker, i ) =>  {
+            activeMarkerIndex.value = i;
+            emit('click-marker', props.waypoints[i], i, JSON.parse(JSON.stringify(marker)));
+        }
+        
+        
+        const  updateMarker = async (event) =>  {
+            let newObject = props.waypoints[activeMarkerIndex.value]
+            newObject.latitude = event.latLng.lat()
+            newObject.longitude = event.latLng.lng()
+            newObject.address = await handlePositionToPlaceId(newObject.latitude, newObject.longitude);
 
-                props.waypoints[activeMarkerIndex.value].destination = {lat: newObject.latitude, lng: newObject.longitude};
+            props.waypoints[activeMarkerIndex.value].destination = {lat: newObject.latitude, lng: newObject.longitude};
 
-                showDrag.value = null;
-                emit('update-marker', newObject);
-            }
-            
-            
-            const  getPlaceIdFromPosition = async (lat, lng) => {
-                const geocoder = new google.maps.Geocoder();
+            showDrag.value = null;
+            emit('update-marker', newObject);
+        }
+        
+        
+        const  getPlaceIdFromPosition = async (lat, lng) => {
+            const geocoder = new google.maps.Geocoder();
 
-                const latLng = { lat: parseFloat(lat), lng: parseFloat(lng) };
+            const latLng = { lat: parseFloat(lat), lng: parseFloat(lng) };
 
-                return new Promise((resolve, reject) => {
-                    geocoder.geocode({ location: latLng }, (results, status) => {
-                    if (status === 'OK') {
-                        results[0] ? resolve(results[0]) : reject('No results found');
-                    } else {
-                        reject(`Geocoder failed due to: ${status}`);
-                    }
-                    });
-                });
-            }
-
-            const handlePositionToPlaceId = async (lat, lng) => {
-
-                try {
-                    const place = await getPlaceIdFromPosition(lat, lng);
-                    return place.formatted_address;
-                    // Perform actions with the retrieved placeId
-                } catch (error) {
-                    console.error(error);
-                    // Handle error if geocoding fails
+            return new Promise((resolve, reject) => {
+                geocoder.geocode({ location: latLng }, (results, status) => {
+                if (status === 'OK') {
+                    results[0] ? resolve(results[0]) : reject('No results found');
+                } else {
+                    reject(`Geocoder failed due to: ${status}`);
                 }
+                });
+            });
+        }
+
+        const handlePositionToPlaceId = async (lat, lng) => {
+
+            try {
+                const place = await getPlaceIdFromPosition(lat, lng);
+                return place.formatted_address;
+                // Perform actions with the retrieved placeId
+            } catch (error) {
+                console.error(error);
+                // Handle error if geocoding fails
             }
-            
-            const  onMarkerDragStart =  (i) =>  {
-                activeMarkerIndex.value = i
-            }
-            
-                    
-            return {
-                checkMarker,
-                onMarkerDragStart,
-                enableDrag,
-                updateMarker,
-                reload,
-                render,
-                showDrag,
-                travelMode,
-                origin: { lat: 0, lng: 0 }, // Replace with your origin location
-                destination: { lat: 0, lng: 0 }, // Replace with your destination location
-                zoom,
-                markers: props.waypoints,
-                polylineCoordinates,
-                activeMarkerIndex,
-                directionPoints,
-                activeDestination,
-            }
-        },
+        }
+        
+        const  onMarkerDragStart =  (i) =>  {
+            activeMarkerIndex.value = i
+        }
+        
+                
+        return {
+            checkMarker,
+            onMarkerDragStart,
+            enableDrag,
+            updateMarker,
+            reload,
+            render,
+            showDrag,
+            travelMode,
+            origin: { lat: 0, lng: 0 }, // Replace with your origin location
+            destination: { lat: 0, lng: 0 }, // Replace with your destination location
+            zoom,
+            markers: props.waypoints,
+            polylineCoordinates,
+            activeMarkerIndex,
+            directionPoints,
+            activeDestination,
+        }
+    },
 
-        props: [
-            'path',
-            'lang',
-            'setting',
-            'conf',
-            'auth',
-            'waypoints',
-            'showroute',
-            'center',
-        ],
+    props: [
+        'path',
+        'lang',
+        'setting',
+        'conf',
+        'auth',
+        'waypoints',
+        'showroute',
+        'center',
+    ],
 
-        // methods:
-        // {
-
-        //     /**
-        //      * When map loaded is ready
-        //      * 
-        //      * @param
-        //      */
-            
-        //     calculateDistance(location1, location2) {
-        //         const R = 6371; // Radius of the Earth in kilometers
-        //         const lat1 = location1.lat;
-        //         const lon1 = location1.lng;
-        //         const lat2 = location2.lat;
-        //         const lon2 = location2.lng;
-
-        //         const dLat = (lat2 - lat1) * (Math.PI / 180);
-        //         const dLon = (lon2 - lon1) * (Math.PI / 180);
-
-        //         const a =
-        //             Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        //             Math.cos(lat1 * (Math.PI / 180)) * Math.cos(lat2 * (Math.PI / 180)) *
-        //             Math.sin(dLon / 2) * Math.sin(dLon / 2);
-
-        //         const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        //         const distance = R * c; // Distance in kilometers
-
-        //         return distance;
-        //     },
-
-        //     addPoint(event, status) {
-        //         if (status == 'waiting')
-        //         {
-        //             this.polylineCoordinates.push(event);
-        //         }
-        //         // Sorting the polylineCoordinates array based on distance from a specific location
-        //         this.polylineCoordinates.sort((a, b) => {
-        //             const locationData = props.waypoints[0];
-        //             const distanceA = this.calculateDistance(locationData, a);
-        //             const distanceB = this.calculateDistance(locationData, b);
-
-        //             return distanceA - distanceB;
-        //         });
-
-        //     },
-        //     setMarker(i) { this.activeMarkerIndex = i; },
-        //     updateDestination(event) {
-        //         this.destination = {
-        //             lat: event.latLng.lat(),
-        //             lng: event.latLng.lng(),
-        //         };
-        //     },
-        //     updateOrigin(event) {
-        //         this.origin = {
-        //             lat: event.latLng.lat(),
-        //             lng: event.latLng.lng(),
-        //         };
-        //     },
-            
-            
-        //      calculateAndDisplayRoute() {
-        //         var t = this;
-        //         if (window.google && props.waypoints.length && this.showroute) 
-        //         {
-        //             t.directionPoints = {origin: props.waypoints[0].destination, destination:props.waypoints[1].destination};
-        //             var {origin, destination} = t.directionPoints;
-                    
-        //             t.directionsService.route(
-        //                 {
-        //                     origin: new window.google.maps.LatLng(origin.lat, origin.lng),
-        //                     destination: this.activeDestination ? this.activeDestination : new window.google.maps.LatLng(destination.lat, destination.lng),
-        //                     travelMode: 'DRIVING'
-        //                 },
-        //                 (response, status) => {
-        //                     if (status === 'OK') {
-        //                         t.directionsDisplay.setDirections(response);
-        //                     } else {
-        //                         console.warn('Directions request failed due to ' + status);
-        //                     }
-        //                 }
-        //             );
-        //         }
-        //     },
-
-
-
-        // }
-    };
+};
 </script>
--- src/components/includes/trip_map.vue
+++ src/components/includes/trip_map.vue
@@ -35,108 +35,233 @@
 </template>
 <script>
 
-// import DirectionsRenderer from "./map_direction.vue";
+import { ref } from "vue";
+import { GoogleMap,  CustomMarker, InfoWindow , Marker,Polyline } from "vue3-google-map";
 
 export default
+{
+    components: {
+        GoogleMap, 
+        Marker,
+        Polyline,
+        InfoWindow,
+        CustomMarker 
+        // DirectionsRenderer,
+    },
+    name: 'Map',
+    emits:[
+        'click-marker',
+        'update-marker',
+    ],
+    setup(props, {emit}) 
     {
-        components: {
-            // DirectionsRenderer,
-        },
-        name: 'Trip Map',
-        data() {
-            return {
-                
-                modes: ['DRIVING', 'WALKING'],
-                activeMarkerIndex: 0,
-                reload: true,
-                render: true,
-                travelMode: 'DRIVING',
-                origin: { lat: 0, lng: 0 }, // Replace with your origin location
-                destination: { lat: 0, lng: 0 }, // Replace with your destination location
-                
-                zoom: 12,
-                markers: [
-                ],
-                
-                center: { lat: 0, lng: 0 }, 
-                waypoints: [],
-                polylineCoordinates: [],
-                directionPoints: {},
-                activeDestination: {},
-            }
+        console.log(props.waypoints);
 
-        },
 
-        computed: {
+        const reload = ref(null);
+        const render = ref(null);
+        const showDrag = ref(null);
+        const travelMode = ref('Driving');
+        const zoom = ref(14);
+        const markers = ref([]);
+        const polylineCoordinates = ref([]);
+        const directionPoints = ref({});
+        const activeDestination = ref({});
+        const activeMarkerIndex = ref({});
+        
+        const  enableDrag = (marker, i) =>  {
+            showDrag.value = true
+            checkMarker(marker, i)
+        }
+        
+        
+        const  checkMarker = async (marker, i ) =>  {
+            activeMarkerIndex.value = i;
+            emit('click-marker', props.waypoints[i], i, JSON.parse(JSON.stringify(marker)));
+        }
+        
+        
+        const  updateMarker = async (event) =>  {
+            let newObject = props.waypoints[activeMarkerIndex.value]
+            newObject.latitude = event.latLng.lat()
+            newObject.longitude = event.latLng.lng()
+            newObject.address = await handlePositionToPlaceId(newObject.latitude, newObject.longitude);
 
-        },
-        props: [
-            'path',
-            'lang',
-            'setting',
-            'conf',
-            'auth',
-            'trip',
-            'showroute',
-            // 'waypoints',
-            // 'center',
-        ],
-        mounted() {
-            var t = this;
-            setTimeout(function(){
-                t.onMapReady()
-            }, 1000)
-        },
+            props.waypoints[activeMarkerIndex.value].destination = {lat: newObject.latitude, lng: newObject.longitude};
 
-        methods:
-        {
+            showDrag.value = null;
+            emit('update-marker', newObject);
+        }
+        
+        
+        const  getPlaceIdFromPosition = async (lat, lng) => {
+            const geocoder = new google.maps.Geocoder();
 
-            /**
-             * When map loaded is ready
-             * 
-             * @param
-             */
-            onMapReady()
-            {
-                var t = this;
-                t.updateMarkers(this.trip.vehicle);
+            const latLng = { lat: parseFloat(lat), lng: parseFloat(lng) };
 
-                setInterval(() => {
-                    t.updateVehicle()
-                }, 10000);
-            },
-
-            updateVehicle()
-            {   
-                var t = this;
-                let url = this.conf ? (this.conf.url + "vehicle/"+this.trip.vehicle_id) : ''
-                this.$root.$children[0].handleGetRequest( url ).then(response=> {
-                    t.updateMarkers(response);
+            return new Promise((resolve, reject) => {
+                geocoder.geocode({ location: latLng }, (results, status) => {
+                if (status === 'OK') {
+                    results[0] ? resolve(results[0]) : reject('No results found');
+                } else {
+                    reject(`Geocoder failed due to: ${status}`);
+                }
                 });
-            },
+            });
+        }
 
-            /**
-             * Set markers and update 
-             * if got new location
-             */
-            updateMarkers(item)
-            {
-                this.center = {lat: parseFloat(item.latitude), lng: parseFloat(item.longitude)}
-                this.waypoints = this.trip.locations;
-                this.waypoints[0].origin = this.center;
-            },
-            
+        const handlePositionToPlaceId = async (lat, lng) => {
 
-            async checkMarker(i)  {
-                this.activeDestination = this.waypoints[i].destination;
-                this.$emit('click-marker', this.waypoints[i], i);
-            },
-            
-            
-
-            __(i) {
-                return this.$root.$children[0].__(i);
+            try {
+                const place = await getPlaceIdFromPosition(lat, lng);
+                return place.formatted_address;
+                // Perform actions with the retrieved placeId
+            } catch (error) {
+                console.error(error);
+                // Handle error if geocoding fails
             }
         }
-    };
+        
+        const  onMarkerDragStart =  (i) =>  {
+            activeMarkerIndex.value = i
+        }
+        
+                
+        return {
+            checkMarker,
+            onMarkerDragStart,
+            enableDrag,
+            updateMarker,
+            reload,
+            render,
+            showDrag,
+            travelMode,
+            origin: { lat: 0, lng: 0 }, // Replace with your origin location
+            destination: { lat: 0, lng: 0 }, // Replace with your destination location
+            zoom,
+            markers: props.waypoints,
+            polylineCoordinates,
+            activeMarkerIndex,
+            directionPoints,
+            activeDestination,
+        }
+    },
+
+    props: [
+        'path',
+        'lang',
+        'setting',
+        'conf',
+        'auth',
+        'trip',
+        'waypoints',
+        'showroute',
+        'center',
+    ]
+
+};
+
+
+// export default
+//     {
+//         components: {
+//             // DirectionsRenderer,
+//         },
+//         name: 'Trip Map',
+//         data() {
+//             return {
+                
+//                 modes: ['DRIVING', 'WALKING'],
+//                 activeMarkerIndex: 0,
+//                 reload: true,
+//                 render: true,
+//                 travelMode: 'DRIVING',
+//                 origin: { lat: 0, lng: 0 }, // Replace with your origin location
+//                 destination: { lat: 0, lng: 0 }, // Replace with your destination location
+                
+//                 zoom: 12,
+//                 markers: [
+//                 ],
+                
+//                 center: { lat: 0, lng: 0 }, 
+//                 waypoints: [],
+//                 polylineCoordinates: [],
+//                 directionPoints: {},
+//                 activeDestination: {},
+//             }
+
+//         },
+
+//         computed: {
+
+//         },
+//         props: [
+//             'path',
+//             'lang',
+//             'setting',
+//             'conf',
+//             'auth',
+//             'trip',
+//             'showroute',
+//             // 'waypoints',
+//             // 'center',
+//         ],
+//         mounted() {
+//             var t = this;
+//             setTimeout(function(){
+//                 t.onMapReady()
+//             }, 1000)
+//         },
+
+//         methods:
+//         {
+
+//             /**
+//              * When map loaded is ready
+//              * 
+//              * @param
+//              */
+//             onMapReady()
+//             {
+//                 var t = this;
+//                 t.updateMarkers(this.trip.vehicle);
+
+//                 setInterval(() => {
+//                     t.updateVehicle()
+//                 }, 10000);
+//             },
+
+//             updateVehicle()
+//             {   
+//                 var t = this;
+//                 let url = this.conf ? (this.conf.url + "vehicle/"+this.trip.vehicle_id) : ''
+//                 this.$root.$children[0].handleGetRequest( url ).then(response=> {
+//                     t.updateMarkers(response);
+//                 });
+//             },
+
+//             /**
+//              * Set markers and update 
+//              * if got new location
+//              */
+//             updateMarkers(item)
+//             {
+//                 this.center = {lat: parseFloat(item.latitude), lng: parseFloat(item.longitude)}
+//                 this.waypoints = this.trip.locations;
+//                 this.waypoints[0].origin = this.center;
+//             },
+            
+
+//             async checkMarker(i)  {
+//                 this.activeDestination = this.waypoints[i].destination;
+//                 this.$emit('click-marker', this.waypoints[i], i);
+//             },
+            
+            
+
+//             __(i) {
+//                 return this.$root.$children[0].__(i);
+//             }
+//         }
+//     };
 </script>
