35709259c99d6df2c6b810c4ea18cc27f9619d6b
unknown <amr_elngm6@yahoo.com>
2024-01-01 13:06:04 +0200

    .

--- src/components/includes/side-form-create.vue
+++ src/components/includes/side-form-create.vue
@@ -33,7 +33,7 @@
                     
                     <vue-medialibrary-field :key="item" v-if="column.column_type == 'file'" :name="'params['+column.key+']'"  v-model="item.picture" :api_url="conf.url"></vue-medialibrary-field>
 
-                    <vue-medialibrary-field :key="item" v-if="column.column_type == 'profile_image' " :name="'params['+column.key+']'" filepath="" v-model="file" :api_url="conf.url"></vue-medialibrary-field>
+                    <vue-medialibrary-field :key="item" v-if="column.column_type == 'profile_image' " :name="'params['+column.key+']'" :api_url="conf.url"></vue-medialibrary-field>
 
                 </div>
 
