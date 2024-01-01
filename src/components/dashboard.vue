a7e281cda00a3a3a70eece56ddd72733e1db4431
unknown <amr_elngm6@yahoo.com>
2024-01-01 17:10:51 +0200

    .

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -191,6 +191,20 @@ export default
         }
 
 
+        const optionsbar = ref({
+            // Data: Data to be displayed in the chart
+            data: [
+                { month: 'Jan', avgTemp: 2.3, iceCreamSales: 162000 },
+                { month: 'Mar', avgTemp: 6.3, iceCreamSales: 302000 },
+                { month: 'May', avgTemp: 16.2, iceCreamSales: 800000 },
+                { month: 'Jul', avgTemp: 22.8, iceCreamSales: 1254000 },
+                { month: 'Sep', avgTemp: 14.5, iceCreamSales: 950000 },
+                { month: 'Nov', avgTemp: 8.9, iceCreamSales: 200000 },
+            ],
+            // Series: Defines which chart type and data to use
+            series: [{ type: 'bar', xKey: 'month', yKey: 'iceCreamSales' }],
+        });
+
         
         /**
          * Set charts based on their values type
@@ -232,24 +246,10 @@ export default
             };
             console.log(lineOptions)
 
-            line_options.value = lineOptions;
+            line_options.value = optionsbar.value;
         }
 
 
-
-        const optionsbar = ref({
-            // Data: Data to be displayed in the chart
-            data: [
-                { month: 'Jan', avgTemp: 2.3, iceCreamSales: 162000 },
-                { month: 'Mar', avgTemp: 6.3, iceCreamSales: 302000 },
-                { month: 'May', avgTemp: 16.2, iceCreamSales: 800000 },
-                { month: 'Jul', avgTemp: 22.8, iceCreamSales: 1254000 },
-                { month: 'Sep', avgTemp: 14.5, iceCreamSales: 950000 },
-                { month: 'Nov', avgTemp: 8.9, iceCreamSales: 200000 },
-            ],
-            // Series: Defines which chart type and data to use
-            series: [{ type: 'bar', xKey: 'month', yKey: 'iceCreamSales' }],
-        });
 
 
         return {
