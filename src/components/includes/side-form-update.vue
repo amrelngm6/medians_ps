34666a79c5672578de5ea932dfba536d4db98ba2
unknown <amr_elngm6@yahoo.com>
2024-01-01 12:26:08 +0200

    .

--- src/components/includes/side-form-update.vue
+++ src/components/includes/side-form-update.vue
@@ -3,7 +3,7 @@
         <div class="p-4 rounded-lg shadow-lg bg-white dark:bg-gray-700" >
             <form action="/api/update" method="POST" data-refresh="1" id="add-device-form" class="action py-0 m-auto rounded-lg max-w-xl pb-10">
                 <div class="w-full flex">
-                    <h1 class="w-full m-auto max-w-xl text-base mb-10 " v-text="trans('update')"></h1>
+                    <h1 class="w-full m-auto max-w-xl text-base mb-10 " v-text="translate('update')"></h1>
                     <span class="cursor-pointer py-1 px-2" @click="emitClose"><close_icon /></span>
                 </div>
                 <input name="type" type="hidden" :value="model">
@@ -19,16 +19,16 @@
                     
                         <input v-if="column.column_type == 'password'" autocomplete="off" :name="'params['+column.key+']'" :type="column.column_type" class="h-12 mb-3 rounded w-full border px-3 text-gray-700  focus:border-blue-100 dark:bg-gray-800  dark:border-gray-600" :placeholder="column.title">
 
-                        <div v-if="column.column_type == 'checkbox' && !showLoader"  class="py-4 flex gap gap-2 cursor-pointer" @click="setActiveStatus(item, column.key)">
+                        <div v-if="column.column_type == 'checkbox'"  class="py-4 flex gap gap-2 cursor-pointer" @click="setActiveStatus(item, column.key)">
                             <span :class="!item[column.key] ? 'bg-inverse-dark' : ''" class="mx-2 mt-1 bg-red-400 block h-4 relative rounded-full w-8" style="direction: ltr;" ><a class="absolute bg-white block h-4 relative right-0 rounded-full w-4" :style="{left: item[column.key] ? '16px' : 0}"></a></span>
-                            <span  v-text="item[column.key] ? trans('Active') : trans('Pending')" class=" font-semibold inline-flex items-center px-2 py-1 rounded-full text-xs font-medium "></span>
+                            <span  v-text="item[column.key] ? translate('Active') : translate('Pending')" class=" font-semibold inline-flex items-center px-2 py-1 rounded-full text-xs font-medium "></span>
                             <input v-model="item[column.key]"  type="checkbox" class="hidden" :name="'params['+column.key+']'" />
                         </div>
                         
                         <textarea v-if="column.column_type == 'textarea'" :name="'params['+column.key+']'" type="text" rows="4" class="mt-3 rounded-lg w-full border px-3 text-gray-700  focus:border-blue-100 dark:bg-gray-800  dark:border-gray-600" :placeholder="column.title" v-model="item[column.key]"></textarea>
 
                         <select v-if="column.data && column.column_type == 'select'" v-model="item[column.key]" :name="'params['+column.key+']'" :type="column.column_type" class="h-12 mb-3 rounded w-full border px-3 text-gray-700  focus:border-blue-100 dark:bg-gray-800  dark:border-gray-600"   :placeholder="column.title">
-                            <option value="0"  v-if="!column.required" v-text="trans('select') +' '+ column.title"></option>
+                            <option value="0"  v-if="!column.required" v-text="translate('select') +' '+ column.title"></option>
                             <option v-for="option in column.data" :value="option[ column.column_key ? column.column_key : column.key]" v-text="option[column.text_key]"></option>
                         </select>
                         
@@ -37,7 +37,7 @@
                     </div>
                 </div>
 
-                <button class="uppercase h-12 mt-3 text-white w-full rounded bg-red-700 hover:bg-red-800" v-text="trans('save')"></button>
+                <button class="uppercase h-12 mt-3 text-white w-full rounded bg-red-700 hover:bg-red-800" v-text="translate('save')"></button>
             </form>
         </div>
     </div>
@@ -53,6 +53,7 @@ export default
         'vue-medialibrary-field': field,
         close_icon
     },
+
     props: [
         'columns',
         'model',
@@ -62,21 +63,21 @@ export default
         'conf',
     ],
 
+    emits: ['callback'],
 
-    data() {
-        return {
-            file: '',
-            showLoader: false
-        }
-    },
-    methods: {
-        
-        emitClose(model)
+    setup(props, {emit}) {
+
+        const emitClose = (model) => 
         {
-            model ? this.$emit('callback', model) : '';
-        },
+            model ? emit('callback', model) : '';
+        }
 
-        isInput(val)
+        
+        const setActiveStatus = (item, key) => {
+            item[key] = !item[key];
+        }      
+
+        const isInput = (val) =>
         {
             switch (val) 
             {
@@ -92,15 +93,14 @@ export default
                     break;
             }
             return false;
-        },   
-        setActiveStatus(item, key) {
-            this.showLoader = true;
-            item[key] = !item[key];
-            this.showLoader = false;
-        },        
-        trans(i)
-        {
-            return translate(i);
+        }
+
+        return {
+            file: '',
+            isInput,
+            setActiveStatus,
+            emitClose,
+            translate
         }
     }
 };
