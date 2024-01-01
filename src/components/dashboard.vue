4fc6e41491a4b0f55a36c43fd7a7bbc076cd0137
unknown <amr_elngm6@yahoo.com>
2024-01-01 17:02:33 +0200

    .

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="w-full overflow-auto" style="height: 85vh; z-index: 9999;">
+        <ag-charts-vue :options="optionsbar"> </ag-charts-vue>
 
         <div class="top-0 py-2 w-full px-4 bg-gray-50 mt-0 sticky rounded" style="z-index:9">
             <div class="w-full flex gap-6">
@@ -235,7 +236,24 @@ export default
         }
 
 
+
+        const optionsbar = ref({
+            // Data: Data to be displayed in the chart
+            data: [
+                { month: 'Jan', avgTemp: 2.3, iceCreamSales: 162000 },
+                { month: 'Mar', avgTemp: 6.3, iceCreamSales: 302000 },
+                { month: 'May', avgTemp: 16.2, iceCreamSales: 800000 },
+                { month: 'Jul', avgTemp: 22.8, iceCreamSales: 1254000 },
+                { month: 'Sep', avgTemp: 14.5, iceCreamSales: 950000 },
+                { month: 'Nov', avgTemp: 8.9, iceCreamSales: 200000 },
+            ],
+            // Series: Defines which chart type and data to use
+            series: [{ type: 'bar', xKey: 'month', yKey: 'iceCreamSales' }],
+        });
+
+
         return {
+            optionsbar,
             translate,
             line_options,
             dates_filters,
