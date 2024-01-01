ba6302356bee16fcc3bb5812eabc33fa7c3f127e
unknown <amr_elngm6@yahoo.com>
2024-01-01 17:26:31 +0200

    .

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -230,16 +230,10 @@ export default
 
             // column_options.value = columnOptions;
 
+            console.log(content.value.trips_charts)
             // Line charts for sales in last days 
             let lineOptions =  {
-                data: [
-                    { month: 'Jan', avgTemp: 2.3, iceCreamSales: 162000 },
-                    { month: 'Mar', avgTemp: 6.3, iceCreamSales: 302000 },
-                    { month: 'May', avgTemp: 16.2, iceCreamSales: 800000 },
-                    { month: 'Jul', avgTemp: 22.8, iceCreamSales: 1254000 },
-                    { month: 'Sep', avgTemp: 14.5, iceCreamSales: 950000 },
-                    { month: 'Nov', avgTemp: 8.9, iceCreamSales: 200000 },
-                ],
+                data: content.value.trips_charts,
                 // Series: Defines which chart type and data to use
                 series: [{ type: 'bar', xKey: 'month', yKey: 'iceCreamSales' }],
                 
