96ecd469f1348035b98b5165f14fd9b28db3cd53
unknown <amr_elngm6@yahoo.com>
2024-01-01 17:00:22 +0200

    .

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -21,7 +21,7 @@
                     </div>
                     
                     <div class="w-full bg-white p-4 mb-4 rounded-lg">
-                        <ag-charts-vue ref="lines" :key="line_options" :options="line_options"> </ag-charts-vue>
+                        <!-- <ag-charts-vue ref="lines" :key="line_options" :options="line_options"> </ag-charts-vue> -->
                     </div>
                     <div class="row mt-6">
                         <dashboard_card class="col-md-3 col-sm-12" classes="bg-gradient-success" :title="translate('Vehciles')" :value="content.vehicles_count"></dashboard_card>
