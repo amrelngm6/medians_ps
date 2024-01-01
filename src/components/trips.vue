5724be057397ec45c1b4135f2ff771c68e30bfe5
unknown <amr_elngm6@yahoo.com>
2024-01-01 09:35:38 +0200

    .

--- src/components/trips.vue
+++ src/components/trips.vue
@@ -11,11 +11,11 @@
 
             <div :style="collapsed ? 'max-height:240px' : 'max-height:calc(100vh - 140px)'"  class="mx-14 h-full absolute top-4 rounded-lg p-4   bg-white rounded-xl flex-col justify-start items-start inline-flex">
                 <div class="self-stretch py-4 flex-col justify-center items-start flex">
-                    <div class="text-black text-lg font-semibold" v-text="__('Trips')"></div>
-                    <div class="py-2 self-stretch text-zinc-600 text-base  tracking-wide" v-text="__('Trips description')"></div>
+                    <div class="text-black text-lg font-semibold" v-text="translate('Trips')"></div>
+                    <div class="py-2 self-stretch text-zinc-600 text-base  tracking-wide" v-text="translate('Trips description')"></div>
                 </div>
                 <div v-if="!collapsed" class="w-full self-stretch pt-2 flex-col justify-center items-start flex">
-                    <input class="w-full bg-gray-100 rounded-lg px-4 py-2 " :placeholder="__('find by name and address')" v-model="searchText" v-on:change="searchTextChanged"  v-on:input="searchTextChanged" v-on:keydown="searchTextChanged" />
+                    <input class="w-full bg-gray-100 rounded-lg px-4 py-2 " :placeholder="translate('find by name and address')" v-model="searchText" v-on:change="searchTextChanged"  v-on:input="searchTextChanged" v-on:keydown="searchTextChanged" />
                 </div>
                 <div :key="collapsed" v-if="content.items" class=" max-h-[400px] overflow-auto my-4 w-full self-stretch py-4  ">
                     <div v-if="!collapsed" v-for="(trip, index) in content.items" :key="trip.active" class="w-full">
@@ -42,12 +42,12 @@
                                 <hr class="w-full" />
                                 <div class="w-full relative flex">
                                     <div class="w-full relative">
-                                        <h3 class="py-2 text-sm mb-10" v-text="__('Waiting pickups')"></h3>
+                                        <h3 class="py-2 text-sm mb-10" v-text="translate('Waiting pickups')"></h3>
                                         <img :style="(lang.lang == 'en' ? 'left: ' : 'right: ') +(20 * i)+'px'" class="rounded-full w-8 h-8 left-0 bottom-1 absolute rounded-[50px] border-2 border-purple-800" v-if="tripLocation.status == 'waiting'" v-for="(tripLocation, i) in filterLocations(trip.pickup_locations, 'waiting')" :src="(tripLocation && tripLocation.location && tripLocation.location.picture) ? tripLocation.location.picture : 'https://via.placeholder.com/37x37'" /> 
                                         <span class="bottom-2 absolute pt-2" :style="(lang.lang == 'en' ? 'left: ' : 'right: ') +((20 * trip.waiting_locations.length) + 20)+'px'"><i class="fa fa-location-dot text-sm"></i> <span class="font-semibold  px-1" v-if="trip.pickup_locations" v-text="trip.waiting_locations.length"></span></span>
                                     </div>
                                     <div class="w-full relative">
-                                        <h3 class="py-2 text-sm mb-10" v-text="__('Active pickups')"></h3>
+                                        <h3 class="py-2 text-sm mb-10" v-text="translate('Active pickups')"></h3>
                                         <img :style="(lang.lang == 'en' ? 'left: ' : 'right: ') +(20 * i)+'px'" class="rounded-full w-8 h-8 left-0 bottom-1 absolute rounded-[50px] border-2 border-purple-800" v-if="tripLocation.status == 'moving'" v-for="(tripLocation, i) in filterLocations(trip.pickup_locations, 'moving')" :src="(tripLocation && tripLocation.location && tripLocation.location.picture) ? tripLocation.location.picture : 'https://via.placeholder.com/37x37'" /> 
                                         <span class="bottom-2 absolute pt-2" :style="(lang.lang == 'en' ? 'left: ' : 'right: ') +((20 * trip.moving_locations_count) + 20)+'px'"><i class="fa fa-location-dot text-sm"></i> <span class="font-semibold  px-1" v-if="trip.pickup_locations" v-text="trip.moving_locations_count"></span></span>
                                     </div>
@@ -59,7 +59,7 @@
                 </div>
                 <div
                     class="flex self-stretch grow shrink basis-0 justify-between items-center inline-flex">
-                    <div @click="collapsed = !collapsed" class="cursor-pointer p-2 block text-center "><i class="fa " :class="collapsed ? 'fa-circle-down' : 'fa-circle-up'"></i><p class="font-semibold" v-text="collapsed ? __('Expand') : __('Collapse')"></p></div>
+                    <div @click="collapsed = !collapsed" class="cursor-pointer p-2 block text-center "><i class="fa " :class="collapsed ? 'fa-circle-down' : 'fa-circle-up'"></i><p class="font-semibold" v-text="collapsed ? translate('Expand') : translate('Collapse')"></p></div>
                 </div>
             </div>
 
@@ -331,7 +331,7 @@ export default
 //         bindings() {
 
 //             this.content.columns.push({
-//                     key: this.__("actions"),
+//                     key: this.translate("actions"),
 //                     component: dataTableSideActions,
 //                     sortable: false,
 //                 });
@@ -531,7 +531,7 @@ export default
 //             return this
 //         },
         
-//         __(i)
+//         translate(i)
 //         {
 //             return translate(i);
 //         }
