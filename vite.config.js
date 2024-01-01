fdb60400f210c83caf120f0d36650211872a05f0
unknown <amr_elngm6@yahoo.com>
2024-01-01 18:52:44 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import { minify } from 'vite-plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build:{
-    outDir: './dist'
+    outDir: './dists'
   },
   root: './src',
   rollupOptions: {
@@ -21,10 +20,6 @@ export default defineConfig({
   
   base: '/dist',
   plugins: [
-    minify({
-      target: 'es2015',
-      // other minification options...
-    }),
     vue(),
     vueJsx(),
   ],
