51b0d2811edbe0054a6ea2d5a0f61032487c6559
unknown <amr_elngm6@yahoo.com>
2024-01-01 10:33:10 +0200

    .

--- src/components/trips.vue
+++ src/components/trips.vue
@@ -192,7 +192,7 @@ export default
             
             showMap.value = !showMap.value
             center.value = loc[0].destination;
-            
+            console.log(loc)
             return loc;
         }
 
