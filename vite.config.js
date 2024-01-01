92d321badc0e20046911bf3274169c1f4b4e5790
unknown <amr_elngm6@yahoo.com>
2024-01-01 18:31:57 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: './',
+  root: './src',
+
   base: '/dist',
   plugins: [
     vue(),
