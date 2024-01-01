80b52079178e1bf5e5638d08c12c8691ebf1f449
unknown <amr_elngm6@yahoo.com>
2024-01-01 14:29:47 +0200

    .

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -37,10 +37,10 @@
                                         class="fa fa-edit py-1"></i> <span class="text-base  "
                                         v-text="translate('Edit')"></span></span></div>
                             <div class="sm:col-span-2 col-span-4">
-                                <div class="hs-dropdown ti-dropdown flex justify-end"><span
+                                <div class="hs-dropdown ti-dropdown flex justify-end"><div
                                         @click="handleAction('delete', role)"
                                         class="cursor-pointer hs-dropdown-toggle ti-dropdown-toggle inline-flex !p-1 flex-shrink-0 justify-center items-center gap-2 rounded-sm border font-medium bg-white text-gray-500 shadow-sm align-middle focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-offset-white focus:ring-primary transition-all text-xs dark:bg-bgdark dark:border-white/10 dark:text-white/70 dark:focus:ring-offset-white/10">
-                                        <delete_icon class="w-4" /></span>
+                                        <delete_icon class="w-4" /></div>
                                 </div>
                             </div>
                         </div>
