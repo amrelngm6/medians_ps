986f68b27f6f01bdfe142f7d1f70eae643db1af4
unknown <amr_elngm6@yahoo.com>
2024-01-01 19:02:19 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -11,8 +11,8 @@ export default defineConfig({
     rollupOptions: {
       output: {
         entryFileNames: 'z.min.js',
-        chunkFileNames: '/js/[name]-[hash].min.js',
-        assetFileNames: '/js/[name]-[hash].[ext]',
+        chunkFileNames: '[name]-[hash].min.js',
+        assetFileNames: '[name]-[hash].[ext]',
       },
     },
   },
