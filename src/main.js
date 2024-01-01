3793a01ddcd36569f5adfab689302ae99ebbcc74
unknown <amr_elngm6@yahoo.com>
2024-01-01 15:07:41 +0200

    .

--- src/main.js
+++ src/main.js
@@ -6,26 +6,9 @@ import App from './App.vue'
 
 const app = createApp(App)
 
-app.use(createPinia())
 
-
-// import VueNativeNotification from 'vue-native-notification'
-// app.use(VueNativeNotification, {requestOnNotify: false})
-
-
-
-// import moment from 'moment';
-app.component('open-icon', () => import('./components/svgs/open-icon.vue'));
-app.component('vue-medialibrary-manager', () => import('./components/includes/Manager.vue'));
-app.component('notifications', () => import('./components/notifications.vue'));
-app.component('login', () => import('./components/login-dashboard.vue')); // Used if sessions expired but needs refresh
-app.component('master_dashboard', () => import('./components/master_dashboard.vue')); // Dashboard for Master
-app.component('users', () => import('./components/users.vue'));
-app.component('pages', () => import('./components/pages.vue'));
-app.component('locations', () => import('./components/locations.vue'));
-app.component('destinations', () => import('./components/destinations.vue'));
-app.component('trip_map', () => import('./components/includes/trip_map.vue'));
-app.component('trip_page', () => import('./components/trip_page.vue'));
+import VueFeather from 'vue-feather';
+app.component(VueFeather.name, VueFeather);
 
 
 app.config.productionTip = false
@@ -53,5 +36,6 @@ function pushScreenshotToServer(dataURL, err, info) {
 //       pushScreenshotToServer(canvas.toDataURL(), err, info); 
 //   });
 // };
+app.use(createPinia())
 
 app.mount('#app')
