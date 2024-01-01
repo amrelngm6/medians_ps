e0b5f2b17f964ced6ac2156752dda21dd0c81f53
unknown <amr_elngm6@yahoo.com>
2024-01-01 16:43:55 +0200

    .

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -3,9 +3,9 @@
 
         <div class="top-0 py-2 w-full px-4 bg-gray-50 mt-0 sticky rounded" style="z-index:9">
             <div class="w-full flex gap-6">
-                <h3 class="text-base lg:text-lg whitespace-nowrap" v-text="__('Dashboard Reports')"></h3> 
+                <h3 class="text-base lg:text-lg whitespace-nowrap" v-text="translate('Dashboard Reports')"></h3> 
                 <ul class="w-full flex gap-4 ">
-                    <li v-for="item in dates_filters" @click="switchDate(item.value)" :class="(activeDate == item.value) ? 'font-semibold' : ''" class="cursor-pointer" v-text="__(item.title)"></li>
+                    <li v-for="item in dates_filters" @click="switchDate(item.value)" :class="(activeDate == item.value) ? 'font-semibold' : ''" class="cursor-pointer" v-text="translate(item.title)"></li>
                 </ul>
             </div>
         </div>
@@ -14,26 +14,26 @@
             <div v-if="lang &&  setting" class="w-full overflow-y-auto overflow-x-hidden px-2 mt-6" >
                 <div class="">
                     <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-6 mb-6">
-                        <dashboard_card_white  icon="/uploads/img/booking-unpaid.png" classes="bg-gradient-danger" :title="__('active_trips')" :value="content.active_trips_count"></dashboard_card_white>
-                        <dashboard_card_white  icon="/uploads/img/booking-paid.png" classes="bg-gradient-info" :title="__('completed_trips')" :value="content.completed_trips_count"></dashboard_card_white>
-                        <dashboard_card_white  icon="/uploads/img/booking_income.png" classes="bg-gradient-success" :title="__('total_trips')" :value="content.total_trips_count"></dashboard_card_white>
-                        <dashboard_card_white  icon="/uploads/img/products_icome.png" classes="bg-gradient-warning" :title="__('help_messages')" :value="content.help_messages_count"></dashboard_card_white>
+                        <dashboard_card_white  icon="/uploads/img/booking-unpaid.png" classes="bg-gradient-danger" :title="translate('active_trips')" :value="content.active_trips_count"></dashboard_card_white>
+                        <dashboard_card_white  icon="/uploads/img/booking-paid.png" classes="bg-gradient-info" :title="translate('completed_trips')" :value="content.completed_trips_count"></dashboard_card_white>
+                        <dashboard_card_white  icon="/uploads/img/booking_income.png" classes="bg-gradient-success" :title="translate('total_trips')" :value="content.total_trips_count"></dashboard_card_white>
+                        <dashboard_card_white  icon="/uploads/img/products_icome.png" classes="bg-gradient-warning" :title="translate('help_messages')" :value="content.help_messages_count"></dashboard_card_white>
                     </div>
                     <div class="w-full bg-white p-4 mb-4 rounded-lg" v-if="content.trips_charts && content.trips_charts.length">
                         <ag-charts-vue :key="line_options" :options="line_options"> </ag-charts-vue>
                     </div>
                     <div class="row mt-6">
-                        <dashboard_card class="col-md-3 col-sm-12" classes="bg-gradient-success" :title="__('Vehciles')" :value="content.vehicles_count"></dashboard_card>
-                        <dashboard_card class="col-md-3 col-sm-12" classes="bg-gradient-danger" :title="__('Drivers')" :value="content.drivers_count"></dashboard_card>
-                        <dashboard_card class="col-md-3 col-sm-12" classes="bg-gradient-primary" :title="__('Routes')" :value="content.routes_count"></dashboard_card>
-                        <dashboard_card class="col-md-3 col-sm-12" classes="bg-gradient-purple" :title="__('Pickup locations')" :value="content.pickup_locations_count" ></dashboard_card>
+                        <dashboard_card class="col-md-3 col-sm-12" classes="bg-gradient-success" :title="translate('Vehciles')" :value="content.vehicles_count"></dashboard_card>
+                        <dashboard_card class="col-md-3 col-sm-12" classes="bg-gradient-danger" :title="translate('Drivers')" :value="content.drivers_count"></dashboard_card>
+                        <dashboard_card class="col-md-3 col-sm-12" classes="bg-gradient-primary" :title="translate('Routes')" :value="content.routes_count"></dashboard_card>
+                        <dashboard_card class="col-md-3 col-sm-12" classes="bg-gradient-purple" :title="translate('Pickup locations')" :value="content.pickup_locations_count" ></dashboard_card>
                     </div>
                 </div>
                 
                 <div class="w-full lg:flex gap gap-6 pb-6">
                     <div class="card mb-0 w-2/3">
-                        <h4 class="p-4 ml-4" v-text="__('Top drivers')"></h4>
-                        <p class="text-sm text-gray-500 px-4 mb-6" v-text="__('top_drivers_who_have_most_trips')"></p>
+                        <h4 class="p-4 ml-4" v-text="translate('Top drivers')"></h4>
+                        <p class="text-sm text-gray-500 px-4 mb-6" v-text="translate('top_drivers_who_have_most_trips')"></p>
                         <div class="card-body w-full">
                             <div class="w-full">
                                 <ag-charts-vue :key="column_options" :options="column_options"> </ag-charts-vue>
@@ -41,16 +41,16 @@
                         </div>
                     </div>
                     <div class="card w-1/3 lg:w-1/3 lg:mb-0">
-                        <h4 class="p-4 ml-4" v-text="__('Latest students')"></h4>
-                        <p class="text-sm text-gray-500 px-4 mb-6" v-text="__('latest_students_has_been_added')"></p>
+                        <h4 class="p-4 ml-4" v-text="translate('Latest students')"></h4>
+                        <p class="text-sm text-gray-500 px-4 mb-6" v-text="translate('latest_students_has_been_added')"></p>
                         <div class="card-body w-full">
                             <div class="w-full ">
                                 <div class="table-responsive w-full">
                                     <table class="w-full table table-striped table-nowrap custom-table mb-0 datatable">
                                         <thead>
                                             <tr>
-                                                <th colspan="2" v-text="__('Student')"></th>
-                                                <th v-text="__('Contact')"></th>
+                                                <th colspan="2" v-text="translate('Student')"></th>
+                                                <th v-text="translate('Contact')"></th>
                                             </tr>
                                         </thead>
                                         <tbody v-if="content.latest_students">
@@ -70,8 +70,8 @@
 
                     <div class="card  w-full  no-mobile">
                         <div class="w-full flex p-4">
-                            <h4 class="w-full " v-text="__('Latest help messages')"></h4>
-                            <a href="/admin/help_messages" class="w-20" v-text="__('View all')"></a>
+                            <h4 class="w-full " v-text="translate('Latest help messages')"></h4>
+                            <a href="/admin/help_messages" class="w-20" v-text="translate('View all')"></a>
                         </div>
                         <div class="card-body w-full">
                             <div class="w-full">
@@ -79,11 +79,11 @@
                                     <table class="w-full table table-striped table-nowrap custom-table mb-0 datatable">
                                         <thead>
                                             <tr>
-                                                <th v-text="__('name')"></th>
-                                                <th v-text="__('subject')"></th>
-                                                <th v-text="__('message')"></th>
-                                                <th v-text="__('date')"></th>
-                                                <th v-text="__('status')"></th>
+                                                <th v-text="translate('name')"></th>
+                                                <th v-text="translate('subject')"></th>
+                                                <th v-text="translate('message')"></th>
+                                                <th v-text="translate('date')"></th>
+                                                <th v-text="translate('status')"></th>
                                             </tr>
                                         </thead>
                                         <tbody >
@@ -198,16 +198,16 @@ export default
             pie_options.value = JSON.parse(JSON.stringify(charts_options.value));
             pie_options.value.data[0] = {
                 type: "pie",
-                yValueFormatString: "#,### "+this.__('booking'),
+                yValueFormatString: "#,### "+this.translate('booking'),
                 dataPoints: this.content.most_played_games
             }
             
             // Column charts for most played on devices
             let columnOptions = JSON.parse(JSON.stringify(charts_options.value));
-            columnOptions.axisY.title = this.__('Trips count')
+            columnOptions.axisY.title = this.translate('Trips count')
             columnOptions.data[0] = {
                 type: "column",
-                yValueFormatString: "#,### "+this.__('Trips count'),
+                yValueFormatString: "#,### "+this.translate('Trips count'),
                 dataPoints: content.value.top_drivers
             }
 
@@ -217,13 +217,13 @@ export default
             let lineOptions = JSON.parse(JSON.stringify(charts_options.value));
             lineOptions.theme = 'light2'
             lineOptions.axisY.suffix = ''
-            lineOptions.axisY.title = this.__('Trips')
+            lineOptions.axisY.title = this.translate('Trips')
             lineOptions.toolTip = {shared: true}
             lineOptions.data[0] = {
                 type: "line",
                 color: '#003c58',
                 showInLegend: true,
-                yValueFormatString: "#,### "+this.__('Trips'),
+                yValueFormatString: "#,### "+this.translate('Trips'),
                 dataPoints: content.value.trips_charts
             }
             
