1df62b36b2032bf731170c5a094b98e4b2aa6a2d
unknown <amr_elngm6@yahoo.com>
2024-01-01 11:36:36 +0200

    .

--- src/components/includes/trip_map.vue
+++ src/components/includes/trip_map.vue
@@ -21,29 +21,11 @@
                         draggable: true,
                         position: marker.destination,
                     }"
-                    v-if="!showDrag"
-                    :key="showDrag" 
-                    :draggable="true"
-                    @click="enableDrag(marker, index)"
                     >
                     <div style="text-align: center">
                         <img :src="marker.icon" width="40" class="rouned-full" height="40" style="margin-top: 8px" />
                     </div>
                 </CustomMarker>
-
-                <Marker
-                    v-for="(marker, index) in markers" 
-                    :options="{
-                        position: marker.destination,
-                        draggable: true,
-                    }"
-                    :key="showDrag" 
-                    v-if="showDrag"
-                    @dragstart="onMarkerDragStart(index)"
-                    @dragend="updateMarker"
-                    >
-
-                </Marker>
 
         </GoogleMap>
     </div>
@@ -74,92 +56,12 @@ export default
         console.log(props.center);
 
 
-        const reload = ref(null);
-        const render = ref(null);
-        const showDrag = ref(null);
-        const travelMode = ref('Driving');
         const zoom = ref(14);
-        const markers = ref([]);
-        const polylineCoordinates = ref([]);
-        const directionPoints = ref({});
-        const activeDestination = ref({});
-        const activeMarkerIndex = ref({});
-        
-        const  enableDrag = (marker, i) =>  {
-            showDrag.value = true
-            checkMarker(marker, i)
-        }
-        
-        
-        const  checkMarker = async (marker, i ) =>  {
-            activeMarkerIndex.value = i;
-            emit('click-marker', props.waypoints[i], i, JSON.parse(JSON.stringify(marker)));
-        }
-        
-        
-        const  updateMarker = async (event) =>  {
-            let newObject = props.waypoints[activeMarkerIndex.value]
-            newObject.latitude = event.latLng.lat()
-            newObject.longitude = event.latLng.lng()
-            newObject.address = await handlePositionToPlaceId(newObject.latitude, newObject.longitude);
-
-            props.waypoints[activeMarkerIndex.value].destination = {lat: newObject.latitude, lng: newObject.longitude};
-
-            showDrag.value = null;
-            emit('update-marker', newObject);
-        }
-        
-        
-        const  getPlaceIdFromPosition = async (lat, lng) => {
-            const geocoder = new google.maps.Geocoder();
-
-            const latLng = { lat: parseFloat(lat), lng: parseFloat(lng) };
-
-            return new Promise((resolve, reject) => {
-                geocoder.geocode({ location: latLng }, (results, status) => {
-                if (status === 'OK') {
-                    results[0] ? resolve(results[0]) : reject('No results found');
-                } else {
-                    reject(`Geocoder failed due to: ${status}`);
-                }
-                });
-            });
-        }
-
-        const handlePositionToPlaceId = async (lat, lng) => {
-
-            try {
-                const place = await getPlaceIdFromPosition(lat, lng);
-                return place.formatted_address;
-                // Perform actions with the retrieved placeId
-            } catch (error) {
-                console.error(error);
-                // Handle error if geocoding fails
-            }
-        }
-        
-        const  onMarkerDragStart =  (i) =>  {
-            activeMarkerIndex.value = i
-        }
         
              
         return {
-            checkMarker,
-            onMarkerDragStart,
-            enableDrag,
-            updateMarker,
-            reload,
-            render,
-            showDrag,
-            travelMode,
-            origin: { lat: 0, lng: 0 }, // Replace with your origin location
-            destination: { lat: 0, lng: 0 }, // Replace with your destination location
             zoom,
             markers: props.waypoints,
-            polylineCoordinates,
-            activeMarkerIndex,
-            directionPoints,
-            activeDestination,
         }
     },
 
