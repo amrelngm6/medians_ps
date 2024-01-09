31cb5743273e4bff715281a68bdac132d6d7ea01
unknown <amr_elngm6@yahoo.com>
2024-01-09 19:51:15 +0200

    .

--- app/config/APP.php
+++ app/config/APP.php
@@ -16,9 +16,9 @@ use \Medians\Auth\Application\AuthService;
 class APP 
 {
 
-	public $default_lang = 'arabic';
+	public $default_lang = 'english';
 
-	public $lang_code = 'ar';
+	public $lang_code = 'en';
 
 	public $lang;
 
