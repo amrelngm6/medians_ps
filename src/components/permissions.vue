6f576e9e69662eb102e5a1ca77d8ee56c2a2307a
unknown <amr_elngm6@yahoo.com>
2024-01-09 22:57:42 +0200

    .

--- src/components/permissions.vue
+++ src/components/permissions.vue
@@ -73,7 +73,10 @@ export default
         load();
 
         const setActiveStatus = (permission) => {
-            permission.value.access = permission.value ? !permission.value.access : true;
+            if (permission.value)
+            {
+                permission.value.access = !permission.value.access;
+            }
         }
 
 
