12745e42c13c3f9035a61a9136e440b772fade89
unknown <amr_elngm6@yahoo.com>
2024-01-28 08:53:37 +0200

    .

--- src/components/maps/route_map.vue
+++ src/components/maps/route_map.vue
@@ -85,10 +85,10 @@ export default {
     const mapDestination = ref({ lat: 0, lng: 0 }); // Set initial values
 
     const fetchRoute = async () => {
-      const baseUrl = 'http://localhost:3000/directions'; // Use your server's URL
-      const url = `${baseUrl}?origin=${mapOrigin.value.lat},${mapOrigin.value.lng}&destination=${mapDestination.value.lat},${mapDestination.value.lng}&apiKey=${props.system_setting.google_map_api}`;
-      // const baseUrl = 'https://maps.googleapis.com/maps/api/directions/json';
-      // const url = `${baseUrl}?origin=${mapOrigin.value.lat},${mapOrigin.value.lng}&destination=${mapDestination.value.lat},${mapDestination.value.lng}&key=${props.system_setting.google_map_api}`;
+      // const baseUrl = 'http://localhost:3000/directions'; // Use your server's URL
+      // const url = `${baseUrl}?origin=${mapOrigin.value.lat},${mapOrigin.value.lng}&destination=${mapDestination.value.lat},${mapDestination.value.lng}&apiKey=${props.system_setting.google_map_api}`;
+      const baseUrl = 'https://maps.googleapis.com/maps/api/directions/json';
+      const url = `${baseUrl}?origin=${mapOrigin.value.lat},${mapOrigin.value.lng}&destination=${mapDestination.value.lat},${mapDestination.value.lng}&key=${props.system_setting.google_map_api}`;
 
       try {
 
