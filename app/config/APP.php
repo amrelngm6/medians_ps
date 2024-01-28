248ac540972bc3933ec293f684ea335cc4297395
unknown <amr_elngm6@yahoo.com>
2024-01-28 04:27:12 +0200

    .

--- app/config/APP.php
+++ app/config/APP.php
@@ -113,8 +113,11 @@ class APP
 	 */
 	public function checkAPISession()
 	{
+		error_log('Checking');
 		if (!empty($this->request()->headers->get('token')))
 		{
+		error_log('!Empty');
+
 			return  (new AuthService())->checkAPISession($this->request()->headers->get('token'), $this->request()->headers->get('userType'));
 		}
 	}  
