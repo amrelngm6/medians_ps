70cc05ce6ef98ad198dea0377a6d258b3e9bf70f
unknown <amr_elngm6@yahoo.com>
2024-01-29 06:02:43 +0200

    .

--- src/components/wizards/privateTripWizard.vue
+++ src/components/wizards/privateTripWizard.vue
@@ -325,13 +325,13 @@
                                     <hr class="block mt-6 my-2 opacity-10" />
                                     <div class="bg-gray-200 shadow-md mb-10 rounded-xl">
                                         <div class="card-header border-0 pt-9">
-                                            <div class="card-title m-0 flex  gap-4" v-if="activeItem.driver">
+                                            <div class="card-title m-0 flex  gap-4" v-if="activeItem.model">
                                                 <div class="symbol symbol-50px w-50px bg-light">
-                                                    <img  :src="activeItem.driver.picture" alt="image" class="p-3" >
+                                                    <img  :src="activeItem.model.picture" alt="image" class="p-3" >
                                                 </div>
                                                 <div class="w-full ">
-                                                    <div class="font-semibold" v-text="activeItem.driver.name"></div>
-                                                    <div class="" v-text="activeItem.driver.mobile"></div>
+                                                    <div class="font-semibold" v-text="activeItem.model.name"></div>
+                                                    <div class="" v-text="activeItem.model.mobile"></div>
                                                 </div>
                                             </div>
                                             <label class=" flex gap-2 cursor-pointer">
