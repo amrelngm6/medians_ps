a72e966400dc658d7b0ff6fc0ae66c863367709b
unknown <amr_elngm6@yahoo.com>
2024-01-31 01:08:01 +0200

    .

--- app/Medians/Trips/Infrastructure/PrivateTripRepository.php
+++ app/Medians/Trips/Infrastructure/PrivateTripRepository.php
@@ -107,8 +107,7 @@ class PrivateTripRepository
 
 	public function getUpcomingDriverTrip($driver_id)
 	{
-		return PrivateTrip::where('business_id', $this->business_id)
-		->where('driver_id', $driver_id)
+		return PrivateTrip::where('driver_id', $driver_id)
 		->where('status',  'scheduled')
 		->whereDate('date', '>=', date('Y-m-d'))
 		->first();
