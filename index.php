828f7da36851be70172a89af6017d90caad09076
unknown <amr_elngm6@yahoo.com>
2024-02-27 00:47:25 +0200

    .

--- index.php
+++ index.php
@@ -6,6 +6,25 @@ session_start();
 date_default_timezone_set('Africa/Cairo');
 
 
+// Allow from any origin
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+}
+
 // Check if installed or redirect to installation 
 file_exists(__DIR__.'/app/config/database.php') 
     ?  require_once __DIR__.'/app/config/database.php' 
