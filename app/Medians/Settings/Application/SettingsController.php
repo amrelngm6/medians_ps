f5f5ef3aa88affb17c3fe32d09f91a7caa070a25
unknown <amr_elngm6@yahoo.com>
2024-02-27 08:05:17 +0200

    .

--- app/Medians/Settings/Application/SettingsController.php
+++ app/Medians/Settings/Application/SettingsController.php
@@ -71,10 +71,10 @@ class SettingsController extends CustomController
 			
 
 			'email'=> [	
-				[ 'key'=> "email", 'title'=> __('Email'), 'fillable'=> true, 'column_type'=>'checkbox' ],
-				[ 'key'=> "address", 'title'=> __('Footer Address'), 'fillable'=> true, 'column_type'=>'checkbox' ],
-				[ 'key'=> "mobile", 'title'=> __('Footer mobile'), 'fillable'=> true, 'column_type'=>'checkbox' ],
-				[ 'key'=> "phone", 'title'=> __('Footer phone'), 'fillable'=> true, 'column_type'=>'checkbox' ],
+				[ 'key'=> "email", 'title'=> __('Email'), 'fillable'=> true, 'column_type'=>'text' ],
+				[ 'key'=> "address", 'title'=> __('Footer Address'), 'fillable'=> true, 'column_type'=>'text' ],
+				[ 'key'=> "mobile", 'title'=> __('Footer mobile'), 'fillable'=> true, 'column_type'=>'text' ],
+				[ 'key'=> "phone", 'title'=> __('Footer phone'), 'fillable'=> true, 'column_type'=>'text' ],
 			],
 
 			'trips'=> [	
