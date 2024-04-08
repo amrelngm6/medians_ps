bbc1c56550abeef4b3b752cc3d78e774338e23fd
unknown <amr_elngm6@yahoo.com>
2024-04-09 01:22:27 +0200

    .

--- src/components/wizards/tripWizard.vue
+++ src/components/wizards/tripWizard.vue
@@ -86,7 +86,7 @@
                                                         <img :src="item.model.picture" class="h-50 align-self-center" alt="">
                                                     </div>
 
-                                                    <div class="flex align-items-center " v-if="item.model && item.location">
+                                                    <div class="flex align-items-center w-full" v-if="item.model && item.location">
                                                         <div class="flex-grow-1 me-2" >
                                                             <a href="javascript:;" class="text-gray-800 text-hover-primary fs-6 fw-bold" v-text="item.model.name"></a>
                                                             <span class="text-muted fw-semibold d-block fs-7" v-text="item.location.start_address"></span>
