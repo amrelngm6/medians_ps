4ed651bffae015a7ef28e447eaa62b0bde91864f
unknown <amr_elngm6@yahoo.com>
2024-04-15 00:01:47 +0200

    .

--- src/components/wizards/invoiceWizard.vue
+++ src/components/wizards/invoiceWizard.vue
@@ -1,199 +1,8 @@
 <template>
     <div class="d-flex flex-column flex-column-fluid">
 
-        <!--begin::Toolbar-->
-        <div id="kt_app_toolbar" class="app-toolbar  py-3 py-lg-6 ">
-
-            <!--begin::Toolbar container-->
-            <div id="kt_app_toolbar_container" class="app-container  container-xxl d-flex flex-stack ">
-
-
-
-                <!--begin::Page title-->
-                <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3 ">
-                    <!--begin::Title-->
-                    <h1 class="page-heading d-flex text-gray-900 fw-bold fs-3 flex-column justify-content-center my-0">
-                        Invoice 1
-                    </h1>
-                    <!--end::Title-->
-
-
-                    <!--begin::Breadcrumb-->
-                    <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
-                        <li class="breadcrumb-item text-muted">
-                            <a href="/metronic8/demo1/index.html" class="text-muted text-hover-primary">
-                                Home </a>
-                        </li>
-
-                        <li class="breadcrumb-item">
-                            <span class="bullet bg-gray-500 w-5px h-2px"></span>
-                        </li>
-
-
-                        <li class="breadcrumb-item text-muted">
-                            Invoice Manager </li>
-
-                        <li class="breadcrumb-item">
-                            <span class="bullet bg-gray-500 w-5px h-2px"></span>
-                        </li>
-
-
-                        <li class="breadcrumb-item text-muted">
-                            View Invoices </li>
-
-
-                    </ul>
-                    <!--end::Breadcrumb-->
-                </div>
-                <!--end::Page title-->
-                <!--begin::Actions-->
-                <div class="d-flex align-items-center gap-2 gap-lg-3">
-                    <!--begin::Filter menu-->
-                    <div class="m-0">
-                        <!--begin::Menu toggle-->
-                        <a href="#" class="btn btn-sm btn-flex btn-secondary fw-bold" data-kt-menu-trigger="click"
-                            data-kt-menu-placement="bottom-end">
-                            <i class="ki-duotone ki-filter fs-6 text-muted me-1"><span class="path1"></span><span
-                                    class="path2"></span></i>
-                            Filter
-                        </a>
-                        <!--end::Menu toggle-->
-
-
-
-                        <!--begin::Menu 1-->
-                        <div class="menu menu-sub menu-sub-dropdown w-250px w-md-300px" data-kt-menu="true"
-                            id="kt_menu_661c4f45d9d8f">
-                            <!--begin::Header-->
-                            <div class="px-7 py-5">
-                                <div class="fs-5 text-gray-900 fw-bold">Filter Options</div>
-                            </div>
-
-                            <div class="separator border-gray-200"></div>
-                            
-                            <div class="px-7 py-5">
-                                
-                                <div class="mb-10">
-                                    
-                                    <label class="form-label fw-semibold">Status:</label>
-                                    
-
-                                    
-                                    <div>
-                                        <select class="form-select form-select-solid select2-hidden-accessible"
-                                            multiple="" data-kt-select2="true" data-close-on-select="false"
-                                            data-placeholder="Select option"
-                                            data-dropdown-parent="#kt_menu_661c4f45d9d8f" data-allow-clear="true"
-                                            data-select2-id="select2-data-7-0u96" tabindex="-1" aria-hidden="true"
-                                            data-kt-initialized="1">
-                                            <option></option>
-                                            <option value="1">Approved</option>
-                                            <option value="2">Pending</option>
-                                            <option value="2">In Process</option>
-                                            <option value="2">Rejected</option>
-                                        </select><span class="select2 select2-container select2-container--bootstrap5"
-                                            dir="ltr" data-select2-id="select2-data-8-i6jm" style="width: 100%;"><span
-                                                class="selection"><span
-                                                    class="select2-selection select2-selection--multiple form-select form-select-solid"
-                                                    role="combobox" aria-haspopup="true" aria-expanded="false"
-                                                    tabindex="-1" aria-disabled="false">
-                                                    <ul class="select2-selection__rendered" id="select2-vm28-container">
-                                                    </ul><span class="select2-search select2-search--inline"><textarea
-                                                            class="select2-search__field" type="search" tabindex="0"
-                                                            autocorrect="off" autocapitalize="none" spellcheck="false"
-                                                            role="searchbox" aria-autocomplete="list" autocomplete="off"
-                                                            aria-label="Search"
-                                                            aria-describedby="select2-vm28-container"
-                                                            placeholder="Select option"
-                                                            style="width: 100%;"></textarea></span>
-                                                </span></span><span class="dropdown-wrapper"
-                                                aria-hidden="true"></span></span>
-                                    </div>
-                                    
-                                </div>
-
-                                
-                                <div class="mb-10">
-                                    
-                                    <label class="form-label fw-semibold">Member Type:</label>
-                                    
-
-                                    <div class="d-flex">
-                                        <label class="form-check form-check-sm form-check-custom form-check-solid me-5">
-                                            <input class="form-check-input" type="checkbox" value="1">
-                                            <span class="form-check-label">
-                                                Author
-                                            </span>
-                                        </label>
-                                        <!--end::Options-->
-
-                                        <!--begin::Options-->
-                                        <label class="form-check form-check-sm form-check-custom form-check-solid">
-                                            <input class="form-check-input" type="checkbox" value="2" checked="checked">
-                                            <span class="form-check-label">
-                                                Customer
-                                            </span>
-                                        </label>
-                                        <!--end::Options-->
-                                    </div>
-                                    <!--end::Options-->
-                                </div>
-                                <!--end::Input group-->
-
-                                
-                                <div class="mb-10">
-                                    
-                                    <label class="form-label fw-semibold">Notifications:</label>
-                                    
-
-                                    <!--begin::Switch-->
-                                    <div
-                                        class="form-check form-switch form-switch-sm form-check-custom form-check-solid">
-                                        <input class="form-check-input" type="checkbox" value="" name="notifications"
-                                            checked="">
-                                        <label class="form-check-label">
-                                            Enabled
-                                        </label>
-                                    </div>
-                                    <!--end::Switch-->
-                                </div>
-                                <!--end::Input group-->
-
-                                <!--begin::Actions-->
-                                <div class="d-flex justify-content-end">
-                                    <button type="reset" class="btn btn-sm btn-light btn-active-light-primary me-2"
-                                        data-kt-menu-dismiss="true">Reset</button>
-
-                                    <button type="submit" class="btn btn-sm btn-primary"
-                                        data-kt-menu-dismiss="true">Apply</button>
-                                </div>
-                                <!--end::Actions-->
-                            </div>
-                            <!--end::Form-->
-                        </div>
-                        <!--end::Menu 1-->
-                    </div>
-                    <!--end::Filter menu-->
-
-
-                    <!--begin::Secondary button-->
-                    <!--end::Secondary button-->
-
-                    <!--begin::Primary button-->
-                    <a href="#" class="btn btn-sm fw-bold btn-primary" data-bs-toggle="modal"
-                        data-bs-target="#kt_modal_create_app">
-                        Create </a>
-                    <!--end::Primary button-->
-                </div>
-                <!--end::Actions-->
-            </div>
-            <!--end::Toolbar container-->
-        </div>
-        <!--end::Toolbar-->
-
         <!--begin::Content-->
         <div id="kt_app_content" class="app-content  flex-column-fluid ">
-
 
             <!--begin::Content container-->
             <div id="kt_app_content_container" class="app-container  container-xxl ">
@@ -208,22 +17,20 @@
                                 <div class="mt-n1">
                                     <div class="d-flex flex-stack pb-10">
                                         <a href="#">
-                                            <img alt="Logo"
-                                                :src="'/metronic8/demo1/assets/media/svg/brand-logos/code-lab.svg'">
+                                            <img alt="Logo" :src=" business_setting.logo">
                                         </a>
-                                        <a href="#" class="btn btn-sm btn-success">Pay Now</a>
                                     </div>
                                     <div class="m-0">
                                         
-                                        <div class="fw-bold fs-3 text-gray-800 mb-8">Invoice #34782</div>
+                                        <div class="fw-bold fs-3 text-gray-800 mb-8" v-text="translate('Invoice')+' #'+activeItem.code"> </div>
                                         <div class="row g-5 mb-11">
                                             <div class="col-sm-6">
                                                 
-                                                <div class="fw-semibold fs-7 text-gray-600 mb-1">Issue Date:</div>
+                                                <div class="fw-semibold fs-7 text-gray-600 mb-1" v-text="translate('Issue Date')"></div>
                                                 
 
                                                 <!--end::Col-->
-                                                <div class="fw-bold fs-6 text-gray-800">12 Apr 2021</div>
+                                                <div class="fw-bold fs-6 text-gray-800" v-text="activeItem.date"></div>
                                                 <!--end::Col-->
                                             </div>
                                             <!--end::Col-->
