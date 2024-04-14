97d819d7b3053b30c1b4722cf7c655357bbb7bf4
unknown <amr_elngm6@yahoo.com>
2024-04-14 23:46:13 +0200

    .

--- app/Medians/Invoices/Application/InvoiceController.php
+++ app/Medians/Invoices/Application/InvoiceController.php
@@ -42,7 +42,7 @@ class InvoiceController extends CustomController
 
 		return [
             [ 'value'=> "invoice_id", 'text'=> "#"],
-            [ 'value'=> "user", 'text'=> __('User'), 'sortable'=> false ],
+            [ 'value'=> "model", 'text'=> __('User'), 'sortable'=> false ],
             [ 'value'=> "subtotal", 'text'=> __('Subtotal'), 'sortable'=> true ],
             [ 'value'=> "total_amount", 'text'=> __('Total Amount'), 'sortable'=> true ],
             [ 'value'=> "payment_method", 'text'=> __('Gateway'), 'sortable'=> true ],
