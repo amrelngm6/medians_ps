a535b47d44fe87535994e918b064508623751633
unknown <amr_elngm6@yahoo.com>
2024-04-14 05:33:20 +0200

    .

--- app/Medians/Routes/Infrastructure/RouteRepository.php
+++ app/Medians/Routes/Infrastructure/RouteRepository.php
@@ -60,7 +60,9 @@ class RouteRepository
 	{
 		// return Route::where('driver_id', $driver_id)
 		return Route::where('business_id', $this->business_id)
-		->with('route_locations','position', 'vehicle')->get();
+		->where('status', 'on')
+		->with('route_locations','position', 'vehicle')
+		->get();
 	}
 
 	public function getParentRoutes($parent_id)
