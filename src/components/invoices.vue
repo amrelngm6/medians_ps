352c3f4deaf5250442a84a655611614a92c2f158
unknown <amr_elngm6@yahoo.com>
2024-04-15 03:16:33 +0200

    .

--- src/components/invoices.vue
+++ src/components/invoices.vue
@@ -1,6 +1,29 @@
 <template>
     <div class="w-full " >
-        
+        <date-range-picker
+            ref="picker"
+            :opens="opens"
+            :locale-data="{ firstDay: 1, format: 'dd-mm-yyyy HH:mm:ss' }"
+            :minDate="minDate" :maxDate="maxDate"
+            :singleDatePicker="singleDatePicker"
+            :timePicker="timePicker"
+            :timePicker24Hour="timePicker24Hour"
+            :showWeekNumbers="showWeekNumbers"
+            :showDropdowns="showDropdowns"
+            :autoApply="autoApply"
+            v-model="dateRange"
+            @update="updateValues"
+            @toggle="logEvent('event: open', $event)"
+            @start-selection="logEvent('event: startSelection', $event)"
+            @finish-selection="logEvent('event: finishSelection', $event)"
+            :linkedCalendars="linkedCalendars"
+            :dateFormat="dateFormat"
+    >
+        <template v-slot:input="picker" style="min-width: 350px;">
+            {{ picker.startDate | date }} - {{ picker.endDate | date }}
+        </template>
+    </date-range-picker>
+    
         <invoice_wizard @callback="showWizard=false" v-if="showWizard" :key="showWizard" :item="activeItem" :business_setting="business_setting"  :system_setting="system_setting" :conf="conf" ></invoice_wizard>
         
         <div  v-if="!showWizard"  class="px-4 mb-6 py-4 rounded-lg shadow-md bg-white dark:bg-gray-700 flex w-full">
