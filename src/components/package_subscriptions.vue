1fbb59f5c90d2ff1adf3f8b764c06e0b6e4c7cad
unknown <amr_elngm6@yahoo.com>
2024-04-15 02:53:58 +0200

    .

--- src/components/package_subscriptions.vue
+++ src/components/package_subscriptions.vue
@@ -43,6 +43,32 @@
                 <hr class="mt-2" />
                 <div class="w-full bg-white" >
 
+                    <div class="card-header align-items-center py-5 gap-2 gap-md-5 w-full flex ">
+                        <!--begin::Card title-->
+                        <div class="card-title">
+                            <!--begin::Search-->
+                            <div class="d-flex align-items-center position-relative my-1">
+                                <input type="text"  v-model="searchValue" data-kt-ecommerce-order-filter="search" class="form-control form-control-solid w-250px ps-12" placeholder="Search Report">
+                            </div>
+                            <!--end::Search-->
+
+                            <!--begin::Export buttons-->
+                            <div id="kt_ecommerce_report_views_export" class="d-none"><div class="dt-buttons btn-group flex-wrap">      <button class="btn btn-secondary buttons-copy buttons-html5" tabindex="0" aria-controls="kt_ecommerce_report_views_table" type="button"><span>Copy</span></button> <button class="btn btn-secondary buttons-excel buttons-html5" tabindex="0" aria-controls="kt_ecommerce_report_views_table" type="button"><span>Excel</span></button> <button class="btn btn-secondary buttons-csv buttons-html5" tabindex="0" aria-controls="kt_ecommerce_report_views_table" type="button"><span>CSV</span></button> <button class="btn btn-secondary buttons-pdf buttons-html5" tabindex="0" aria-controls="kt_ecommerce_report_views_table" type="button"><span>PDF</span></button> </div></div>
+                            <!--end::Export buttons-->
+                        </div>
+                        <!--end::Card title-->
+
+                        <!--begin::Card toolbar-->
+                        <div class="card-toolbar flex-row-fluid justify-content-end gap-5">
+
+                            <div class="w-150px">
+                                <select v-model="searchField" class="form-select form-select-solid select2-hidden-accessible" data-control="select2" data-hide-search="true" data-placeholder="Rating" data-kt-ecommerce-order-filter="rating" data-select2-id="select2-data-9-zple" tabindex="-1" aria-hidden="true" data-kt-initialized="1">
+                                    <option v-for="col in content.columns" v-text="col.text" :value="col.value"></option>
+                                </select>
+                            </div>
+
+                        </div>
+                    </div>
                     <datatabble  class="align-middle fs-6 gy-5 table table-row-dashed px-6" :body-text-direction="translate('lang') == 'ar' ? 'right' : 'left'" fixed-checkbox v-if="content.columns" :headers="content.columns" :items="content.items" >
 
                         <template #item-name="item">
