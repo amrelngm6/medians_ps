852ae6caf653e8cba95e72b444aa03a14d65b67e
unknown <amr_elngm6@yahoo.com>
2024-04-15 03:36:23 +0200

    .

--- src/components/invoices.vue
+++ src/components/invoices.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="w-full " >
-        <vue-tailwind-datepicker v-model="dateValue" />
+        <vue-tailwind-datepicker 
+        :separator="' - '+translate('To')+' - '"
+        v-model="dateValue" />
 
         <invoice_wizard @callback="showWizard=false" v-if="showWizard" :key="showWizard" :item="activeItem" :business_setting="business_setting"  :system_setting="system_setting" :conf="conf" ></invoice_wizard>
         
