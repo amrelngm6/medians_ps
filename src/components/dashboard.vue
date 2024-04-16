a927d92e8fdedec66f9a34a04d18734f3e657268
unknown <amr_elngm6@yahoo.com>
2024-04-16 02:11:28 +0200

    .

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -75,7 +75,7 @@
                                             </tr>
                                         </thead>
                                         <tbody v-if="content.applicants"  :key="content.applicants">
-                                            <tr :key="index" v-for="(applicant, index) in content.applicants" class="text-center" >
+                                            <tr :key="index" v-for="(applicant, index) in content.applicants"  >
                                                 <td>
                                                     <div class="flex gap-4 w-full">
                                                         <img width="48" height="48" class="h-10 w-10 rounded-full" :src="'/app/image.php?w=50&h=50&src='+(applicant.model.picture ?? '/uploads/images/default_profile.png')" />
