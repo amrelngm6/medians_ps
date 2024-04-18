c1fcf50e33f7a211d072b40e7fffc37cc00d25a6
unknown <amr_elngm6@yahoo.com>
2024-04-19 01:19:26 +0200

    .

--- app/Medians/Plans/Infrastructure/PlanSubscriptionRepository.php
+++ app/Medians/Plans/Infrastructure/PlanSubscriptionRepository.php
@@ -57,6 +57,7 @@ class PlanSubscriptionRepository
 	public function getLatest($params, $limit = 10 ) 
 	{
 	  	return PlanSubscription::whereBetween('created_at' , [$params['start'], $params['end']])
+	  	->with('user')
 	  	->limit($limit)
 	  	->orderBy('created_at', 'DESC')
 		->get();
