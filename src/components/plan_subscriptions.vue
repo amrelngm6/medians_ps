b586169878bb195245ac7065dcf80ed810264f25
unknown <amr_elngm6@yahoo.com>
2024-04-18 13:21:49 +0200

    .

--- src/components/plan_subscriptions.vue
+++ src/components/plan_subscriptions.vue
@@ -42,19 +42,13 @@
                     <img :src="item.picture" class="w-8 h-8 rounded-full" />
                 </template>
 
-
-                <template #item-students="item">
-                    
-                    <div class="w-full h-8 relative flex">
-                        <div  v-for="(student, i) in item.students" :style="'left: '+(20 * i)+'px'" class="rounded-full w-8 h-8 left-0 top-0 absolute" >
-                            <img v-if="i < 3" :key="i" class="rounded-full w-8 h-8 rounded-[50px] border-2 border-purple-800" :src="(student && student.picture) ? student.picture : 'https://via.placeholder.com/37x37'" /> 
-                        </div>
-                        <span class="flex absolute pt-2" :style="'left: '+((20 * (item.students.length < 3 ? item.students.length : 3) ) + 20)+'px'"> <route_icon /><span class="font-semibold  px-1" v-if="item.students" v-text="item.students.length"></span></span>
+                <template #item-user="item">
+                    <div class="flex gap-2" v-if="item.user" >
+                        <img :src="item.user.picture" class="w-8 h-8 rounded-full" />
+                        <span class="py-2" v-text="item.user.name"></span>
                     </div>
-                    
                 </template>
-
-
+                
                 <template #item-edit="item">
                     <button v-if="!item.not_editable" class="p-2  hover:text-gray-600 text-purple" @click="handleAction('edit', item)">
                         <vue-feather class="w-5" type="edit"></vue-feather>
