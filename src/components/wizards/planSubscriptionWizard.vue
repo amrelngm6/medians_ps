a73b9b49a141d9b9ceef85e03c03f1deee083c7b
unknown <amr_elngm6@yahoo.com>
2024-04-18 22:53:37 +0200

    .

--- src/components/wizards/planSubscriptionWizard.vue
+++ src/components/wizards/planSubscriptionWizard.vue
@@ -213,6 +213,12 @@
                                 </div>
 
 
+                                <div class="card-body p-9" v-if="activeItem.business">
+                                    <div class="fs-3 fw-bold text-gray-900" v-text="translate('Business')"></div>
+                                    <p class="text-gray-500 fw-semibold fs-5 mt-1 mb-7"
+                                    v-text="activeItem.business.business_name"></p>
+                                </div>
+                                    
                                 <div class="card-body p-9" v-if="activeItem.plan">
                                     <div class="fs-3 fw-bold text-gray-900" v-text="activeItem.plan.name"></div>
                                     <p class="text-gray-500 fw-semibold fs-5 mt-1 mb-7"
