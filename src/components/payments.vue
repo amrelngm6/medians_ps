d4308b4dae43b3f763f45edea7793ccd897647cc
unknown <amr_elngm6@yahoo.com>
2024-04-18 13:01:34 +0200

    .

--- src/components/payments.vue
+++ src/components/payments.vue
@@ -89,7 +89,7 @@ export default
         {
             handleGetRequest( url ).then(response=> {
                 content.value = JSON.parse(JSON.stringify(response))
-                searchField.value = content.value.columns[1].value;
+                searchField.value = content.value.columns;
             });
         }
         
