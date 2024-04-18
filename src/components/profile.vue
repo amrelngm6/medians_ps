935ffa69c2dfc069d9cf48a495ca073e2b3c14b2
unknown <amr_elngm6@yahoo.com>
2024-04-18 12:20:50 +0200

    .

--- src/components/profile.vue
+++ src/components/profile.vue
@@ -1,101 +1,95 @@
 <template>
     <div class="w-full mb-5 mb-xl-10">
-        
-            <!--begin::Details-->
-            <div class="card d-flex flex-wrap flex-sm-nowrap">
-                <!--begin: Pic-->
-                <div class="me-7 mb-4">
-                    <div class="symbol symbol-100px symbol-lg-160px symbol-fixed position-relative">
-                        <img :src="auth.photo" class="w-20" alt="image" />
-                        <div
-                            class="position-absolute translate-middle bottom-0 start-100 mb-6 bg-success rounded-circle border border-4 border-body h-20px w-20px">
+
+            <div class="card ">
+                <div class="d-flex flex-wrap flex-sm-nowrap">
+                    <div class="me-7 mb-4">
+                        <div class="symbol symbol-100px symbol-lg-160px symbol-fixed position-relative">
+                            <img :src="auth.photo" class="w-20" alt="image" />
+                            <div
+                                class="position-absolute translate-middle bottom-0 start-100 mb-6 bg-success rounded-circle border border-4 border-body h-20px w-20px">
+                            </div>
                         </div>
                     </div>
+                    <div class="flex-grow-1">
+                        <div class="d-flex justify-content-between align-items-start flex-wrap mb-2">
+                            <div class="d-flex flex-column">
+                                <div class="d-flex align-items-center mb-2">
+                                    <a href="#" class="text-gray-900 text-hover-primary fs-2 fw-bold me-1"
+                                        v-text="auth.name"></a>
+                                </div>
+                                <!--end::Name-->
+
+                                <!--begin::Info-->
+                                <div class="d-flex flex-wrap fw-semibold fs-6 mb-4 pe-2">
+                                    <a href="#" class="d-flex align-items-center text-gray-500 text-hover-primary me-5 mb-2">
+                                        <vue-feather class="w-5 mx-1" type="eye"></vue-feather>
+                                        <span v-text="auth.role.name"></span>
+                                    </a>
+                                    <a href="#" class="d-flex align-items-center text-gray-500 text-hover-primary me-5 mb-2">
+                                        <vue-feather class="w-5 mx-1" type="smartphone"></vue-feather>
+                                        <span v-text="auth.phone"></span>
+                                    </a>
+                                    <a href="#" class="d-flex align-items-center text-gray-500 text-hover-primary me-5 mb-2">
+                                        <vue-feather class="w-5 mx-1" type="at-sign"></vue-feather>
+                                        <span v-text="auth.email"></span>
+                                    </a>
+                                </div>
+                                <!--end::Info-->
+                            </div>
+                            <!--end::User-->
+
+                            <!--begin::Actions-->
+                            <div class="d-flex my-4">
+                                <a @click="showEditSide = true" href="javascript:;" class="text-white btn btn-sm btn-primary me-3" v-text="translate('Edit')"></a>
+                            </div>
+                            <!--end::Actions-->
+                        </div>
+                        <!--end::Title-->
+
+                        <!--begin::Stats-->
+                        <div class="d-flex flex-wrap flex-stack">
+                            <!--begin::Wrapper-->
+                            <div class="d-flex flex-column flex-grow-1 pe-8">
+                                <!--begin::Stats-->
+                                <div class="d-flex flex-wrap" v-if="activeItem.business && content">
+                                    <!--begin::Stat-->
+                                    <div class="border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3">
+                                        <div class="d-flex align-items-center">
+                                            <div class="fs-2 fw-bold" v-text="activeItem.business.subscription.plan_name"></div>
+                                        </div>
+                                        <div class="fw-semibold fs-6 text-gray-500" v-text="translate('Plan')"></div>
+                                    </div>
+                                    <div class="border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3">
+                                        <div class="d-flex align-items-center">
+                                            <div class="fs-2 fw-bold" v-text="content.stats.drivers_count"></div>
+                                        </div>
+                                        <div class="fw-semibold fs-6 text-gray-500" v-text="translate('Drivers')"></div>
+                                    </div>
+                                    <div class="border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3">
+                                        <div class="d-flex align-items-center">
+                                            <div class="fs-2 fw-bold" v-text="content.stats.vehicles_count"></div>
+                                        </div>
+                                        <div class="fw-semibold fs-6 text-gray-500" v-text="translate('Vehicles')"></div>
+                                    </div>
+                                    <div class="border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3">
+                                        <div class="d-flex align-items-center">
+                                            <div class="fs-2 fw-bold" v-text="content.stats.trips_count"></div>
+                                        </div>
+                                        <div class="fw-semibold fs-6 text-gray-500" v-text="translate('Trips')"></div>
+                                    </div>
+                                    <!--end::Stat-->
+
+                                </div>
+                                <!--end::Stats-->
+                            </div>
+                            <!--end::Wrapper-->
+
+                        </div>
+                        <!--end::Stats-->
+                    </div>
+                    <!--end::Info-->
                 </div>
-                <!--end::Pic-->
-
-                <!--begin::Info-->
-                <div class="flex-grow-1">
-                    <!--begin::Title-->
-                    <div class="d-flex justify-content-between align-items-start flex-wrap mb-2">
-                        <!--begin::User-->
-                        <div class="d-flex flex-column">
-                            <!--begin::Name-->
-                            <div class="d-flex align-items-center mb-2">
-                                <a href="#" class="text-gray-900 text-hover-primary fs-2 fw-bold me-1"
-                                    v-text="auth.name"></a>
-                            </div>
-                            <!--end::Name-->
-
-                            <!--begin::Info-->
-                            <div class="d-flex flex-wrap fw-semibold fs-6 mb-4 pe-2">
-                                <a href="#" class="d-flex align-items-center text-gray-500 text-hover-primary me-5 mb-2">
-                                    <vue-feather class="w-5 mx-1" type="eye"></vue-feather>
-                                    <span v-text="auth.role.name"></span>
-                                </a>
-                                <a href="#" class="d-flex align-items-center text-gray-500 text-hover-primary me-5 mb-2">
-                                    <vue-feather class="w-5 mx-1" type="smartphone"></vue-feather>
-                                    <span v-text="auth.phone"></span>
-                                </a>
-                                <a href="#" class="d-flex align-items-center text-gray-500 text-hover-primary me-5 mb-2">
-                                    <vue-feather class="w-5 mx-1" type="at-sign"></vue-feather>
-                                    <span v-text="auth.email"></span>
-                                </a>
-                            </div>
-                            <!--end::Info-->
-                        </div>
-                        <!--end::User-->
-
-                        <!--begin::Actions-->
-                        <div class="d-flex my-4">
-                            <a @click="showEditSide = true" href="javascript:;" class="text-white btn btn-sm btn-primary me-3" v-text="translate('Edit')"></a>
-                        </div>
-                        <!--end::Actions-->
-                    </div>
-                    <!--end::Title-->
-
-                    <!--begin::Stats-->
-                    <div class="d-flex flex-wrap flex-stack">
-                        <!--begin::Wrapper-->
-                        <div class="d-flex flex-column flex-grow-1 pe-8">
-                            <!--begin::Stats-->
-                            <div class="d-flex flex-wrap" v-if="activeItem.business && content">
-                                <!--begin::Stat-->
-                                <div class="border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3">
-                                    <div class="d-flex align-items-center">
-                                        <div class="fs-2 fw-bold" v-text="activeItem.business.subscription.plan_name"></div>
-                                    </div>
-                                    <div class="fw-semibold fs-6 text-gray-500" v-text="translate('Plan')"></div>
-                                </div>
-                                <div class="border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3">
-                                    <div class="d-flex align-items-center">
-                                        <div class="fs-2 fw-bold" v-text="content.stats.drivers_count"></div>
-                                    </div>
-                                    <div class="fw-semibold fs-6 text-gray-500" v-text="translate('Drivers')"></div>
-                                </div>
-                                <div class="border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3">
-                                    <div class="d-flex align-items-center">
-                                        <div class="fs-2 fw-bold" v-text="content.stats.vehicles_count"></div>
-                                    </div>
-                                    <div class="fw-semibold fs-6 text-gray-500" v-text="translate('Vehicles')"></div>
-                                </div>
-                                <div class="border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3">
-                                    <div class="d-flex align-items-center">
-                                        <div class="fs-2 fw-bold" v-text="content.stats.trips_count"></div>
-                                    </div>
-                                    <div class="fw-semibold fs-6 text-gray-500" v-text="translate('Trips')"></div>
-                                </div>
-                                <!--end::Stat-->
-
-                            </div>
-                            <!--end::Stats-->
-                        </div>
-                        <!--end::Wrapper-->
-
-                    </div>
-                    <!--end::Stats-->
-                </div>
-                <!--end::Info-->
             </div>
             <ul class="nav nav-stretch nav-line-tabs nav-line-tabs-2x border-transparent fs-5 fw-bold">
                 <li class="nav-item mt-2" v-for="tab in tabsList">
