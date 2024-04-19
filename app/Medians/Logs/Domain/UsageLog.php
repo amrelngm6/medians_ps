a7da8423c2197248f8685c36d0535d7cef78646a
unknown <amr_elngm6@yahoo.com>
2024-04-19 05:56:05 +0200

    .

--- app/Medians/Logs/Domain/UsageLog.php
+++ app/Medians/Logs/Domain/UsageLog.php
@@ -26,22 +26,30 @@ class UsageLog extends CustomModel
 
 	public static function addItem($model, $action, $updatedFields = null)
 	{
-		$user = (new \config\APP)->auth();
-		if ($model::class == UsageLog::class)
-		{
-			return null;
+		try {
+			
+			$user = (new \config\APP)->auth();
+			if ($model::class == UsageLog::class)
+			{
+				return null;
+			}
+			$pk = $model->getPrimaryKey();
+
+			$userpk = $user ? $user->getPrimaryKey() : null;
+			
+			$data = array();
+			$data['model_type'] = $model::class;
+			$data['model_id'] = $model->$pk;
+			$data['user_type'] = $userpk ? $user::class : '';
+			$data['user_id'] = $userpk ? $user->$userpk : 0;
+			$data['action'] = $action;
+			$data['data'] = $updatedFields ?? json_encode(json_decode($model));
+			$save = UsageLog::create($data);
+			return $save;
+
+		} catch (\Throwable $th) {
+			return error_log($th->getMessage());
 		}
-		$pk = $model->getPrimaryKey();
-		$userpk = $user->getPrimaryKey();
-		$data = array();
-		$data['model_type'] = $model::class;
-		$data['model_id'] = $model->$pk;
-		$data['user_type'] = $user::class;
-		$data['user_id'] = $user->$userpk;
-		$data['action'] = $action;
-		$data['data'] = $updatedFields ?? json_encode(json_decode($model));
-		$save = UsageLog::create($data);
-		return $save;
 	}
 
 }
