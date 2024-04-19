5015dcb36756e22700055a241419ba9eb9be6435
unknown <amr_elngm6@yahoo.com>
2024-04-20 01:42:24 +0200

    .

--- src/components/system_settings.vue
+++ src/components/system_settings.vue
@@ -27,7 +27,7 @@
                                 <div class="row mb-6"   v-for="(field, i) in row" >
                 
                                     <label class="col-lg-4 col-form-label " :class="field.required ? 'required' : ''" :for="'input'+i"  v-if="field.column_type != 'hidden'">
-                                        <p class="inline inline-block fw-bold fs-4" v-text="field.title" ></p>
+                                        <p class="inline inline-block fw-bold fs-4 w-full" v-text="field.title" ></p>
                                         <span v-text="field.help_text" v-if="field.help_text" ></span>
                                     </label>
 
