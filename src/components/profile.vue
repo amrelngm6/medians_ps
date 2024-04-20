fd0bc0fedd76776a87fa266e9172088253c56958
unknown <amr_elngm6@yahoo.com>
2024-04-20 05:38:58 +0200

    .

--- src/components/profile.vue
+++ src/components/profile.vue
@@ -312,7 +312,7 @@
                                     <div class="d-flex flex-wrap">
                                         <div class="border border-dashed border-gray-300 w-125px rounded my-3 p-4 me-6">                    
                                             <span class="fs-2x fw-bold text-gray-800 lh-1" v-if="wallet">
-                                                <span data-kt-countup="true" data-kt-countup-value="6,840" data-kt-countup-prefix="$" class="counted" data-kt-initialized="1" v-text="wallet ? (system_setting.currency+''+wallet.credit_balance) : '0'"></span>
+                                                <span v-text="wallet ? (system_setting.currency+''+wallet.credit_balance) : '0'"></span>
                                             </span>
                                             <span class="fs-6 fw-semibold text-gray-500 d-block lh-1 pt-2" v-text="translate('Net Earnings')"></span>
                                         </div>
