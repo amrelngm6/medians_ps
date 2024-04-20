b811f214e1022afa0f7b842425c45f776ecd471a
unknown <amr_elngm6@yahoo.com>
2024-04-20 09:17:09 +0200

    .

--- src/components/wizards/businessWithdrawalWizard.vue
+++ src/components/wizards/businessWithdrawalWizard.vue
@@ -134,15 +134,15 @@
                             <!--end::Sidebar-->
                         </div>
                         
-                        <div class="mx-auto flex gap-6 w-500px py-2" v-if="activeItem.status == 'pending'">
+                        <div class="mx-auto flex gap-6 w-500px py-2 text-gray-200" v-if="activeItem.status == 'pending'">
                             <div class="text-center"><a href="javascript:;"
                                 class="uppercase px-4 py-3 mx-2 text-center text-white rounded-lg bg-primary"
                                 @click="approveItem" v-text="translate('Approved')"></a></div>
-                                >
+                                -
                             <div class="text-center"><a href="javascript:;"
                                 class="uppercase px-4 py-3 mx-2 text-center text-white rounded-lg bg-info"
                                 @click="confirmItem" v-text="translate('Set as done')"></a></div>
-                                >
+                                -
                             <div class="text-center"><a href="javascript:;"
                                 class="uppercase px-4 py-3 mx-2 text-center text-white rounded-lg bg-danger"
                                 @click="rejectItem" v-text="translate('Reject')"></a></div>
