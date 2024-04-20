1af548d0f7d12c4d512f4434660a398799fe1dc0
unknown <amr_elngm6@yahoo.com>
2024-04-20 05:45:11 +0200

    .

--- src/components/profile.vue
+++ src/components/profile.vue
@@ -311,8 +311,8 @@
                                 <div class="d-flex flex-wrap justify-content-between pb-6">
                                     <div class="d-flex flex-wrap">
                                         <div class="border border-dashed border-gray-300 w-125px rounded my-3 p-4 me-6">                    
-                                            <span class="fs-2x fw-bold text-gray-800 lh-1" v-if="wallet">
-                                                <span v-text="wallet ? (system_setting.currency+''+(wallet.credit_balance ?? '0')) : '0'"></span>
+                                            <span class="fs-2x fw-bold text-gray-800 lh-1" v-if="content.wallet">
+                                                <span v-text="content.wallet ? (system_setting.currency+''+(content.wallet.credit_balance ?? '0')) : '0'"></span>
                                             </span>
                                             <span class="fs-6 fw-semibold text-gray-500 d-block lh-1 pt-2" v-text="translate('Net Earnings')"></span>
                                         </div>
@@ -324,7 +324,7 @@
                                         </div>
                                         <div class="border border-dashed border-gray-300 w-125px rounded my-3 p-4 me-6">
                                             <span class="fs-2x fw-bold text-gray-800 lh-1">
-                                                <span data-kt-countup="true" data-kt-countup-value="1,240" data-kt-countup-prefix="$" class="counted" data-kt-initialized="1" v-text="wallet ? wallet.credit_balance : '0'"></span>
+                                                <span data-kt-countup="true" data-kt-countup-value="1,240" data-kt-countup-prefix="$" class="counted" data-kt-initialized="1" v-text="content.wallet ? content.wallet.credit_balance : '0'"></span>
                                             </span>
                                             <span class="fs-6 fw-semibold text-gray-500 d-block lh-1 pt-2" v-text="translate('Balance to withdraw')"></span>
                                         </div>
