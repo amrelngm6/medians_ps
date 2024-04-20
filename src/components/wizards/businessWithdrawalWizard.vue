f9c2c6c4258c9912443c33c3280faa8fbf24df63
unknown <amr_elngm6@yahoo.com>
2024-04-20 10:11:57 +0200

    .

--- src/components/wizards/businessWithdrawalWizard.vue
+++ src/components/wizards/businessWithdrawalWizard.vue
@@ -197,7 +197,7 @@
                             <!--end::Sidebar-->
                         </div>
                         
-                        <div class="mx-auto flex gap-6 w-500px py-2 text-gray-200" v-if="activeItem.status == 'pending'">
+                        <div class="mx-auto flex gap-6 w-500px py-2 text-gray-200" v-if="activeItem.status == 'pending' || activeItem.states == 'approved'">
                             <div class="text-center"><a href="javascript:;"
                                 class="uppercase px-4 py-3 mx-2 text-center text-white rounded-lg bg-primary"
                                 @click="approveItem" v-text="translate('Approved')"></a></div>
