74bdf10ad4be4abe708327c9635efa57b9457356
unknown <amr_elngm6@yahoo.com>
2024-04-21 11:18:38 +0200

    .

--- src/components/wizards/withdrawalWizard.vue
+++ src/components/wizards/withdrawalWizard.vue
@@ -18,8 +18,8 @@
                             <div class="flex-lg-row-fluid me-xl-18 mb-10 mb-xl-0">
                                 <div class="mt-n1">
                                     <div class="d-flex flex-stack pb-10" >
-                                        <a href="#" v-if="business_setting">
-                                            <img alt="Logo" class="w-250px" :src="business_setting['logo'] ?? system_setting.logo">
+                                        <a href="#" v-if="activeItem.user">
+                                            <img alt="Logo" class="w-250px" :src="activeItem.user.picture ?? '/uploads/images/default_profile.png'">
                                         </a>
                                     </div>
                                     <div class="m-0">
