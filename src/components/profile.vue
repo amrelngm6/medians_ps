922195e5b3caf8f1c1386b9d3e6faad727d309de
unknown <amr_elngm6@yahoo.com>
2024-04-21 04:00:27 +0200

    .

--- src/components/profile.vue
+++ src/components/profile.vue
@@ -347,7 +347,6 @@
                         </div>
                     </div>
 
-                    
                     <div class="card card-flush pt-3 mb-5 mb-xl-10" v-if="activeTab == 'invoices'">
                         <!--begin::Card header-->
                         <div class="card-header">
@@ -425,7 +424,7 @@
                             <table id="kt_customer_details_invoices_table_1" class="table align-middle table-row-dashed fs-6 fw-bold gs-0 gy-4 p-0 m-0">
                                 <thead class="border-bottom border-gray-200 fs-7 text-uppercase fw-bold">
                                     <tr class="text-start text-gray-500">
-                                        <th class="min-w-100px" v-text="translate('#')"></th>
+                                        <th class="min-w-100px" v-text="translate('Payment method')"></th>
                                         <th class="min-w-100px" v-text="translate('Amount')"></th>
                                         <th class="min-w-100px" v-text="translate('Status')"></th>                                    
                                         <th class="min-w-125px" v-text="translate('Date')"></th>
@@ -434,7 +433,7 @@
                                 </thead>
                                 <tbody class="fs-6 fw-semibold text-gray-600">
                                     <tr v-for="withdrawal in content.business_withdrawals">
-                                        <td><a href="#" class="text-gray-600 text-hover-primary" v-text="withdrawal ? withdrawal.withdrawal_id : ''"></a></td>
+                                        <td v-text="withdrawal.payment_method"></td>
                                         <td class="text-success"  v-text="system_setting.currency+''+withdrawal.amount"></td>
                                         <td><span class="badge badge-light-warning" v-text="withdrawal.status"></span></td>
                                         <td v-text="withdrawal.date"></td>
