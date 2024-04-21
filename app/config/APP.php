b0bd5f49774a1f10f1dd5d7a5b6f850b71249f43
unknown <amr_elngm6@yahoo.com>
2024-04-21 05:11:41 +0200

    .

--- app/config/APP.php
+++ app/config/APP.php
@@ -323,7 +323,7 @@ class APP
 			
 			array('title'=>__('Wallets'),  'icon'=>'dollar-sign', 'link'=>'#wallets', 'sub'=>
 			[
-				array('permission'=>'BusinessWallets.index', 'title'=>__('Business Wallets'),  'icon'=>'help-circle', 'link'=>'admin/business_wallets', 'component'=>'wallets'),
+				array('permission'=>'BusinessWallets.index', 'title'=>__('Business Wallets'),  'icon'=>'help-circle', 'link'=>'admin/business_wallets', 'component'=>'business_wallets'),
 				array('permission'=> 'BusinessWithdrawals.index', 'title'=> __('Business Withdrawals'), 'icon'=>'credit-card', 'link'=>'admin/business_withdrawals', 'component'=>'business_withdrawals'),
 			]
 			),
