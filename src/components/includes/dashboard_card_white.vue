21ecaa329286089213a5447c7b90be30d329ba55
unknown <amr_elngm6@yahoo.com>
2024-04-21 05:19:05 +0200

    .

--- src/components/includes/dashboard_card_white.vue
+++ src/components/includes/dashboard_card_white.vue
@@ -1,15 +1,17 @@
 <template>
-    <div class="widget-item bg-white p-6 flex justify-between rounded-md">
-        <div>
-            <h4 class="text-xl font-semibold text-slate-700 mb-1 leading-none" v-text="value"></h4>
-            <p class="text-tiny leading-4" v-text="title"></p>
+    
+    <a href="#" :class="classes" class="card hoverable card-xl-stretch mb-xl-8">
+        <!--begin::Body-->
+        <div class="card-body">
+            <img width="25" height="25" class="mx-auto" :src="icon" />
+
+            <div class="text-gray-900 fw-bold fs-2 mb-2 mt-5"  v-text="value"> </div>
+
+            <div class="fw-semibold text-gray-400"  v-text="title"></div>
         </div>
-        <div>
-            <span :class="classes"  class="text-lg text-purple-300 rounded-full flex items-center justify-center h-12 w-12 shrink-0 ">
-                <img width="25" height="25" class="mx-auto" :src="icon" />
-            </span>
-        </div>
-    </div>
+        <!--end::Body-->
+    </a>
+
 </template>
 <script>
 
