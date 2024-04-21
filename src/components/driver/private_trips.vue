399dfaa076995e9278823f78b5bd554b100098cf
unknown <amr_elngm6@yahoo.com>
2024-04-21 14:59:36 +0200

    .

--- src/components/driver/private_trips.vue
+++ src/components/driver/private_trips.vue
@@ -2,25 +2,25 @@
     <div class="w-full card mb-5 mb-xl-10">
         <div class="card-header">
                 <div class="card-title flex gap-4">
-                    <h2 class="w-full" v-text="translate('Latest trips')"></h2>
+                    <h2 class="w-full" v-text="translate('Latest private trips')"></h2>
                 </div>
             </div>
-            <div class="card-body pt-2" v-if="trips">
+            <div class="card-body pt-2" v-if="private_trips">
                 <table id="kt_customer_details_invoices_table_1"
                     class="table align-middle table-row-dashed fs-6 fw-bold gs-0 gy-4 p-0 m-0">
                     <thead class="border-bottom border-gray-200 fs-7 text-uppercase fw-bold">
                         <tr class="text-start text-gray-500">
                             
                             <th class="min-w-100px" v-text="translate('ID')"></th>
-                            <th class="min-w-100px" v-text="translate('Locations')"></th>
-                            <th class="min-w-100px" v-text="translate('Route')"></th>
-                            <th class="min-w-100px" v-text="translate('Type')"></th>
+                            <th class="min-w-100px" v-text="translate('Pickup')"></th>
+                            <th class="min-w-100px" v-text="translate('Destination')"></th>
+                            <th class="min-w-100px" v-text="translate('Cost')"></th>
                             <th class="min-w-100px" v-text="translate('Status')"></th>
                             <th class="min-w-125px" v-text="translate('Date')"></th>
                         </tr>
                     </thead>
                     <tbody class="fs-6 fw-semibold text-gray-600">
-                        <tr v-for="trip in trips">
+                        <tr v-for="trip in private_trips">
                             <td v-text="trip.trip_id"></td>
                             <td v-text="trip.locations.length"></td>
                             <td v-text="trip.route.route_name ?? ''"></td>
@@ -78,7 +78,7 @@ export default {
         'conf',
         'auth',
         'item',
-        'trips',
+        'private_trips',
     ]
 };
 </script>
