3b16e416bf2f73f48276d4f4c8763155afcb206d
unknown <amr_elngm6@yahoo.com>
2024-04-21 03:54:59 +0200

    .

--- src/components/wizards/businessWithdrawalWizard.vue
+++ src/components/wizards/businessWithdrawalWizard.vue
@@ -109,7 +109,6 @@
                             <div class="text-center"  v-if="activeItem.states == 'pending'"><a href="javascript:;"
                                 class="uppercase px-4 py-3 mx-2 text-center text-white rounded-lg bg-primary"
                                 @click="approveItem" v-text="translate('Approved')"></a> - </div>
-                                
                             <div v-if="activeItem.status == 'approved'" class="text-center"><a href="javascript:;"
                                 class="uppercase px-4 py-3 mx-2 text-center text-white rounded-lg bg-info"
                                 @click="confirmItem" v-text="translate('Set as done')"></a> - </div>
