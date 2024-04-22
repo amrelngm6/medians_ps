9abba921d7d5886f49d6b709ace56f1d85edd919
unknown <amr_elngm6@yahoo.com>
2024-04-22 10:15:09 +0200

    .

--- app/Medians/Businesses/Domain/Business.php
+++ app/Medians/Businesses/Domain/Business.php
@@ -49,7 +49,7 @@ class Business extends CustomModel
 
     public function subscription()
     {
-		return $this->hasOne(PlanSubscription::class, 'business_id', 'business_id')->with('plan', 'plan_features')->orderBy('end_date', 'DESC');	
+		return $this->hasOne(PlanSubscription::class, 'business_id', 'business_id')->with('plan', 'plan_features')->orderBy('start_date', 'DESC');	
     }
 
     public function drivers()
