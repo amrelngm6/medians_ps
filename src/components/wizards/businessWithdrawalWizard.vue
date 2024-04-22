80e2682bb45eeba7216e21e2247aa3c25e782fb6
unknown <amr_elngm6@yahoo.com>
2024-04-22 02:24:10 +0200

    .

--- src/components/wizards/businessWithdrawalWizard.vue
+++ src/components/wizards/businessWithdrawalWizard.vue
@@ -65,10 +65,6 @@
                                             <div class="fw-bold text-gray-800 fs-2" v-text="activeItem.wallet.credit_balance"></div>
                                         </div>
                 
-                                        <div class="mb-6">
-                                            <div class="fw-semibold text-gray-600 fs-4" v-text="translate('Status')"></div>
-                                            <div class="fw-bold text-gray-800 fs-2" v-text="activeItem.status"></div>
-                                        </div>
 
                                         <h6 class="mb-2 mt-4 fw-bolder text-gray-600 text-hover-primary" v-text="translate('Request Details')"></h6>
 
