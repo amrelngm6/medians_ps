75a0e6401ca60f69b6b74dd72dcc27a66502814f
unknown <amr_elngm6@yahoo.com>
2024-04-22 05:04:48 +0200

    .

--- src/components/wizards/paymentMethodWizard.vue
+++ src/components/wizards/paymentMethodWizard.vue
@@ -56,7 +56,7 @@
 
                                         <hr class="opacity-10 my-4" />
                                         <div class="flex w-full">
-                                            <a v-text="translate('add_new')" @click="activeItem.fields ? activeItem.fields.append({'title':''}) : (activeItem.fields = [{'title': ''}])" href="javascript:;" class="text-white btn btn-primary er fs-6 px-8 py-4" ></a>
+                                            <a v-text="translate('add_new')" @click="addField()" href="javascript:;" class="text-white btn btn-primary er fs-6 px-8 py-4" ></a>
                                         </div>
                                         <div class="w-full mb-6 " v-if="activeItem.fields">
                                             <div class="w-full mb-6 mx-auto row" v-for="(field, key) in activeItem.fields">
@@ -270,7 +270,17 @@ export default
                 return getProgressWidth(requiredData, activeItem);
             }
 
+            const addField = () => {
+                if (!activeItem.fields)
+                {
+                    activeItem.fields = [{'title': ''}];
+                } else {
+                    activeItem.fields.append({'title':''})
+                }
+            }
+
             return {
+                addField,
                 progressWidth,
                 showEditSide,
                 content,
