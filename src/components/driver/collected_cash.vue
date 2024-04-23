22cefb9550f0e53b3d12e46cde700737bc8f97c1
unknown <amr_elngm6@yahoo.com>
2024-04-23 11:34:01 +0200

    .

--- src/components/driver/collected_cash.vue
+++ src/components/driver/collected_cash.vue
@@ -13,12 +13,14 @@
                     class="table align-middle table-row-dashed fs-6 fw-bold gs-0 gy-4 p-0 m-0">
                     <thead class="border-bottom border-gray-200 fs-7 text-uppercase fw-bold">
                         <tr class="text-start text-gray-500">
+                            <th class="min-w-100px" v-text="translate('ID')"></th>
                             <th class="min-w-100px" v-text="translate('Amount')"></th>
                             <th class="min-w-125px" v-text="translate('Date')"></th>
                         </tr>
                     </thead>
                     <tbody class="fs-6 fw-semibold text-gray-600">
                         <tr v-for="collected in collected_cash" >
+                            <td v-text="collected.collection_id"></td>
                             <td class="text-success" v-text="system_setting.currency + '' + collected.amount"></td>
                             <td v-text="collected.date"></td>
                         </tr>
