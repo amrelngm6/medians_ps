02cdc63ebd43033e7c3a126b53c956d718d60dd8
unknown <amr_elngm6@yahoo.com>
2024-04-23 11:18:05 +0200

    .

--- app/Medians/Wallets/Application/CollectedCashController.php
+++ app/Medians/Wallets/Application/CollectedCashController.php
@@ -38,7 +38,6 @@ class CollectedCashController extends CustomController
             [ 'value'=> "wallet.user.name", 'text'=> translate('User'), 'sortable'=> true ],
             [ 'value'=> "amount", 'text'=> translate('Amount'), 'sortable'=> true ],
             [ 'value'=> "date", 'text'=> translate('Date'), 'sortable'=> true ],
-            [ 'value'=> "edit", 'text'=> translate('Edit') ],
             [ 'value'=> "delete", 'text'=> translate('Delete') ],
         ];
 	}
