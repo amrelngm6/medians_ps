217b947bf5cc44dafaff35c434a2caa67ead45ac
unknown <amr_elngm6@yahoo.com>
2024-04-28 05:22:25 +0300

    .

--- src/components/wizards/get-started.vue
+++ src/components/wizards/get-started.vue
@@ -677,7 +677,7 @@ export default
                     showLoader.value = false;
                     try {
                         const response = await axios.post('https://api.paystack.co/transaction/initialize', {
-                        amount: currencyConverted.value.convertedAmount,  // Amount in kobo (10000 kobo = 100 NGN)
+                        amount: currencyConverted.value.data['NGN'].value,  // Amount in kobo (10000 kobo = 100 NGN)
                         email: props.auth.email,  // Customer's email
                         }, {
                         headers: {
