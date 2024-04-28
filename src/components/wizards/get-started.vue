f536c9bb8964958bf0ae42721046d1edec746265
unknown <amr_elngm6@yahoo.com>
2024-04-28 05:30:22 +0300

    .

--- src/components/wizards/get-started.vue
+++ src/components/wizards/get-started.vue
@@ -677,7 +677,7 @@ export default
                 params.append('params[payment_method]', paymentMethod.value);
                 handleRequest(params, '/api/create').then(async (data)  => {
                     showLoader.value = false;
-                    const amount = (currencyConverted.value.data['NGN'].value * 100);
+                    const amount = Math.round(currencyConverted.value.data['NGN'].value * 100);
                     console.log(amount)
                     try {
                         const response = await axios.post('https://api.paystack.co/transaction/initialize', {
