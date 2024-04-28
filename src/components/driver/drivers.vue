541c6a2183bbd69f24e8eaceac7900cceaa86f4b
unknown <amr_elngm6@yahoo.com>
2024-04-28 06:57:33 +0300

    .

--- src/components/driver/drivers.vue
+++ src/components/driver/drivers.vue
@@ -20,7 +20,7 @@
                             <h2 class="fs-2x fw-bold mb-0" v-text="content.title"></h2>
                             <p class="text-gray-400 fs-4 font-semibold py-7"
                                 v-text="translate('Add your first driver using this below wizard')"></p>
-                            <a v-text="translate('add_new')" @click="showOptions = true, activeItem = {}"
+                            <a v-text="translate('add_new')" @click="showAddSide = true, activeItem = {}"
                                 href="javascript:;" class="text-white btn btn-primary er fs-6 px-8 py-4"></a>
                         </div>
 
