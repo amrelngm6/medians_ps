cbd3166ec40e1c34ae2ad4f57748273fd77d76db
unknown <amr_elngm6@yahoo.com>
2024-04-30 17:09:49 +0300

    .

--- src/components/newsletter_subscribers.vue
+++ src/components/newsletter_subscribers.vue
@@ -8,8 +8,7 @@
                     <div class="card-body">
                         <div class="card-px text-center pt-15 pb-15">
                             <h2 class="fs-2x fw-bold mb-0" v-text="content.title"></h2>
-                            <p class="text-gray-400 fs-4 font-semibold py-7" v-text="translate('Add your first translation using this below wizard')"></p>
-                            <a v-text="translate('add_new')" @click="showWizard = true, activeItem = {}" href="javascript:;" class="text-white btn btn-primary er fs-6 px-8 py-4" ></a>
+                            <p class="text-gray-400 fs-4 font-semibold py-7" v-text="translate('Here you will se the list of the newsletter subscriptions')"></p>
                         </div>
 
                         <div class="text-center pb-15 px-5">
