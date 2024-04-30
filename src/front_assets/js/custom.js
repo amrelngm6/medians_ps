9f55a00c2f06e343c5caed4c8aeb97b85c4fea51
unknown <amr_elngm6@yahoo.com>
2024-04-30 16:37:18 +0300

    .

--- src/front_assets/js/custom.js
+++ src/front_assets/js/custom.js
@@ -435,14 +435,13 @@ function showAlert(text = '', duration=5000)
   Toastify({
     text: "This is a toast",
     duration: duration,
-    destination: "https://github.com/apvarun/toastify-js",
     newWindow: true,
     close: true,
     gravity: "top", // `top` or `bottom`
     position: "center", // `left`, `center` or `right`
     stopOnFocus: true, // Prevents dismissing of toast on hover
     style: {
-      background: "linear-gradient(to right, #00b09b, #96c93d)",
+      background: "linear-gradient(to right, #ef326f, #fe6c4e, #ef326f, #fe6c4e)",
     },
     onClick: function(){} // Callback after click
   }).showToast();
