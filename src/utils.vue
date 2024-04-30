a9702372d6be092ae4098d1a8dbb497d705e4ea5
unknown <amr_elngm6@yahoo.com>
2024-04-30 18:25:20 +0300

    .

--- src/utils.vue
+++ src/utils.vue
@@ -52,18 +52,17 @@ export function customConfirm(message) {
     const modal = document.createElement('div');
     modal.classList.add('delete-modal');
     modal.innerHTML = `
-    <div class="swal2-container swal2-center swal2-backdrop-show overflow-y-auto"> 
+    <div class="swal2-container swal2-center swal2-backdrop-show overflow-y-auto py-10"> 
         <div class="swal2-popup swal2-modal swal2-icon-warning swal2-show grid" >
             <div class="swal2-icon swal2-warning swal2-icon-show flex" >
                 <div class="swal2-icon-content">!</div>
             </div>
             <div class="swal2-html-container" id="swal2-html-container" style="display: block;">${message}</div>
             <div class="swal2-actions" style="display: flex;">
-                <button type="button" class="swal2-confirm btn fw-bold btn-danger"
+                <button type="button" class="swal2-confirm btn fw-bold btn-danger" id="confirmBtn"
                     style="display: inline-block;">${translate('yes delete')}</button>
                 <button type="button"
-                    class="swal2-cancel btn fw-bold btn-active-light-primary" style="display: inline-block;">No,
-                    cancel</button>
+                    class="swal2-cancel btn fw-bold btn-active-light-primary text-white" id="cancelBtn" style="display: inline-block;">${translate('no cancel')}</button>
             </div>
         </div>
     </div>
