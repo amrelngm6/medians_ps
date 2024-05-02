9ffb18b54622cc7ca46f7228bcd6d9e3200d996f
unknown <amr_elngm6@yahoo.com>
2024-05-02 07:57:18 +0300

    .

--- src/components/includes/dashboard_chart.vue
+++ src/components/includes/dashboard_chart.vue
@@ -1,20 +1,11 @@
 <template>
     
-    <div :class="classes" class="card hoverable card-xl-stretch mb-xl-8">
+    <div >
         <CChartLine
             v-if="options"
             :wrapper="false"
             :data="handleData"
             />
-        <!--begin::Body-->
-        <div class="card-body">
-            <img width="25" height="25" :src="icon" />
-
-            <div :class="value_class" class="fw-bold fs-2 mb-2 mt-5"  v-text="value"> </div>
-
-            <div :class="text_class" class="fw-semibold "  v-text="title"></div>
-        </div>
-        <!--end::Body-->
     </div>
 
 </template>
@@ -30,7 +21,6 @@ export default
         
         const handleData = () => 
         {
-            console.log(props.options);
             return props.options;
             
         }
