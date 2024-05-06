684e031e5db3dc595181d8a53addd5e2d26ef5c2
unknown <amr_elngm6@yahoo.com>
2024-05-06 12:55:24 +0300

    .

--- src/components/datatable_pages/data_table_page.vue
+++ src/components/datatable_pages/data_table_page.vue
@@ -7,7 +7,7 @@
                         <h2 class="fs-2x fw-bold mb-0" v-text="content.title"></h2>
                         <p class="text-gray-400 fs-4 font-semibold py-7"
                             v-text="translate('Empty data')"></p>
-                        <a v-text="translate('add_new')" @click="openCreate()"
+                        <a  v-if="!content.no_create" v-text="translate('add_new')" @click="openCreate()"
                             href="javascript:;" class="text-white btn btn-primary er fs-6 px-8 py-4"></a>
                     </div>
 
