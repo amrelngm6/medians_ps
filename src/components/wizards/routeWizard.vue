7e5a18894e67f5988228d6d7ca795cfb05c16bbc
unknown <amr_elngm6@yahoo.com>
2024-05-06 12:32:31 +0300

    .

--- src/components/wizards/routeWizard.vue
+++ src/components/wizards/routeWizard.vue
@@ -361,6 +361,11 @@ export default
 
         const saveRoute = () =>
         {
+            
+            const currentInstance = getCurrentInstance();
+            if (currentInstance) 
+                currentInstance.root.data.loader = true;
+
             var params = new URLSearchParams();
             let array = JSON.parse(JSON.stringify(activeItem.value));
             let keys = Object.keys(array)
@@ -374,10 +379,6 @@ export default
             let type = array.route_id > 0 ? 'update' : 'create';
             params.append('type', 'Route.'+type)
             
-            const currentInstance = getCurrentInstance();
-            if (currentInstance) 
-                currentInstance.root.data.loader = true;
-
             handleRequest(params, '/api/'+type).then(response => {
                 handleAccess(response)
             })
