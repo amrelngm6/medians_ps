c014e6d227cdfb0a4eb5be5b1b44dca1704cb12c
unknown <amr_elngm6@yahoo.com>
2024-05-06 12:37:37 +0300

    .

--- src/utils.vue
+++ src/utils.vue
@@ -130,10 +130,10 @@ export async function handleGetRequest(url) {
 export async function handleRequest(params, url = '/api') {
     
          
-    const currentInstance =  getCurrentInstance();
-    if (currentInstance)
-        currentInstance.root.data.loader = true;  
-    console.log(currentInstance)
+    const currentInstance2 =  getCurrentInstance();
+    if (currentInstance2)
+        currentInstance2.root.data.loader = true;  
+    console.log(currentInstance2)
     return null;
     return await axios.post(url, params.toString()).then(response => {
         return response.data;
