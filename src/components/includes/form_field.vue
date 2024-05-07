47670f4a9be63570cb88dcaf6d3f3624595cffe7
unknown <amr_elngm6@yahoo.com>
2024-05-07 13:29:00 +0300

    .

--- src/components/includes/form_field.vue
+++ src/components/includes/form_field.vue
@@ -13,10 +13,10 @@
             <span :class="!item[column.key] ? 'bg-gray-200' : 'bg-red-400'" class="mx-2 mt-1 bg-red-400 block h-4 relative rounded-full w-8" style="direction: ltr;" ><a class="absolute bg-white block h-4 relative right-0 rounded-full w-4" :style="{left: item[column.key] ? '16px' : 0}"></a></span>
             <span  v-text="item[column.key] ? translate('Active') : translate('Pending')" v-if="!column.hide_text" class=" font-semibold inline-flex items-center px-2 py-1 rounded-full text-xs font-medium "></span>
             <input :value="''" :checked="item[column.key] ? false : true"  type="checkbox" class="hidden" :name="handleName(column)" />
-            <input  @change="changed(item[column.key])" :value="'on'" :checked="item[column.key] ? true : false"  type="checkbox" class="hidden" :name="handleName(column)" />
+            <input  :value="'on'" :checked="item[column.key] ? true : false"  type="checkbox" class="hidden" :name="handleName(column)" />
         </div>
         
-        <textarea @change="changed(item[column.key])" :required="column.required" :disabled="column.disabled" v-if="column.column_type == 'textarea'"  :name="handleName(column)" type="text" rows="4" class="mt-3 form-control form-control-solid" :placeholder="column.title" v-model="item[column.key]"></textarea>
+        <textarea  :required="column.required" :disabled="column.disabled" v-if="column.column_type == 'textarea'"  :name="handleName(column)" type="text" rows="4" class="mt-3 form-control form-control-solid" :placeholder="column.title" v-model="item[column.key]"></textarea>
 
         <Multiselect
             v-if="column.multiple && column.data && column.column_type == 'select'" 
@@ -33,7 +33,7 @@
 
         <input v-if="column.multiple && column.data && column.column_type == 'select'" type="hidden" v-for="selected in  item[column.column_key]" :name="'params['+(column.column_key)+'][]'" :value="selected[column.column_key]" />
 
-        <select @change="changed(item)" :required="column.required" :disabled="column.disabled" v-if="!column.multiple && column.data && column.column_type == 'select'" v-model="item[column.key]"  :name="handleName(column)" :type="column.column_type" class="form-control form-control-solid"   :placeholder="column.title">
+        <select @change="changed(column.data.map(e => e[column.key] == item[column.key]))" :required="column.required" :disabled="column.disabled" v-if="!column.multiple && column.data && column.column_type == 'select'" v-model="item[column.key]"  :name="handleName(column)" :type="column.column_type" class="form-control form-control-solid"   :placeholder="column.title">
             <option value="0"  v-if="!column.required" v-text="translate('select') +' '+ column.title"></option>
             <option v-for="option in column.data" :value="option[ column.column_key ? column.column_key : column.key]" v-text="option[column.text_key]"></option>
         </select>
