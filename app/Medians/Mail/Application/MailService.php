e826ebe31d62194d83ef177693d33598bd11b038
unknown <amr_elngm6@yahoo.com>
2024-05-08 01:25:40 +0300

    .

--- app/Medians/Mail/Application/MailService.php
+++ app/Medians/Mail/Application/MailService.php
@@ -65,7 +65,7 @@ class MailService
 		    //Content
 		    $mail->isHTML(true);                                  //Set email format to HTML
 		    $mail->Subject = $this->subject;
-		    $mail->Body    = render('views/email/email.html.twig',['msg'=> $this->body], null);
+		    $mail->Body    = $this->body;
 
 		    $mail->send();
 
