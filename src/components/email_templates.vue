a81333f2e464cb2b7f6a45737700f38e93050dd4
unknown <amr_elngm6@yahoo.com>
2024-05-08 00:17:22 +0300

    .

--- src/components/email_templates.vue
+++ src/components/email_templates.vue
@@ -1,40 +1,26 @@
 <template>
     <div class="w-full " >
         <main v-if="content " class=" flex-1 overflow-x-hidden overflow-y-auto  w-full">
-            <!-- Begin -->
             <div class="px-4 mb-6 py-4 rounded-lg shadow-md bg-white dark:bg-gray-700 flex w-full">
                 <h1 class="font-bold text-lg w-full" v-text="content.title"></h1>
             </div>
             <div class="mx-2 bg-white px-4 rounded shadow-sm py-2 ">
                     
                 <div class="card-header align-items-center py-5 gap-2 gap-md-5 w-full flex ">
-                    <!--begin::Card title-->
                     <div class="card-title">
-                        <!--begin::Search-->
                         <div class="d-flex align-items-center position-relative my-1">
                             <input type="text"  v-model="searchValue" data-kt-ecommerce-order-filter="search" class="form-control form-control-solid w-250px ps-12" placeholder="Search Report">
                         </div>
-                        <!--end::Search-->
-
-                        <!--begin::Export buttons-->
                         <div id="kt_ecommerce_report_views_export" class="d-none"><div class="dt-buttons btn-group flex-wrap">      <button class="btn btn-secondary buttons-copy buttons-html5" tabindex="0" aria-controls="kt_ecommerce_report_views_table" type="button"><span>Copy</span></button> <button class="btn btn-secondary buttons-excel buttons-html5" tabindex="0" aria-controls="kt_ecommerce_report_views_table" type="button"><span>Excel</span></button> <button class="btn btn-secondary buttons-csv buttons-html5" tabindex="0" aria-controls="kt_ecommerce_report_views_table" type="button"><span>CSV</span></button> <button class="btn btn-secondary buttons-pdf buttons-html5" tabindex="0" aria-controls="kt_ecommerce_report_views_table" type="button"><span>PDF</span></button> </div></div>
-                        <!--end::Export buttons-->
                     </div>
-                    <!--end::Card title-->
-
-                    <!--begin::Card toolbar-->
                     <div class="card-toolbar flex-row-fluid justify-content-end gap-5">
-
                         <div class="w-150px">
                             <select v-model="searchField" class="form-select form-select-solid select2-hidden-accessible" data-control="select2" data-hide-search="true" data-placeholder="Rating" data-kt-ecommerce-order-filter="rating" data-select2-id="select2-data-9-zple" tabindex="-1" aria-hidden="true" data-kt-initialized="1">
                                 <option v-for="col in content.columns" v-text="col.text" :value="col.value"></option>
                             </select>
                         </div>
-
                     </div>
-
                     <a href="javascript:;" class="uppercase p-2 mx-2 text-center text-white w-32 rounded-lg bg-danger" @click="openCreate()" v-text="translate('add_new')"></a>
-
                 </div>
                 <datatabble 
                     :search-field="searchField"
@@ -59,9 +45,9 @@
                     </template>
                 </datatabble>
 
-                <side_form_create ref="activeFormCreate" @callback="closeSide" :conf="conf" :model="'Page.create'" v-if="showAddSide && !showEditSide" :columns="content.fillable"  class="col-md-3" />
+                <side_form_create ref="activeFormCreate" @callback="closeSide" :conf="conf" :model="'EmailTemplate.create'" v-if="showAddSide && !showEditSide" :columns="content.fillable"  class="col-md-3" />
             
-                <side-form-update ref="activeFormUpdate" @callback="closeSide" :key="activeItem" :conf="conf" :model="'Page.update'" v-if="showEditSide && !showAddSide" :item="activeItem" :model_id="activeItem.page_id" index="page_id"  :columns="content.fillable"  class="col-md-3" />
+                <side-form-update ref="activeFormUpdate" @callback="closeSide" :key="activeItem" :conf="conf" :model="'EmailTemplate.update'" v-if="showEditSide && !showAddSide" :item="activeItem" :model_id="activeItem.template_id" index="template_id"  :columns="content.fillable"  class="col-md-3" />
 
 
             </div>
@@ -137,7 +123,7 @@ export default
             switch(actionName) 
             {
                 case 'details':
-                    window.open(props.conf.url+'admin/builder?prefix='+data.content.prefix, '_blank').focus();
+                    window.open(props.conf.url+'admin/email_builder?prefix='+data.content.prefix, '_blank').focus();
                     break;  
 
                 case 'edit':
@@ -147,7 +133,7 @@ export default
                     break;  
 
                 case 'delete':
-                    deleteByKey('page_id', data, 'Page.delete');
+                    deleteByKey('template_id', data, 'EmailTemplate.delete');
                     break;  
             }
         }
