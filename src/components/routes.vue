1f5c496ccbbda59f53d63d848f3a82c4d4542484
unknown <amr_elngm6@yahoo.com>
2024-05-07 03:09:45 +0300

    .

--- src/components/routes.vue
+++ src/components/routes.vue
@@ -68,7 +68,7 @@
                             
                             <div class="w-full relative flex" @click="handleAction('driver', item)">
                                 <img  class="rounded-full w-8 h-8 rounded-[50px] border-2 border-purple-800" :src="(item.driver && item.driver.picture) ? item.driver.picture : 'https://via.placeholder.com/37x37'" /> 
-                                <span class="font-semibold  px-1" v-if="item.driver" v-text="item.driver.name"></span></span>
+                                <span class="font-semibold  px-1" v-if="item.driver" v-text="item.driver.name"></span>
                             </div>
                             
                         </template>
