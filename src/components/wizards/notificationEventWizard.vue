ab070f08bf3729354428ec522f10d7981ffecbf5
unknown <amr_elngm6@yahoo.com>
2024-05-08 07:43:37 +0300

    .

--- src/components/wizards/notificationEventWizard.vue
+++ src/components/wizards/notificationEventWizard.vue
@@ -138,7 +138,7 @@
                                                     <div class="timeline-item" v-for="item in ['subject', 'model', 'receiver_model']">
                                                         <div class="timeline-line"></div>
                                                         <div class="timeline-icon me-4"><vue-feather type="check" class="fs-2 "></vue-feather></div>
-                                                        <div class="timeline-content pt-4 mt-n2">
+                                                        <div class="timeline-content mt-n2">
                                                             <div class="overflow-auto pe-3">
                                                                 <div class="text-muted me-2 fs-7" v-text="translate(item)"></div>
                                                                 <div class="fs-5 fw-semibold mb-2" v-if="activeItem[item]" v-text="activeItem[item]"></div>
