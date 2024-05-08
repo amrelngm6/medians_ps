722a4e977ce11a3e936d3f67eddee01be9279a95
unknown <amr_elngm6@yahoo.com>
2024-05-08 19:28:14 +0300

    .

--- app/Medians/Builders/Application/BuilderController.php
+++ app/Medians/Builders/Application/BuilderController.php
@@ -47,13 +47,13 @@ class BuilderController extends CustomController
 			$check = $this->pageRepo->findByLang($request->get('page_id'), $lang );
 
 			(isset($check->page_id) && !$check->lang_content) ? $this->contentRepo->handleMissingContent($check, $lang) : null;
-			
+			$check->content = $check->lang_content;
 			return render('views/admin/builder/index.html.twig', [
 				'page' => $check->lang_content ?? [], 
 				'item' => $check,
 				'current_lang' => $lang,
-				'precode' => isset($check->content) && (substr(trim($check->content), 0, 8) == '<section') ? '' : '<section id="newKeditItem" class="kedit">', 
-				'postcode' => isset($check->content) && (substr(trim($check->content), 0, 8) == '<section') ? '' : '</section>', 
+				'precode' => isset($check->lang_content) && (substr(trim($check->lang_content), 0, 8) == '<section') ? '' : '<section id="newKeditItem" class="kedit">', 
+				'postcode' => isset($check->lang_content) && (substr(trim($check->lang_content), 0, 8) == '<section') ? '' : '</section>', 
 			]);
 
 		} catch (\Exception $e) {
