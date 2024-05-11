f91f6d4cd153f92096b9fd81b5f59900f2be821e
unknown <amr_elngm6@yahoo.com>
2024-05-11 03:38:36 +0300

    .

--- src/components/includes/side-form-update.vue
+++ src/components/includes/side-form-update.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="sidebar-create-form " >
-        <div class="p-4 rounded-lg shadow-lg bg-white dark:bg-gray-700" >
+        <div class="p-4 rounded-lg shadow-lg bg-white " >
             <form action="/api/update" method="POST" data-refresh="1" id="add-device-form" class="action py-0 m-auto rounded-lg max-w-xl pb-10">
                 <div class="w-full flex">
                     <h1 class="w-full m-auto max-w-xl text-base mb-10 " v-text="translate('update')"></h1>
@@ -15,9 +15,9 @@
                     <div class="w-full" v-if="column" >
                         <span class="block mb-2" v-text="column.title" v-if="column.column_type != 'hidden'"></span>
 
-                        <input v-if="isInput(column.column_type)" autocomplete="off" :name="'params['+column.key+']'" :type="column.column_type" class="h-12 mb-3 rounded w-full border px-3 text-gray-700  focus:border-blue-100 dark:bg-gray-800  dark:border-gray-600" :placeholder="column.title" v-model="item[column.key]">
+                        <input v-if="isInput(column.column_type)" autocomplete="off" :name="'params['+column.key+']'" :type="column.column_type" class="h-12 mb-3 rounded w-full border px-3 text-gray-700  focus:border-blue-100   dark:border-gray-600" :placeholder="column.title" v-model="item[column.key]">
                     
-                        <input v-if="column.column_type == 'password'" autocomplete="off" :name="'params['+column.key+']'" :type="column.column_type" class="h-12 mb-3 rounded w-full border px-3 text-gray-700  focus:border-blue-100 dark:bg-gray-800  dark:border-gray-600" :placeholder="column.title">
+                        <input v-if="column.column_type == 'password'" autocomplete="off" :name="'params['+column.key+']'" :type="column.column_type" class="h-12 mb-3 rounded w-full border px-3 text-gray-700  focus:border-blue-100   dark:border-gray-600" :placeholder="column.title">
 
                         <div v-if="column.column_type == 'checkbox'"  class="py-4 flex gap gap-2 cursor-pointer" @click="setActiveStatus(item, column.key)">
                             <span :class="!item[column.key] ? 'bg-inverse-dark' : ''" class="mx-2 mt-1 bg-red-400 block h-4 relative rounded-full w-8" style="direction: ltr;" ><a class="absolute bg-white block h-4 relative right-0 rounded-full w-4" :style="{left: item[column.key] ? '16px' : 0}"></a></span>
@@ -25,9 +25,9 @@
                             <input v-model="item[column.key]"  type="checkbox" class="hidden" :name="'params['+column.key+']'" />
                         </div>
                         
-                        <textarea v-if="column.column_type == 'textarea'" :name="'params['+column.key+']'" type="text" rows="4" class="mt-3 rounded-lg w-full border px-3 text-gray-700  focus:border-blue-100 dark:bg-gray-800  dark:border-gray-600" :placeholder="column.title" v-model="item[column.key]"></textarea>
+                        <textarea v-if="column.column_type == 'textarea'" :name="'params['+column.key+']'" type="text" rows="4" class="mt-3 rounded-lg w-full border px-3 text-gray-700  focus:border-blue-100   dark:border-gray-600" :placeholder="column.title" v-model="item[column.key]"></textarea>
 
-                        <select v-if="column.data && column.column_type == 'select'" v-model="item[column.key]" :name="'params['+column.key+']'" :type="column.column_type" class="h-12 mb-3 rounded w-full border px-3 text-gray-700  focus:border-blue-100 dark:bg-gray-800  dark:border-gray-600"   :placeholder="column.title">
+                        <select v-if="column.data && column.column_type == 'select'" v-model="item[column.key]" :name="'params['+column.key+']'" :type="column.column_type" class="h-12 mb-3 rounded w-full border px-3 text-gray-700  focus:border-blue-100   dark:border-gray-600"   :placeholder="column.title">
                             <option value="0"  v-if="!column.required" v-text="translate('select') +' '+ column.title"></option>
                             <option v-for="option in column.data" :value="option[ column.column_key ? column.column_key : column.key]" v-text="option[column.text_key]"></option>
                         </select>
