00defa07e4cb99b3f1c548e4179f86fbbc7fe2fc
unknown <amr_elngm6@yahoo.com>
2024-07-01 23:38:09 +0300

    Fix builder issue

--- app/Medians/Builders/Application/BuilderController.php
+++ app/Medians/Builders/Application/BuilderController.php
@@ -52,9 +52,11 @@ class BuilderController extends CustomController
 			$itemId = $request->get('item_id') ?? $request->get('prefix');
 			
 			$item = $this->contentRepo->findItemByLang($itemId, $lang, $type ) ?? $this->contentRepo->find($itemId );
+			print_r($item);
 			
 			$check = isset($item->item_type) ? (new $item->item_type)->find($item->item_id) : null;
 
+			print_r($check);
 			(empty($check->content)) ? $this->handleMissingContent($type, $itemId, $lang) : null;
 
 			return render('views/admin/builder/index.html.twig', [
