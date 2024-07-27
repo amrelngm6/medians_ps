0df836fca94c1d7b40bee0948162063973cc4b20
unknown <amr_elngm6@yahoo.com>
2024-07-27 13:50:34 +0300

    Add CSV reader

--- src/components/wizards/campaignWizard.vue
+++ src/components/wizards/campaignWizard.vue
@@ -264,7 +264,7 @@ export default
             const activeItem = ref({});
             const activeTab = ref('Info');
             const content = ref({});
-            const fillable = ref(['Info', 'Fields' , 'Picture', 'Confirm']);
+            const fillable = ref(['Info', 'Fields' , 'Confirm']);
 
             if (props.item) {
                 activeItem.value = props.item
