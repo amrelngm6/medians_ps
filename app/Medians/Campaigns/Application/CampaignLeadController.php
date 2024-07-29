3ea503fc3a0e88bdcd848b502576da9f8945f49a
unknown <amr_elngm6@yahoo.com>
2024-07-29 06:25:33 +0300

    Add StatusList Domain

---
+++ app/Medians/Campaigns/Application/CampaignLeadController.php
@@ -0,0 +1,168 @@
+<?php
+namespace Medians\Campaigns\Application;
+
+use Shared\dbaser\CustomController;
+
+use Medians\Campaigns\Infrastructure\CampaignRepository;
+use Medians\Customers\Infrastructure\AgentRepository;
+
+class CampaignLeadController extends CustomController 
+{
+
+	/**
+	* @var Object
+	*/
+	protected $repo;
+
+	protected $agentRepo;
+
+	protected $app;
+
+	
+
+	function __construct()
+	{
+
+		$this->app = new \config\APP;
+
+		$this->repo = new CampaignRepository();
+		$this->agentRepo = new AgentRepository();
+	}
+
+
+
+
+	/**
+	 * Columns list to view at DataTable 
+	 *  
+	 */ 
+	public function columns( ) 
+	{
+
+		return [
+            [ 'value'=> "campaign_lead_id", 'text'=> "#"],
+            [ 'value'=> "campaign.name", 'text'=> translate('name'), 'sortable'=> true ],
+            [ 'value'=> "status", 'text'=> translate('status'), 'sortable'=> true ],
+            [ 'value'=> "edit", 'text'=> translate('edit')  ],
+            [ 'value'=> "delete", 'text'=> translate('delete')  ],
+        ];
+	}
+
+	
+
+	/**
+	 * Columns list to view at DataTable 
+	 *  
+	 */ 
+	public function fillable( ) 
+	{
+
+		return [
+            [ 'key'=> "campaign_lead_id", 'title'=> "", 'fillable'=>true, 'column_type'=>'hidden'],
+            [ 'key'=> "name", 'title'=> translate('name'),  'fillable'=> true, 'column_type'=>'text', 'required'=>true ],
+            [ 'key'=> "status", 'title'=> translate('status'),  'fillable'=>true, 'column_type'=>'checkbox' ],
+        ];
+	}
+
+	
+
+	/**
+	 * Admin index items
+	 * 
+	 * @param Silex\Application $app
+	 * @param \Twig\Environment $twig
+	 * 
+	 */ 
+	public function index( ) 
+	{
+		
+		try {
+			
+		    return render('data_table', 
+			[
+		        'load_vue' => true,
+		        'title' => translate('Campaign Leads'),
+		        'columns' => $this->columns(),
+		        'fillable' => $this->fillable(),
+		        'items' => $this->repo->get(),
+		        'agents' => $this->agentRepo->get(),
+				'object_name' => 'CampaignLead',
+				'object_key' => 'campaign_lead_id'
+		    ]);
+		} catch (\Exception $e) {
+			throw new \Exception($e->getMessage(), 1);
+			
+		}
+	}
+
+
+	
+
+
+	public function store() 
+	{
+
+		$params = $this->app->params();
+
+        try {	
+
+        	$params['created_by'] = $this->app->auth()->id;        	
+
+            $returnData = (!empty($this->repo->store($params))) 
+            ? array('success'=>1, 'result'=>translate('Added'), 'reload'=>1)
+            : array('success'=>0, 'result'=>'Error', 'error'=>1);
+
+        } catch (Exception $e) {
+        	throw new Exception(json_encode(array('result'=>$e->getMessage(), 'error'=>1)), 1);
+        }
+
+		return $returnData;
+	}
+
+
+	public function update()
+	{
+		$params = $this->app->params();
+
+        try {
+
+        	$params['status'] = !empty($params['status']) ? $params['status'] : null;
+
+            if ($this->repo->update($params) && $this->updateLeads($params['campaign_leads'], $params['campaign_id']))
+            {
+                return array('success'=>1, 'result'=>translate('Updated'), 'reload'=>1);
+            }
+        
+
+        } catch (\Exception $e) {
+        	throw new \Exception("Error Processing Request", 1);
+        	
+        }
+
+	}
+	
+
+	public function delete() 
+	{
+
+		$params = $this->app->params();
+
+        try {
+
+        	$check = $this->repo->find($params['campaign_id']);
+
+
+            if ($this->repo->delete($params['campaign_id']))
+            {
+                return json_encode(array('success'=>1, 'result'=>translate('Deleted'), 'reload'=>1));
+            }
+            
+
+        } catch (Exception $e) {
+        	throw new \Exception("Error Processing Request", 1);
+        	
+        }
+
+	}
+
+}
