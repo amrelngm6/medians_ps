c7595bb728f9c2101d2e60c3163c9b001131e5b4
unknown <amr_elngm6@yahoo.com>
2024-07-29 12:36:15 +0300

    Add StatusList Domain

--- app/Medians/StatusList/Infrastructure/StatusRepository.php
+++ app/Medians/StatusList/Infrastructure/StatusRepository.php
@@ -22,7 +22,7 @@ class StatusRepository
 	public function getLastWeekLeads()
 	{
 		return Status::withCount(['leads'=> function ($q) {
-			return $q->whereDate('updated_at', '>', date('Y-m-d', strtotime("-7 days")));
+			return $q->whereDate('updated_at', '>', date('Y-m-d', strtotime("-10 hours")));
 		}])
 		// whereHas('leads')
 		->get();
