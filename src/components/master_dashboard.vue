ec60ea1faabfae79439531972ef9519a8c007a87
unknown <amr_elngm6@yahoo.com>
2024-08-14 16:54:15 +0300

    Handle Errors

--- src/components/master_dashboard.vue
+++ src/components/master_dashboard.vue
@@ -15,7 +15,7 @@
                 </div>
             </div>
         </div>
-        <Timeline :resources="projects" :events="events" />
+        <timeline :resources="projects" :events="events" ></timeline>
 
         <div class="block w-full overflow-x-auto py-2">
             <div class="w-full overflow-y-auto overflow-x-hidden px-2 mt-6" >
