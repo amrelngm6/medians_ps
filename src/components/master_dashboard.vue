db4725a08f82f797c70faccc7d02fb301d2dc1ac
unknown <amr_elngm6@yahoo.com>
2024-08-14 16:39:42 +0300

    Handle Errors

--- src/components/master_dashboard.vue
+++ src/components/master_dashboard.vue
@@ -226,7 +226,7 @@ import dashboard_center_squares from '@/components/includes/dashboard_center_squ
 import {translate, handleGetRequest} from '@/utils.vue';
 import MapChart from 'vue-map-chart'
 // import { Timeline } from "@teej/vue-timeline";
-import { Timeline } from 'vue-timeline';
+import Timeline  from 'vue-timeline';
 
 import { AgChartsVue } from 'ag-charts-vue3';
 import VueTailwindDatepicker from "vue-tailwind-datepicker";
