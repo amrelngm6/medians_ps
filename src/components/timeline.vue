a1bd512d56d2c5781ee288492f7bb7bf3d286160
unknown <amr_elngm6@yahoo.com>
2024-08-14 19:15:21 +0300

    Handle Errors

--- src/components/timeline.vue
+++ src/components/timeline.vue
@@ -53,8 +53,8 @@ export default
                     
                     events.value = [];
                     for (let i = 0; i < content.value.visits_list.length; i++) {
-                        const element2 = content.value.visits_list[i];
-                        const id = getId(projects.value, element2);
+                        const element = content.value.visits_list[i];
+                        const id = getId(projects.value, element);
                         events.value.push({ id: i+1, resourceId: id, startDate: formatCustomTime(element.created_at, 'YYYY-MM-DD'), endDate: formatCustomTime(element.updated_at, 'YYYY-MM-DD'), name: (element && element.item) ? element.item.title : 'test'},)
                     }
                 }
