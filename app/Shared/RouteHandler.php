3832e15e9de8a0425e82b68770722755dedd7299
unknown <amr_elngm6@yahoo.com>
2024-08-29 06:39:28 +0300

    Doctors space

--- app/Shared/RouteHandler.php
+++ app/Shared/RouteHandler.php
@@ -73,7 +73,7 @@ class RouteHandler {
 
       self::$routes = preg_replace('/\/+/', '/', self::$routes);
       
-      // $uri = (substr($uri, -1) == '/') ? substr($uri, 0,-1) : $uri;
+      $uri = (substr($uri, -1) == '/') ? substr($uri, 0,-1) : $uri;
       $uri = empty($uri) ? '/' : $uri;
       // Check if route is defined without regex
       if (in_array($uri, self::$routes)) {
--- app/config/route.php
+++ app/config/route.php
@@ -359,8 +359,8 @@ RouteHandler::get('/logout', function ()
 /**
  * Load sub-pages
  */
-RouteHandler::get('/(:all)/', \Medians\Pages\Application\PageController::class.'@page'); 
 RouteHandler::get('/(:all)/(:all)', \Medians\Pages\Application\PageController::class.'@sub_page'); 
+RouteHandler::get('/(:all)/', \Medians\Pages\Application\PageController::class.'@page'); 
 RouteHandler::get('/(:all)', \Medians\Pages\Application\PageController::class.'@page'); 
 
 
