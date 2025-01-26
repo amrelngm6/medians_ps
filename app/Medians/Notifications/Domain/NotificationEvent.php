bb516cd06b45b99898904ff73d49aff4336405e9
unknown <amr_elngm6@yahoo.com>
2025-01-26 14:49:11 +0200

    Handle Forum Notification Event

--- app/Medians/Notifications/Domain/NotificationEvent.php
+++ app/Medians/Notifications/Domain/NotificationEvent.php
@@ -194,7 +194,7 @@ class NotificationEvent extends CustomModel
     	$params['receiver'] = $receiver;
 		$templateRepo = new \Medians\Templates\Infrastructure\EmailTemplateRepository;
 		$template = $templateRepo->findByLang($event->template_id, isset($receiver->field['language']) ? $receiver->field['language'] : $app->default_lang);
-    	$event->body = isset($template->content->content) ? $app->renderTemplate($template->content->content)->render($params) : '';
+    	$event->body = isset($template->content) ? $app->renderTemplate($template->content)->render($params) : 'No content';
     	$event->subject = $app->renderTemplate($event->subject)->render($params);
     	$event->body_text = $app->renderTemplate($event->body_text)->render($params);
 
