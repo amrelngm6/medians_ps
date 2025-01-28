5c5098e572477d4eaa9fbc2c69eb5ac5cd145074
unknown <amr_elngm6@yahoo.com>
2025-01-28 08:37:12 +0200

    Handle Builder icon

--- src/components/datatable_pages/data_table_page.vue
+++ src/components/datatable_pages/data_table_page.vue
@@ -202,7 +202,7 @@ export default
                     break;  
 
                 case 'builder':
-                    window.open(props.conf.url+'admin/builder?lang='+data.content.prefix)
+                    window.open(props.conf.url+'admin/builder?prefix='+data.content.prefix)
                     break;  
 
                 case 'delete':
